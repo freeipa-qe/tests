a3f69defc60ba2d7b2ba6c1a415b06e58428d413
Yi Zhang <yzhang@REDHAT.COM>
2008-10-02 23:44:18 +0000

    better directory handling
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@282 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/qautil/qa_ipa.restore.sh
+++ ipa-tests/qautil/qa_ipa.restore.sh
@@ -5,7 +5,7 @@ stopipa()
 {
   IPACTL=/usr/sbin/ipactl
   if [ -e $IPACTL ] &&  [ -x $IPACTL ];then
-	#$IPACTL stop
+	$IPACTL stop
 	ps -eo pid,cmd | grep ipa_web    | grep -v "grep" | cut -d" " -f1 > pid.txt	
 	ps -eo pid,cmd | grep kdc        | grep -v "grep" | cut -d" " -f1 >> pid.txt
 	ps -eo pid,cmd | grep ipa_kpasswd| grep -v "grep" | cut -d" " -f1 >> pid.txt
@@ -15,37 +15,35 @@ stopipa()
 		do
 		  kill -9 $pid
 		done
-		sleep 3
 		rm pid.txt
-	else
-		echo "ipa stopped"
 	fi
+	echo "ipa stopped"
+  	sleep 3
   else
 	echo "No $IPACTL found"
   fi
-}
-
-cleanup_runningfile()
-{
-  echo "clean up the lock and run file"
-  rm -rf /var/lock/subsys/dirsrv
-  rm -rf /var/lock/dirsrv
-  rm -rf /var/run/dirsrv
 }
 
 current_dir=`pwd`
 conf=$1
 if [ -e $conf ] && [ -r $conf ] ;then
 	echo "using $conf as snapshot restore configuration file"
+	echo "stoping ipa"
+	stopipa
 	while read line
 	do
 		echo $line
 		original=`echo $line | cut -d" " -f1`
 		tar=`echo $line | cut -d" " -f2`
 		cd $original
+		# change current file to backup files
+		for f in `ls`
+		do
+			mv $f bk.$f
+		done
 		tar xf $tar
 	done < $conf
-	cleanup_runningfile
+	$IPACTL start
 else
 	echo "$conf file does not exist or not readable, exit"
 fi
--- ipa-tests/qautil/qa_ipa.snapshot.sh
+++ ipa-tests/qautil/qa_ipa.snapshot.sh
@@ -5,7 +5,7 @@ stopipa()
 {
   IPACTL=/usr/sbin/ipactl
   if [ -e $IPACTL ] &&  [ -x $IPACTL ];then
-	#$IPACTL stop
+	$IPACTL stop
 	ps -eo pid,cmd | grep ipa_web    | grep -v "grep" | cut -d" " -f1 > pid.txt	
 	ps -eo pid,cmd | grep kdc        | grep -v "grep" | cut -d" " -f1 >> pid.txt
 	ps -eo pid,cmd | grep ipa_kpasswd| grep -v "grep" | cut -d" " -f1 >> pid.txt
@@ -15,58 +15,62 @@ stopipa()
 		do
 		  kill -9 $pid
 		done
-		sleep 3
 		rm pid.txt
-	else
-		echo "ipa stopped"
 	fi
+	sleep 3
+	echo "ipa stopped"
   else
 	echo "No $IPACTL found"
   fi
 }
 
 snapshot_base=$1
+snapshot_name=$2
 if [ -z $snapshot_base ];then
 	snapshot_base=/tmp/snapshot
-	echo "using $snapshot_base as snapshot directory storage"
 fi
-timestr=`date +%F_%H_%k_%M_%S`
-snapshot_dir=$snapshot_base/$timestr
+
+if [ -z $snapshot_name ];then
+        timestr=`date +%F_%H_%k_%M_%S`
+        snapshot_dir=$snapshot_base/$timestr
+else
+	snapshot_dir=$snapshot_base/$snapshot_name
+fi
+echo "using $snapshot_dir as snapshot directory storage"
+
 current_dir=`pwd`
 
 mkdir -p $snapshot_dir 
-if [ -d $snapshot_dir ];then
+if [ -d $snapshot_dir ] && [ -d /etc/dirsrv ] && [ -d /var/lib/dirsrv ] && [ -d /var/log/dirsrv ]
+then
 	echo "using [$snapshot_dir]"
-else
-	return
+	echo "step 1: stop ipa"
+	stopipa
+
+	echo "step 2: archive ipa config directory"
+	cd /etc/dirsrv
+	tar cf config.tar *
+	mv config.tar $snapshot_dir/.
+	echo "/etc/dirsrv $snapshot_dir/config.tar" >> $snapshot_dir/restore.conf
+			
+	echo "step 3: archive ipa log directory"
+	cd /var/log/dirsrv
+	tar cf log.tar *
+	mv log.tar $snapshot_dir/.
+	echo "/var/log/dirsrv $snapshot_dir/log.tar" >> $snapshot_dir/restore.conf
+
+	echo "step 4: archive ipa db directory"
+	cd /var/lib/dirsrv
+	tar cf db.tar *
+	mv db.tar $snapshot_dir/.
+	echo "/var/lib/dirsrv $snapshot_dir/db.tar" >> $snapshot_dir/restore.conf
+
+	cd $snapshot_dir
+	pwd
+	ls -lh
+	echo "content of restore.conf file:"
+	cat restore.conf
+	$IPACTL start
+	echo "done"
+	cd $current_dir
 fi
-
-echo "step 1: stop ipa"
-stopipa
-
-echo "step 2: archive ipa config directory"
-cd /etc/dirsrv
-tar cf config.tar *
-mv config.tar $snapshot_dir/.
-echo "/etc/dirsrv $snapshot_dir/config.tar" >> $snapshot_dir/restore.conf
-
-echo "step 3: archive ipa log directory"
-cd /var/log/dirsrv
-tar cf log.tar *
-mv log.tar $snapshot_dir/.
-echo "/var/log/dirsrv $snapshot_dir/log.tar" >> $snapshot_dir/restore.conf
-
-echo "step 4: archive ipa db directory"
-cd /var/lib/dirsrv
-tar cf db.tar *
-mv db.tar $snapshot_dir/.
-echo "/var/lib/dirsrv $snapshot_dir/db.tar" >> $snapshot_dir/restore.conf
-
-cd $snapshot_dir
-pwd
-ls -lh
-cat restore.conf
-
-echo "done"
-
-cd $current_dir
