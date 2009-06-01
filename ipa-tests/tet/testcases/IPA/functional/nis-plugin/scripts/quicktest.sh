187c5dd107e36f22f3f55a248e89625762a4fce7
Yi Zhang <yzhang@REDHAT.COM>
2009-06-01 23:09:34 +0000

    make quicktest.sh for quick and dirty test, the purpose is just to ensure the basic configuration works
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@502 aeb79ed8-e03d-0410-be94-af332b95afa2

---
+++ ipa-tests/tet/testcases/IPA/functional/nis-plugin/scripts/quicktest.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+server="mv32a-vm.idm.lab.bos.redhat.com"
+client="mv32b-vm.idm.lab.bos.redhat.com"
+nisdomain="idm.lab.bos.redhat.com"
+nisserver=$server
+echo "enable nis on remote ipa server : [$server]"
+./env.nis.enable.sh $server
+echo "config nis client : [$client]"
+./config.nis.client.sh $client $nisdomain $nisserver
