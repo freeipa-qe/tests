9b65ba831683a01239a4537c7392f6755873f11c
Yi Zhang <yzhang@REDHAT.COM>
2009-10-27 19:01:52 +0000

    simple perl to create ldif file for data import under dc=example,dc=com
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@649 aeb79ed8-e03d-0410-be94-af332b95afa2

---
+++ ipa-tests/qautil/ds/ldifproducer.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/perl
+# filename: ldifproducer.pl
+# used for automatic data genaration, used for sasl stress testing. 
+# notes:  users are under suffix of: dc=example,dc=com
+
+use warnings;
+use strict ;
+
+# Usage: ldifproducer.pl <output file name> <total records desired>
+# default output filename: test.ldif
+# default record number: 999
+
+# command line options
+our $total=999;
+our $output="data.$total.ldif"; 
+
+if($#ARGV == 0){
+	$output=$ARGV[0]; 
+} 
+if($#ARGV == 1){
+	$output=$ARGV[0]; 
+	$total=$ARGV[1]; 
+} 
+
+print "\nstarts... create $total entries in $output file ...";
+##########
+# GLobal #
+##########
+
+if (!(open (OUT, ">$output"))){
+	#print "can not open output file [$output]";
+	print 0;
+	exit;
+}
+our $len=length($total);
+our $root=qq(# define the top
+dn: dc=example,dc=com
+objectClass: top
+objectClass: domain
+dc: example
+
+dn: ou=people,dc=example,dc=com
+objectClass: top
+objectClass: organizationalunit
+ou: people);
+
+print OUT $root;
+
+# define entry for members in group A
+for my $i (1..$total){
+	my $id=sprintf("%0${len}d", $i);
+	#my $id="$i";
+	print OUT "\n\ndn: uid=user.".$id.",ou=people,dc=example,dc=com";
+	print OUT "\ncn: user.".$id;
+	print OUT "\nsn: ".$id;
+	print OUT "\nuid: user.".$id;
+	print OUT "\nmail: user".$id."\@example.com";
+	print OUT "\nobjectclass: top";
+	print OUT "\nobjectclass: person";
+	print OUT "\nobjectclass: inetorgperson";
+	print OUT "\nuserpassword: redhat"; 
+}
+
+close OUT;
+# the output "1" will be callected by caller
+print "Done\n";
+print 1;
+
