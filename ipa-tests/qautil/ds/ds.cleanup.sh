fb64badc3bffba3b12c693c48b6193d45557100b
Yi Zhang <yzhang@REDHAT.COM>
2010-01-13 23:38:58 +0000

    add redhat-ds-base-selinux into remove pkg list
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@670 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/qautil/ds/ds.cleanup.sh
+++ ipa-tests/qautil/ds/ds.cleanup.sh
@@ -31,14 +31,16 @@ sudo rm -Rf /var/cache/yum/*
 
 # remove rpms
 echo "=== remove rpms ==="
+rpm -qa > /tmp/rpms
 for i in `echo dirsec-nspr dirsec-nss dirsec-nss-tools svrcore mozldap6 mozldap6-tools redhat-ds-base redhat-ds-base-selinux perl-Mozilla-LDAP adminutil dirsec-jss fortitude-mod_nss redhat-ds-admin redhat-admin-console redhat-ds-console redhat-idm-console idm-console-framework ldapjdk redhat-ds mozldap mozldap-tools jss icu libicu dirsec-nspr-devel dirsec-nss-devel svrcore-devel mozldap6-devel mozldap6-tools-devel redhat-ds-base-devel perl-Mozilla-LDAP-devel adminutil-devel dirsec-jss-devel fortitude-mod_nss-devel redhat-ds-admin-devel redhat-admin-console-devel redhat-ds-console-devel redhat-idm-consol-devel idm-console-framework-devel ldapjdk-devel redhat-ds-devel mozldap-devel mozldap-tools-devel jss-devel icu-devel libicu-devel dirsec-nspr-debuginfo dirsec-nss-debuginfo svrcore-debuginfo mozldap6-debuginfo mozldap6-tools-debuginfo redhat-ds-base-debuginfo perl-Mozilla-LDAP-debuginfo adminutil-debuginfo dirsec-jss-debuginfo fortitude-mod_nss-debuginfo redhat-ds-admin-debuginfo redhat-admin-console-debuginfo redhat-ds-console-debuginfo redhat-idm-console-debuginfo idm-console-framework-debuginfo ldapjdk-debuginfo redhat-ds-debuginfo icu-debuginfo libicu-debuginfo mozldap-debuginfo mozldap-tools-debuginfo jss-debuginfo`
 do
-	if rpm -qa | grep "$i" 2>&1 1>/dev/null
+	if grep "$i" /tmp/rpms 2>&1 1>/dev/null
 	then
         	echo -e "  rpm remove: [$i]"
         	sudo rpm -e --nodeps $i 
 	fi
 done
+rm /tmp/rpms
 
 echo "=== end of ds clean up ==="
 
