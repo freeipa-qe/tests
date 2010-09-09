fd42fcc9ff50e2fb1ace3c6e8def9fa027da75fd
Jenny Galipeau <jgalipea@REDHAT.COM>
2010-09-09 19:19:35 +0000

    still more work to do and commented out kinit as admin until shared sub routine added to shared library manually kinit to run the script
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@679 aeb79ed8-e03d-0410-be94-af332b95afa2

---
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-host-cli/runtest.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+# vim: dict=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   runtest.sh of /CoreOS/ipa-tests/acceptance/ipa-host-cli
+#   Description: IPA host CLI acceptance tests
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# The following ipa cli commands needs to be tested:
+#  host-add                  Add a new host.
+#  host-del                  Delete an existing host.
+#  host-find                 Search the hosts.
+#  host-mod                  Edit an existing host.
+#  host-show                 Examine an existing host.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Author: Jenny Galipeau <jgalipea@redhat.com>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2010 Red Hat, Inc. All rights reserved.
+#
+#   This copyrighted material is made available to anyone wishing
+#   to use, modify, copy, or redistribute it subject to the terms
+#   and conditions of the GNU General Public License version 2.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free
+#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#   Boston, MA 02110-1301, USA.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Include rhts environment
+. /usr/bin/rhts-environment.sh
+. /usr/lib/beakerlib/beakerlib.sh
+. /dev/shm/ipa-host-cli-lib.sh
+. /dev/shm/ipa-server-shared.sh
+
+########################################################################
+# Test Suite Globals
+########################################################################
+ADMINID="admin"
+ADMINPWD="Admin123"
+
+REALM=`os_getdomainname | tr "[a-z]" "[A-Z]"`
+DOMAIN=`os_getdomainname`
+
+host1="nightcrawler."$DOMAIN
+host2="NIGHTCRAWLER."$DOMAIN
+host3="SHADOWFALL."$DOMAIN
+host4="shadowfall."$DOMAIN
+host5="qe-blade-01."$DOMAIN
+########################################################################
+
+PACKAGE="ipa-admintools"
+
+rlJournalStart
+    rlPhaseStartSetup "ipa-host-cli-startup: Check for admintools package and Kinit"
+        rlAssertRpm $PACKAGE
+        rlRun "TmpDir=\`mktemp -d\`" 0 "Creating tmp directory"
+        rlRun "pushd $TmpDir"
+	#rlRun "mykinit $ADMINID $ADMINPWD" 0 "Kinit as admin user"
+    rlPhaseEnd
+
+    rlPhaseStartTest "ipa-host-cli-001: Add lower case host"
+        rlRun "addHost $host1" 0 "Adding new host with ipa host-add."
+        rlRun "findHost $host1" 0 "Verifying host was added with ipa host-find lower case."
+        rlRun "findHost $host2" 0 "Verifying host was added with ipa host-find upper case."
+    rlPhaseEnd
+
+    rlPhaseStartTest "ipa-host-cli-02: Add upper case host"
+        rlRun "addHost $host3" 0 "Adding new host with ipa host-add."
+        rlRun "findHost $host3" 0 "Verifying host was added with ipa host-find lower case."
+        rlRun "findHost $host4" 0 "Verifying host was added with ipa host-find upper case."
+    rlPhaseEnd
+
+    rlPhaseStartTest "ipa-host-cli-03: Add host with dashes in hostname"
+        rlRun "addHost $host5" 0 "Adding new host with ipa host-add."
+        rlRun "findHost $host5" 0 "Verifying host was added with ipa host-find lower case."
+    rlPhaseEnd
+
+    rlPhaseStartTest "ipa-host-cli-04: Modify host location"
+	for item in $host1 $host3 $host5 ; do
+		attr="location"
+		value='IDM Westford lab 3'
+        	rlRun "modifyHost $item $attr \"${value}\"" 0 "Modifying host $item $attr."
+        	rlRun "verifyHostAttr $item $attr \"$value\"" 0 "Verifying host $attr was modified."
+	done
+    rlPhaseEnd
+
+    rlPhaseStartTest "ipa-host-cli-05: Modify host platform"
+        for item in $host1 $host3 $host5 ; do
+		attr="platform"
+                value='x86_64'
+                rlRun "modifyHost $item $attr \"$value\"" 0 "Modifying host $item $attr."
+                rlRun "verifyHostAttr $item $attr \"$value\"" 0 "Verifying host $attr was modified."
+        done
+    rlPhaseEnd
+
+    rlPhaseStartTest "ipa-host-cli-06: Modify host os"
+        for item in $host1 $host3 $host5 ; do
+		attr="os"
+                value="Fedora 11"
+                rlRun "modifyHost $item $attr \"$value\"" 0 "Modifying host $item $attr."
+                rlRun "verifyHostAttr $item $attr \"$value\"" 0 "Verifying host $attr was modified."
+        done
+    rlPhaseEnd
+
+    rlPhaseStartTest "ipa-host-cli-07: Modify host description"
+        for item in $host1 $host3 $host5 ; do
+		attr="desc"
+                value="interesting description"
+                rlRun "modifyHost $item $attr \"$value\"" 0 "Modifying host $item $attr."
+                rlRun "verifyHostAttr $item $attr \"$value\"" 0 "Verifying host $attr was modified."
+        done
+    rlPhaseEnd
+
+    rlPhaseStartTest "ipa-host-cli-08: Modify host locality"
+        for item in $host1 $host3 $host5 ; do
+                attr="locality"
+                value="Mountain View, CA"
+                rlRun "modifyHost $item $attr \"$value\"" 0 "Modifying host $item $attr."
+                rlRun "verifyHostAttr $item $attr \"$value\"" 0 "Verifying host $attr was modified."
+        done
+    rlPhaseEnd
+
+    rlPhaseStartCleanup "ipa-host-cli-cleanup: Destroying admin credentials."
+        rlRun "popd"
+        rlRun "rm -r $TmpDir" 0 "Removing tmp directory"
+	rlRun "ipa host-del \"$host1\"" 0 "Deleting host added."
+	rlRun "ipa host-del \"$host3\"" 0 "Deleting host added."
+	rlRun "ipa host-del \"$host5\"" 0 "Deleting host added."
+	#rlRun "kdestroy" 0 "Destroying admin credentials."
+    rlPhaseEnd
+rlJournalPrintText
+rlJournalEnd
