093bfbc1c1fab9e6aca22b551ada1d89566769ce
Jenny Galipeau <jgalipea@REDHAT.COM>
2010-09-09 19:26:41 +0000

    added a couple of sub routine to get domainname and nslookup
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@681 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/shared/ipa-server-shared.sh
+++ ipa-tests/beaker/ipa-server/shared/ipa-server-shared.sh
@@ -1,1 +1,49 @@
-# Nothing yet!
+#!/bin/sh
+
+########################################################################
+#  IPA SERVER SHARED LIBRARY
+#######################################################################
+# Includes:
+#       os_nslookup
+#       os_getdomainname
+######################################################################
+
+#######################################################################
+# os_nslookup Usage:
+#       os_nslookup
+#####################################################################
+os_nslookup()
+{
+h=$1
+case $ARCH in
+        "Linux "*)
+                rval=`nslookup -sil $h`
+                if [ `expr "$rval" : "server can't find"` -gt 0 ]; then
+                        tmpdn=`domainname -f`
+                        echo "Name: $tmpdn"
+                        tmpaddr=`/sbin/ifconfig -a | egrep inet | egrep -v 127.0.0.1 | egrep -v inet6 | awk '{print $2}' | awk -F: '{print $2}'`
+                        echo "Addr: $tmpaddr"
+                else
+                        nslookup -sil $h
+                fi
+                ;;
+        *)
+                nslookup $h
+                ;;
+esac
+}
+
+#######################################################################
+# os_getdomainname Usage:
+#       mydomain=`os_getdomainname`
+#####################################################################
+
+os_getdomainname()
+{
+   mydn=`hostname | nslookup 2> /dev/null | grep 'Name:' | cut -d"." -f2-`
+   if [ "$mydn" = "" ]; then
+     mydn=`hostname -f |  cut -d"." -f2-`
+   fi
+   echo "$mydn"
+}
+
