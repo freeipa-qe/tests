1e6725aa104718783bd8e6b14f4bbbee30bc254a
Jenny Galipeau <jgalipea@REDHAT.COM>
2010-09-22 14:12:25 +0000

    more sub routines added
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@725 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/shared/ipa-group-cli-lib.sh
+++ ipa-tests/beaker/ipa-server/shared/ipa-group-cli-lib.sh
@@ -5,10 +5,15 @@
 #######################################################################
 # Includes:
 #       addGroup
-#       finGroup
-#       modGroup
+#	addPosixGroup
+#       findGroup
+#       modifyGroup
 #       verifyGroupAttr
 #       verifyGroupClasses
+#	addGroupMembers
+#	verifyGroupMembers
+#	verifyUserMembers
+#	detachGroup
 #       deleteGroup
 ######################################################################
 # Assumes:
@@ -28,8 +33,8 @@ addGroup()
    description=$1
    groupname=$2
    rc=0
-
-        ipa host-group "$description" "$groupname"
+	rlLog "Executing: ipa group-add --desc=\"$description\" \"$groupname\""
+        ipa group-add --desc="$description" $groupname
         rc=$?
         if [ $rc -ne 0 ] ; then
                 rlLog "Adding new group \"$groupname\" failed."
@@ -41,6 +46,30 @@ addGroup()
 }
 
 #######################################################################
+# addPosixGroup Usage:
+#       addPosixGroup <description> <groupname>
+# Example:
+#       addPosix Group "Idenity Management Quality Engineering" "IDM QE"
+######################################################################
+addPosixGroup()
+{
+   description=$1
+   groupname=$2
+   rc=0
+
+        ipa group-add --posix --desc="$description" $groupname
+        rc=$?
+        if [ $rc -ne 0 ] ; then
+                rlLog "Adding new posix group \"$groupname\" failed."
+        else
+                rlLog "Adding new posix group \"$groupname\" successful."
+        fi
+
+   return $rc
+}
+
+
+#######################################################################
 # findGroup Usage:
 #       findGroup <groupname>
 # example:
@@ -50,11 +79,11 @@ addGroup()
 findGroup()
 {
    mygroup=$1
-   ipa host-group $mygroup
+   ipa group-find $mygroup
    rc=$?
    if [ $rc -eq 0 ] ; then
         rlLog "$mygroup was found."
-        # check hostgroup
+        # check group
         result=`ipa group-find $mygroup`
         check=`echo $mygroup | tr "[A-Z]" "[a-z]"`
         echo $result | grep "Group name: $check"
@@ -88,7 +117,8 @@ modifyGroup()
    value=$3
    rc=0
 
-   ipa group-mod --$attribute="${value}" $mygroup
+   rlLog "Executing: ipa group-mod --$attribute=\"$value\" $mygroup"
+   ipa group-mod --$attribute="$value" $mygroup
    rc=$?
    if [ $rc -ne 0 ] ; then
         rlLog "ERROR: Modifying group $mygroup failed."
@@ -124,7 +154,7 @@ verifyGroupAttr()
         if [ $rc -ne 0 ] ; then
                 rlLog "$mygroup verification failed: Value of $attribute is $value."
         else
-                rlLog "Value of $attribute for $mygroup is as expected."
+                rlLog "Value of $attribute for $mygroup is as expected: $value."
         fi
    else
         rlLog "ERROR: ipa group-show command failed. Return code: $rc"
@@ -135,15 +165,15 @@ verifyGroupAttr()
 
 #######################################################################
 # verifyGroupClasses Usage:
-#       verifyGroupClasses groupname <yes or no>
+#       verifyGroupClasses groupname <ipa or upg or posix>
 # example:
-#       verifyGroupClasses test no
+#       verifyGroupClasses test ipa
 ######################################################################
 
 verifyGroupClasses()
 {
    mygroup=$1
-   private=$2
+   grptype=$2
    tmpfile=/tmp/groupshow.out
    rc=0
 
@@ -152,7 +182,8 @@ verifyGroupClasses()
    if [ $rc -eq 0 ] ; then
 	classes=`cat $tmpfile | grep objectclass`
 	rlLog "DEBUG: $classes"
-	if [ $private == "no" ] ; then
+	if [ $grptype == "ipa" ] ; then
+		rlLog "Group type is ipa"
 		for item in top groupofnames nestedgroup ipausergroup ipaobject ; do
                 	echo $classes | grep "$item"
                 	if [ $? -ne 0 ] ; then
@@ -162,7 +193,8 @@ verifyGroupClasses()
                         	rlLog "objectclass $item was returned as expected with group-show --all"
                 	fi
         	done
-	else
+	elif [ $grptype == "upg" ] ; then
+		rlLog "Group type is user private"
                 for item in posixGroup mepManagedEntry top ; do
                         echo $classes | grep "$item"
                         if [ $? -ne 0 ] ; then
@@ -172,11 +204,104 @@ verifyGroupClasses()
                                 rlLog "objectclass $item was returned as expected with group-show --all"
                         fi
                 done	
+	elif [ $grptype == "posix" ] ; then
+		rlLog "Group type is posix"
+		for item in top groupofnames nestedgroup ipausergroup ipaobject posixgroup ; do
+                        echo $classes | grep "$item"
+                        if [ $? -ne 0 ] ; then
+                                rlLog "ERROR - objectclass $item was not returned with group-show --all"
+                                rc=1
+                        else
+                                rlLog "objectclass $item was returned as expected with group-show --all"
+                        fi
+		done
+	else
+		rlLog "ERROR: Unknown Group Type."
+		rc=1
 	fi
    else
         rlLog "ERROR: Show group failed."
    fi
 
+   return $rc
+}
+
+#######################################################################
+# addGroupMembers Usage:
+#       addGroupMembers <groups or users> <comma_separated_list_of_groups> groupname
+# example:
+#       addGroupMembers groups "animalkingdom,epcot" disneyworld
+######################################################################
+
+addGroupMembers()
+{
+  membertype=$1
+  memberlist=$2
+  mygroup=$3
+  rc=0
+
+  flag="--$membertype"
+
+  rlLog "Executing: ipa group-add-member $flag=\"$memberlist\" $mygroup"
+  ipa group-add-member $flag="$memberlist" $mygroup
+  rc=$?
+  if [ $rc -ne 0 ] ; then
+        rlLog "ERROR: Adding $membertype to group $mygroup failed."
+  else
+        rlLog "Adding $membertype \"$memberlist\" to group $mygroup successful."
+  fi
+
+  return $rc
+}
+
+#######################################################################
+# removeGroupMembers Usage:
+#       removeGroupMembers <groups or users> <comma_separated_list_of_groups> groupname
+# example:
+#       removeGroupMembers groups "animalkingdom,epcot" disneyworld
+######################################################################
+
+removeGroupMembers()
+{
+  membertype=$1
+  memberlist=$2
+  mygroup=$3
+  rc=0
+
+  flag="--$membertype"
+
+  rlLog "Executing: ipa group-remove-member $flag=\"$memberlist\" $mygroup"
+  ipa group-remove-member $flag="$memberlist" $mygroup
+  rc=$?
+  if [ $rc -ne 0 ] ; then
+        rlLog "ERROR: Removing $membertype from group $mygroup failed."
+  else
+        rlLog "Removing $membertype \"$memberlist\" from group $mygroup successful."
+  fi
+
+  return $rc
+}
+
+
+#######################################################################
+# detachUPG Usage:
+#       detachUPG userprivategroupname
+# example:
+#       deleteUPG test
+######################################################################
+
+detachUPG()
+{
+   upgroup=$1
+   rc=0
+
+   ipa group-detach $upgroup
+   rc=$?
+   if [ $rc -ne 0 ] ; then
+        rlLog "ERROR: Detaching user private group $upgroup failed."
+   else
+        rlLog "User Private Group $upgroup detached successfully."
+   fi
    return $rc
 }
 
