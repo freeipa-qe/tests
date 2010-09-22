8ac21a44f583afecd80d06116f34bcfb22308e5b
Jenny Galipeau <jgalipea@REDHAT.COM>
2010-09-22 20:25:43 +0000

    checking in
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@727 aeb79ed8-e03d-0410-be94-af332b95afa2

---
+++ ipa-tests/beaker/ipa-server/shared/runtest.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# vim: dict=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   runtest.sh of /CoreOS/ipa-tests/shared
+#   Description: IPA shared libraries
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Libraries Included:
+#	ipa-server-shared.sh
+#	ipa-host-cli-lib.sh
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Author: Jenny Galipeau <jgalipea@redhat.com>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2010 Red Hat, Inc. All rights reserved.
+#
+#   This copyrighted material is made available to anyone wishing
+#   to use, modify, copy, or redistribute it subject to the terms
+#   and conditions of the GNU General Public License version 2.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free
+#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#   Boston, MA 02110-1301, USA.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Include rhts environment
+. /usr/bin/rhts-environment.sh
+. /usr/lib/beakerlib/beakerlib.sh
+
+rlJournalStart
+    rlPhaseStartSetup "Copy Shared Libraries to /dev/shm"
+	rlRun "cp ipa-server-share.sh /dev/shm/" 0 "Copying ipa-server-shared.sh"
+	rlRun "cp ipa-host-cli-lib.sh /dev/shm/" 0 "Copying ipa-server-cli-lib.sh"
+	rlRun "cp ipa-host-cli-lib.sh /dev/shm/" 0 "Copying ipa-group-cli-lib.sh"
+    rlPhaseEnd
+
+    rlPhaseStartTest "Verify shared libraries are available"
+	rlRun "ls /dev/shm/ipa-server-share.sh" 0 "Verifying ipa-server-shared.sh"
+	rlRun "ls /dev/shm/ipa-host-cli-lib.sh" 0 "Verifying ipa-host-cli-lib.sh"
+	rlRun "ls /dev/shm/ipa-host-cli-lib.sh" 0 "Verifying ipa-group-cli-lib.sh"
+    rlPhaseEnd
+
+rlJournalPrintText
+rlJournalEnd
