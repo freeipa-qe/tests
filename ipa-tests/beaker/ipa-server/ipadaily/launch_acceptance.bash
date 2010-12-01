28f0b229d41d5c5665dad84c99eb53c5dd2f6145
Michael Gregg <mgregg@REDHAT.COM>
2010-12-01 02:10:46 +0000

    added new packages to testsuites line
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@930 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/ipadaily/launch_acceptance.bash
+++ ipa-tests/beaker/ipa-server/ipadaily/launch_acceptance.bash
@@ -7,7 +7,7 @@ rm -rf $PWD/beaker
 SHAREDLIB=$PWD/beaker/ipa-server/shared
 TESTROOT=$PWD/beaker/ipa-server/acceptance
 REPOCLONE=$PWD/beaker/ipa-server/repoclone
-DAILY=$PWD/beaker/ipa-server/daily
+DAILY=$PWD/beaker/ipa-server/ipadaily
 # check out the lastest
 svn co https://svn.devel.redhat.com/repos/ipa-tests/trunk/ipa-tests/beaker
 
@@ -22,7 +22,7 @@ rm -rf *.rpm
 make rpm
 bkr task-add *.rpm
 
-testsuites="ipa-user-cli ipa-group-cli ipa-host-cli ipa-hostgroup-cli ipa-netgroups-cli install ipa-default ipa-password ipa-hbac-cli install-slave install-client uninstall"
+testsuites="ipa-user-cli ipa-group-cli ipa-host-cli ipa-hostgroup-cli ipa-netgroup-cli ipa-set-add-attr nis-cli install ipa-default ipa-password ipa-hbac-cli ipa-hbacsvc-cli ipa-config ipa-dns install-slave install-client uninstall"
 for item in $testsuites ; do
 	cd $TESTROOT/$item/
 	rm -rf *.rpm
