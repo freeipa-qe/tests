dabe1135f51b70d4f0e14b6e4bee57e66ff08e97
Michael Gregg <mgregg@REDHAT.COM>
2011-01-18 02:06:16 +0000

    more additions to the dns test
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1053 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-dns/runtest.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-dns/runtest.sh
@@ -183,6 +183,55 @@ fi
 		rlRun "ipa dnszone-find $zone | grep $ttl" 0 "checking to ensure that the new zone got created with the correct ttl"
 	rlPhaseEnd
 
+	a="1.2.3.4"
+	a2="1.2.3.4,2.3.4.5"
+	aaaa="fec0:0:a10:6000:10:16ff:fe98:193"
+	afsdb="green.femto.edu."
+	
+	  --a-rec=LIST          comma-separated list of A records
+  --aaaa-rec=LIST       comma-separated list of AAAA records
+  --a6-rec=LIST         comma-separated list of A6 records
+  --afsdb-rec=LIST      comma-separated list of AFSDB records
+  --apl-rec=LIST        comma-separated list of APL records
+  --cert-rec=LIST       comma-separated list of CERT records
+  --cname-rec=LIST      comma-separated list of CNAME records
+  --dhcid-rec=LIST      comma-separated list of DHCID records
+  --dlv-rec=LIST        comma-separated list of DLV records
+  --dname-rec=LIST      comma-separated list of DNAME records
+  --dnskey-rec=LIST     comma-separated list of DNSKEY records
+  --ds-rec=LIST         comma-separated list of DS records
+  --hinfo-rec=LIST      comma-separated list of HINFO records
+  --hip-rec=LIST        comma-separated list of HIP records
+  --ipseckey-rec=LIST   comma-separated list of IPSECKEY records
+  --key-rec=LIST        comma-separated list of KEY records
+  --kx-rec=LIST         comma-separated list of KX records
+  --loc-rec=LIST        comma-separated list of LOC records
+  --md-rec=LIST         comma-separated list of MD records
+  --minfo-rec=LIST      comma-separated list of MINFO records
+  --mx-rec=LIST         comma-separated list of MX records
+  --naptr-rec=LIST      comma-separated list of NAPTR records
+  --ns-rec=LIST         comma-separated list of NS records
+  --nsec-rec=LIST       comma-separated list of NSEC records
+  --nsec3-rec=LIST      comma-separated list of NSEC3 records
+  --nsec3param-rec=LIST
+                        comma-separated list of NSEC3PARAM records
+  --nxt-rec=LIST        comma-separated list of NXT records
+  --ptr-rec=LIST        comma-separated list of PTR records
+  --rrsig-rec=LIST      comma-separated list of RRSIG records
+  --rp-rec=LIST         comma-separated list of RP records
+  --sig-rec=LIST        comma-separated list of SIG records
+  --spf-rec=LIST        comma-separated list of SPF records
+  --srv-rec=LIST        comma-separated list of SRV records
+  --sshfp-rec=LIST      comma-separated list of SSHFP records
+  --ta-rec=LIST         comma-separated list of TA records
+  --tkey-rec=LIST       comma-separated list of TKEY records
+  --tsig-rec=LIST       comma-separated list of TSIG records
+  --txt-rec=LIST        comma-separated list of TXT records
+
+	rlPhaseStartTest "ipa-dns-22: add record type A with one record"
+		rlRun "ipa drecord-add " 0 "add record type"
+	rlPhaseEnd
+
     makereport
 rlJournalEnd
 
