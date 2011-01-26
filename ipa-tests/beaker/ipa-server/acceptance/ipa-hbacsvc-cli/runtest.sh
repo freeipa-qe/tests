c9fcc00353bd127398603a6923fa86de1af6545e
Jenny Galipeau <jgalipea@REDHAT.COM>
2011-01-26 20:56:27 +0000

    changing how to check for ipa-admintools package cuz of rename
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1104 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-hbacsvc-cli/runtest.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-hbacsvc-cli/runtest.sh
@@ -62,7 +62,12 @@ PACKAGE="ipa-admintools"
 
 rlJournalStart
     rlPhaseStartSetup "ipa-hbacsvc-cli-startup: Check for admintools package and Kinit"
-        rlAssertRpm $PACKAGE
+        rpm -qa | grep $PACKAGE
+        if [ $? -eq 0 ] ; then
+                rlPass "ipa-admintools package is installed"
+        else
+                rlFail "ipa-admintools package NOT found!"
+        fi
         rlRun "TmpDir=\`mktemp -d\`" 0 "Creating tmp directory"
         rlRun "pushd $TmpDir"
 	rlRun "kinitAs $ADMINID $ADMINPW" 0 "Kinit as admin user"
