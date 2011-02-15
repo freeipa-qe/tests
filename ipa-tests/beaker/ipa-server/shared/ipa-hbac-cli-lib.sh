562086ded6d05863522ea040b92f3dd165630ab3
Jenny Galipeau <jgalipea@REDHAT.COM>
2011-02-15 13:13:51 +0000

    changes in the location of the hbac objects in the directory server
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1194 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/shared/ipa-hbac-cli-lib.sh
+++ ipa-tests/beaker/ipa-server/shared/ipa-hbac-cli-lib.sh
@@ -665,12 +665,12 @@ verifyHBACGroupMember()
 
   # construct memberDN
   memberCN="cn=$member"
-  memberDN="$memberCN,cn=hbacservices,cn=hbac,dc=$RELM"
+  memberDN="$memberCN,cn=hbacservices,cn=hbac,cn=accounts,dc=$RELM"
   rlLog "Verifying Service: $memberDN"
 
   # construct groupDN
   mygroupCN="cn=$mygroup"
-  groupDN="$mygroupCN,cn=hbacservicegroups,cn=hbac,dc=$RELM"
+  groupDN="$mygroupCN,cn=hbacservicegroups,cn=hbac,cn=accounts,dc=$RELM"
   rlLog "Verifying Service Group: $groupDN"
 
   # verify member attribute for group
