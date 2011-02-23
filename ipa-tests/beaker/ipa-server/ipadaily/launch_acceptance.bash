beec1a2d51b500ca2ca846b651e81f46e5578f64
Michael Gregg <mgregg@REDHAT.COM>
2011-02-23 00:34:42 +0000

    adding some logging and syncing to launch_acceptance.bash
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1232 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/ipadaily/launch_acceptance.bash
+++ ipa-tests/beaker/ipa-server/ipadaily/launch_acceptance.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+scriptname="launch_acceptance.bash"
+
+logger -t $scriptname "starting"
+
 # remove existing automation scripts
 HOME=$PWD
 rm -rf $PWD/beaker
@@ -11,14 +15,27 @@ DAILY=$PWD/beaker/ipa-server/ipadaily
 # check out the lastest
 svn co https://svn.devel.redhat.com/repos/ipa-tests/trunk/ipa-tests/beaker
 
-# submit beaker jobs
-cd $DAILY
-ls *.xml | while read x; do
-	bkr job-submit $x
-done
+# Make sure that this script is the updated script.
+diff $DAILY/$scriptname /root/$scriptname
+if [ $? -eq 0 ]; then
+	# The current script is the same as the one in svn
+	# submit beaker jobs
+	cd $DAILY
+	ls *.xml | while read x; do
+		logger -t $scriptname "running bkr job-submit $x"
+		bkr job-submit $x
+	done
 
-cd $REPOCLONE
-ls *.xml | while read x; do
-	bkr job-submit $x
-done
+	cd $REPOCLONE
+	ls *.xml | while read x; do
+		logger -t $scriptname "running bkr job-submit $x"
+		bkr job-submit $x
+	done
+else
+	logger -t $scriptname "/root/ differs from subversion, syncing up."
+	cat $DAILY/$scriptname > /root/$scriptname
+	chmod 755 /root/$scriptname
+	bash /root/$scriptname
+fi
 
+logger -t $scriptname "done"
