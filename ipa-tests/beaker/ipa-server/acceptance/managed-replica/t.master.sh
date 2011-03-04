9b7d959909d7f8237e731048fcbae1f0deb19c15
Michael Gregg <mgregg@REDHAT.COM>
2011-03-04 23:57:02 +0000

    adding server test script
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1262 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/managed-replica/t.master.sh
+++ ipa-tests/beaker/ipa-server/acceptance/managed-replica/t.master.sh
@@ -5,4 +5,11 @@ run_master_tests()
 {
 	rlPass "this is the master server managed replica test suite"
 	rlRun check_list 0 "checking to ensure that all machines are viewable by replica manage" 
+
+	# Disconnect from a slave server
+	serverfull=$(echo $SLAVE | cut -d\  -f1)	
+	serversimple=$(echo $serverfull | cut -d\  -f1)
+	server=$(echo serversimple | cut -d\. -f1)
+	echo "Server I am going to disconnect is: $server"
+	
 }
