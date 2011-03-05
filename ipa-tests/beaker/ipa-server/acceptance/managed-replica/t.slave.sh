f025f38989803be58c1bcecbf3f5e332be3a2f23
Michael Gregg <mgregg@REDHAT.COM>
2011-03-05 00:21:26 +0000

    checking in fixed slave tests
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1265 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/managed-replica/t.slave.sh
+++ ipa-tests/beaker/ipa-server/acceptance/managed-replica/t.slave.sh
@@ -1,7 +1,7 @@
 . ./t.tests.common.sh
 
 # Main test case
-run_master_tests()
+run_slave_tests()
 {
 	rlPass "this is the slave server managed replica test suite"
 	rlRun check_list 0 "checking to ensure that all machines are viewable by replica manage" 
