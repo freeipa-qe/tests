a9a4fe78211b5962c55b7d18bf6c821ce34b5ca6
Michael Gregg <mgregg@REDHAT.COM>
2011-03-05 00:19:51 +0000

    checking in managed replica tests
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1264 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/managed-replica/t.master.sh
+++ ipa-tests/beaker/ipa-server/acceptance/managed-replica/t.master.sh
@@ -7,10 +7,29 @@ run_master_tests()
 	rlRun check_list 0 "checking to ensure that all machines are viewable by replica manage" 
 
 	# Disconnect from a slave server
-	serverfull=$(echo $SLAVE | cut -d\  -f1)	
+	serverfull=$(echo $SLAVE | cut -d\  -f1)
 	serveralone=$(echo $serverfull | cut -d\  -f1)
-	serversimple=$(echo serversimple | cut -d\. -f1)
-	server=$(host $server) # This should be the servername in the servername.relm format
+	serversimple=$(echo $serveralone | cut -d\. -f1)
+	server=$(host $serversimple) # This should be the servername in the servername.relm format
 	echo "Server I am going to disconnect is: $server"
-	
+
+	rlRun "ipa-replica-manage -p $ADMINPW re-initialize $server" 0 "reinitilizing server $server."
+
+	rlRun "ipa-replica-manage -p $ADMINPW disconnect $server" 0 "disconnecting server $server from the replication list"
+
+	rlRun "ipa-replica-manage -p $ADMINPW list | grep $server" 1 "Server $server should not be in the list"
+
+	rlRun "ipa-replica-manage -p $ADMINPW connect $server" 0 "connecting to server $server again"
+
+	rlRun "ipa-replica-manage -p $ADMINPW list | grep $server" 0 "Server $server should be in the list"
+
+	rlRun "ipa-replica-manage -p $ADMINPW del $server" 0 "connecting to server $server again"
+
+	rlRun "ipa-replica-manage -p $ADMINPW list | grep $server" 1 "Server $server should not be in the list"
+
+	rlRun "ipa-replica-manage -p $ADMINPW connect $server" 0 "connecting to server $server again"
+
+	rlRun "ipa-replica-manage -p $ADMINPW list | grep $server" 0 "Server $server should be in the list"
+
 }
+
