9d7fe9a68c1f1b9cb94b5a4d9cd127df8525d728
Michael Gregg <mgregg@REDHAT.COM>
2011-03-15 19:16:39 +0000

    finishing commit of add and remove users section for i18n tests
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1311 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-i18n/runtest.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-i18n/runtest.sh
@@ -60,17 +60,21 @@ rlJournalStart
     rlPhaseEnd
 
 # Add all of the users to be used in this test
-rlPhaseStartTest "Adding $uname1"
-	echo running "ipa user-add --first=\'$name1f\' --last=\'$name1l\' $uname1"
-	rlRun "ipa user-add --first=\'$name1f\' --last=\'$name1l\' $uname1" 0 "Adding username $uname1 with full name $name1"
+rlPhaseStartTest "Adding usernames"
+#	echo running "ipa user-add --first='$name1f' --last='$name1l' $uname1"
+	rlRun "ipa user-add --first='$name1f' --last='$name1l' $uname1" 0 "Adding username $uname1 with full name $name1"
+	rlRun "ipa user-add --first='$name2f' --last='$name2l' $uname2" 0 "Adding username $uname2 with full name $name2"
+	rlRun "ipa user-add --first='$name3f' --last='$name3l' $uname3" 0 "Adding username $uname3 with full name $name3"
+	rlRun "ipa user-add --first='$name4f' --last='$name4l' $uname4" 0 "Adding username $uname4 with full name $name4"
 rlPhaseEnd
 
 rlPhaseStartTest "checking to ensuer that $uname1 was added correctly"
-	echo "running ipa user-find $uname1 | grep \'$name1\'"
-	rlRun "ipa user-find $uname1 | grep \'$name1\'" 0 "Checking to ensure that $uname1 has the full name of $name1"
+#	echo "running ipa user-find $uname1 | grep '$name1'"
+	rlRun "ipa user-find $uname1 | grep '$name1'" 0 "Checking to ensure that $uname1 has the full name of $name1"
+	rlRun "ipa user-find $uname2 | grep '$name2'" 0 "Checking to ensure that $uname2 has the full name of $name2"
+	rlRun "ipa user-find $uname3 | grep '$name3'" 0 "Checking to ensure that $uname3 has the full name of $name3"
+	rlRun "ipa user-find $uname4 | grep '$name4'" 0 "Checking to ensure that $uname4 has the full name of $name4"
 rlPhaseEnd
-
-ipa user-find $uname1
 
     # r2d2_test_starts
 #    ipa-i18n
@@ -78,7 +82,13 @@ ipa user-find $uname1
 
     rlPhaseStartCleanup "ipa-i18n cleanup"
 	rlRun "ipa user-del $uname1" 0 "Removing $uname1" 
+	rlRun "ipa user-del $uname2" 0 "Removing $uname2" 
+	rlRun "ipa user-del $uname3" 0 "Removing $uname3" 
+	rlRun "ipa user-del $uname4" 0 "Removing $uname4" 
 	rlRun "ipa user-find $uname1 | grep $uname1" 1 "Confirming remove of $uname1" 
+	rlRun "ipa user-find $uname2 | grep $uname2" 1 "Confirming remove of $uname2" 
+	rlRun "ipa user-find $uname3 | grep $uname3" 1 "Confirming remove of $uname3" 
+	rlRun "ipa user-find $uname4 | grep $uname4" 1 "Confirming remove of $uname4" 
         rlRun "popd"
         rlRun "rm -r $TmpDir" 0 "Removing tmp directory"
     rlPhaseEnd
