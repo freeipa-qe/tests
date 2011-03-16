c29eccd2adf1a1201ea3ae653fb0c821b1ef190f
Michael Gregg <mgregg@REDHAT.COM>
2011-03-16 00:40:23 +0000

    adding i18n tests to nightly rebuild script
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1329 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/ipadaily/rebuild-tests.bash
+++ ipa-tests/beaker/ipa-server/ipadaily/rebuild-tests.bash
@@ -30,7 +30,7 @@ if [ $? -eq 0 ]; then
 	make rpm
 	bkr task-add *.rpm
 
-	testsuites="install         ipa-cert    ipa-default     ipa-functional-services  ipa-hbac-cli     ipa-hostgroup-cli  ipa-password install-client  ipa-config  ipa-delegation  ipa-get-rm-keytab        ipa-hbacsvc-cli  ipa-krbtpolicy     ipa-services      nis-cli install-slave   ipa-ctl     ipa-dns         ipa-group-cli            ipa-host-cli     ipa-netgroup-cli   ipa-set-add-attr  uninstall ipa-user-cli/adduser ipa-user-cli/moduser"
+	testsuites="install         ipa-cert    ipa-default     ipa-functional-services  ipa-hbac-cli     ipa-hostgroup-cli  ipa-password install-client  ipa-config  ipa-delegation  ipa-get-rm-keytab        ipa-hbacsvc-cli  ipa-krbtpolicy     ipa-services      nis-cli install-slave   ipa-ctl     ipa-dns         ipa-group-cli            ipa-host-cli     ipa-netgroup-cli   ipa-set-add-attr  uninstall ipa-user-cli/adduser ipa-user-cli/moduser ipa-i18n"
 	for item in $testsuites ; do
 		cd $TESTROOT/$item/
 		rm -rf *.rpm
