57b4a74a617b9519a79191cc05706de2291068aa
Michael Gregg <mgregg@REDHAT.COM>
2011-03-30 23:51:37 +0000

    adding a bit of debugging to the first test
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1428 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/replication/t.replication.sh
+++ ipa-tests/beaker/ipa-server/acceptance/replication/t.replication.sh
@@ -6,8 +6,10 @@ hdir=/var/www/html
 hostname=`hostname -s`
 echo $MASTER | grep $hostname
 if [ $? -eq 0 ]; then
+	echo "this is a MASTER"
 	export master=1
 else
+	echo "This is a SLAVE"
 	export master=0
 fi
 
@@ -15,15 +17,15 @@ replication()
 {
 	user1=u34va
 	rlPhaseStartTest "ipa-replication-1: check basic user-add replication"
-		$hfile="ipa-replication-1"
+		hfile="ipa-replication-1"
 		if [ $master -eq 1 ]; then 
 			rlRun "ipa user-add --first=f --last=l $user1" 0 "Create a user on the master"
 			# Populate the notification file to let the slave know to proceed
-			touch $hdir/$hfile
+			touch $hdir/rt/$hfile
 		else # This is a slave
 			done=0
 			while [ $done -eq 0 ]; do
-				wget http://$MASTER/$hfile
+				wget http://$MASTER/rt/$hfile
 				if [ $? -eq 0 ]; then # if successfully downloaded file, then break out of while loop
 					done=1
 				fi
