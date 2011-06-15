90062d1d4d55df438e920b5c50ee739c196e4d15
Namita Soman <nsoman@REDHAT.COM>
2011-06-15 17:18:31 +0000

    UIElements update
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1597 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/selenium/ipa/src/com/redhat/qe/ipa21/tasks/GroupTasks.java
+++ ipa-tests/selenium/ipa/src/com/redhat/qe/ipa21/tasks/GroupTasks.java
@@ -8,16 +8,13 @@ public class GroupTasks {
 	
 	private static com.redhat.qe.ipa21.locators.UIElements UI = new UIElements();
 	
-	
-	public static Element groupNameInput = new Element ("cn");
-	public static Element descriptionInput = new Element ("description");
-	public static Element addButton = new Element("//button[@type='button']");
+		
 	
 	public static void createGroup(ExtendedSelenium selenium, String groupName, String description) {
 		selenium.click(new Element(UI.link,"Add"));
-		selenium.type(groupNameInput, groupName);
-		selenium.type(descriptionInput, description);
-		selenium.click(addButton);	
+		selenium.type(UI.groupNameInput, groupName);
+		selenium.type(UI.descriptionInput, description);
+		selenium.click(UI.addButton);	
 	}
 
 }
