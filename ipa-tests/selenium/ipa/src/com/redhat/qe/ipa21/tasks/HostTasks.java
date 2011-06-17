2228abb5bc04d7e5d2a92cca6dfd1b1be339641c
Jenny Galipeau <jgalipea@REDHAT.COM>
2011-06-17 20:49:20 +0000

    checking in changes for the weekend
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1608 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/selenium/ipa/src/com/redhat/qe/ipa21/tasks/HostTasks.java
+++ ipa-tests/selenium/ipa/src/com/redhat/qe/ipa21/tasks/HostTasks.java
@@ -3,21 +3,31 @@ package com.redhat.qe.ipa21.tasks;
 import com.redhat.qe.auto.selenium.Element;
 import com.redhat.qe.auto.selenium.ExtendedSelenium;
 import com.redhat.qe.ipa21.locators.UIElements;
+import com.redhat.qe.ipa21.locators.HostUIElements;
 
 public class HostTasks {
 	
-	private static com.redhat.qe.ipa21.locators.UIElements UI = new UIElements();
+	private static com.redhat.qe.ipa21.locators.HostUIElements HostUI = new HostUIElements();
 	
 	public static void forceAddHost(ExtendedSelenium selenium, String hostName) {
-		selenium.click(new Element(UI.link,"Add"));
-		selenium.type(UI.hostNameInput, hostName);
-		selenium.click(UI.forceFlag);
-		selenium.click(UI.addButton);	
+		selenium.click(new Element(HostUI.link,"Add"));
+		selenium.type(HostUI.hostNameInput, hostName);
+		selenium.click(HostUI.forceFlag);
+		selenium.click(HostUI.addButton);	
+	}
+	
+	public static void modifyHost(ExtendedSelenium selenium, String hostName, String hostDescription) {
+		selenium.click(new Element(HostUI.link,hostName));
+		selenium.type(HostUI.hostDescriptionInput, hostDescription);
+		selenium.click(new Element(HostUI.link,"Update"));
+		selenium.click(new Element(HostUI.hostUpdateLink));	
 	}
 	
 	
 	public static void deleteHost(ExtendedSelenium selenium, String hostName) {
-		selenium.getAllFields();
+		selenium.click("css=.entity[name='host'] .facet[name='search'] input[value='hostName']");
+		selenium.click("link=Delete");
+		selenium.click("//button[@type='button']");
 	}
 
 }
