a827d82a511c802a38fc5316c634e01c68b6b53f
Namita Soman <nsoman@REDHAT.COM>
2011-06-20 12:06:35 +0000

    User UI elements
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1613 aeb79ed8-e03d-0410-be94-af332b95afa2

---
+++ ipa-tests/selenium/ipa/src/com/redhat/qe/ipa21/locators/UserUIElements.java
@@ -0,0 +1,42 @@
+package com.redhat.qe.ipa21.locators;
+
+import com.redhat.qe.auto.selenium.Element;
+import com.redhat.qe.auto.selenium.ExtendedSelenium;
+import com.redhat.qe.auto.selenium.LocatorStrategy;
+import com.redhat.qe.auto.selenium.StringSandwichLocatorStrategy;
+import com.redhat.qe.ipa.base.SeleniumTestScript;
+
+/**
+ * A container for User UI locators that are common to more than one page in the 
+ * IPA application. 
+ * 
+ * @author nkrishnan
+ *
+ */
+
+public class UserUIElements {
+    public UserUIElements(){
+		
+	}
+
+	public ExtendedSelenium sel(){
+		return SeleniumTestScript.selenium;
+	}
+	
+	//Identity - User - Add
+	public Element userNameInput = new Element ("uid");
+	public Element givennameInput = new Element ("givenname");
+	public Element snInput = new Element ("sn");
+	
+	//Identity - User - Edit
+	public Element titleInput = new Element ("title");
+	public Element mailInput = new Element ("mail");
+	public Element undoInput = new Element ("undo");
+	public LocatorStrategy userMailLink = new StringSandwichLocatorStrategy("userMailLink", "css=span[name=mail] a[name=add]");
+	public LocatorStrategy updateLink = new StringSandwichLocatorStrategy("updateLink", "css=span.input_link");
+	
+	//Identity - User - Delete
+	//TODO: username is hardcoded here....to test delete temporarily
+	public Element testuserDeleteLink = new Element("css=.entity[name='user'] .facet[name='search'] input[value='testuser']");	
+
+}
