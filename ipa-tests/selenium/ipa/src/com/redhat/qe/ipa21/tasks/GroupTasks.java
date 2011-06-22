802902afd0e17714d878a630443940b8d73c8329
Yi Zhang <yzhang@REDHAT.COM>
2011-06-22 20:45:53 +0000

    add deleteGroup function
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1618 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/selenium/ipa/src/com/redhat/qe/ipa21/tasks/GroupTasks.java
+++ ipa-tests/selenium/ipa/src/com/redhat/qe/ipa21/tasks/GroupTasks.java
@@ -2,19 +2,25 @@ package com.redhat.qe.ipa21.tasks;
 
 import com.redhat.qe.auto.selenium.Element;
 import com.redhat.qe.auto.selenium.ExtendedSelenium;
+import com.redhat.qe.ipa21.locators.GroupUIElements;
 import com.redhat.qe.ipa21.locators.UIElements;
 
 public class GroupTasks {
-	
-	private static com.redhat.qe.ipa21.locators.UIElements UI = new UIElements();
-	
-		
+	 
+	private static com.redhat.qe.ipa21.locators.GroupUIElements GroupUI = new GroupUIElements();
+
 	
 	public static void createGroup(ExtendedSelenium selenium, String groupName, String description) {
-		selenium.click(new Element(UI.link,"Add"));
-		selenium.type(UI.groupNameInput, groupName);
-		selenium.type(UI.groupDescriptionInput, description);
-		selenium.click(UI.addButton);	
+		selenium.click(new Element(GroupUI.link,"Add"));
+		selenium.type(GroupUI.groupNameInput, groupName);
+		selenium.type(GroupUI.groupDescriptionInput, description);
+		selenium.click(GroupUI.addButton);	
 	}
 
-}
+	public static void deleteGroup(ExtendedSelenium selenium, String groupName) {
+		selenium.click("//input[@name='select' and @value='" + groupName + "']");
+		selenium.click("link=Delete"); 
+		selenium.click(GroupUI.deleteButton);	
+	}
+	
+}//class GroupTasks
