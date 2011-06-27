3a71d7d2907d7032c3b089589086bf815c37cd51
Jenny Galipeau <jgalipea@REDHAT.COM>
2011-06-27 14:58:08 +0000

    fixing shell script error uninary operator expected
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1636 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-services/t.ipa-services.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-services/t.ipa-services.sh
@@ -522,7 +522,7 @@ rlPhaseStartTest "service_find_004: ipa service-find with --not-man-by-host opti
         rlRun "ipa service-add-host --hosts=$TESTHOST $SERVICE/$HOSTNAME@$RELM > $TmpDir/service_find_004.out 2>&1"
         rlRun "ipa service-find --not-man-by-hosts=$TESTHOST > $TmpDir/service_find_004.out 2>&1"
         rlRun "cat $TmpDir/service_find_004.out"
-	if [ $SLAVE = "" ] ; then
+	if [[ "$SLAVE" = "" ]] ; then
         	rlAssertGrep "Number of entries returned 4" "$TmpDir/service_find_004.out"
 	else
 		rlAssertGrep "Number of entries returned 8" "$TmpDir/service_find_004.out"
@@ -561,7 +561,7 @@ rlPhaseStartTest "service_find_007: ipa service-find with --timelimit option"
         rlRun "ipa service-find --timelimit=0 > $TmpDir/service_find_007.out 2>&1"
         result=`cat $TmpDir/service_find_007.out | grep "Number of entries returned"`
         number=`echo $result | cut -d " " -f 5`
-	if [ $SLAVE = "" ] ; then
+	if [[ "$SLAVE" = "" ]] ; then
         	if [ $number -eq 5 ] ; then
                 	rlPass "Number of 5 services returned as expected with time limit of 0"
         	else
