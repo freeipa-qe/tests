31d1f96a599a1c708d76f52ecc5bcad12b57abe1
Yi Zhang <yzhang@REDHAT.COM>
2011-07-20 18:28:59 +0000

    add_and_edit and add_and_add_another test case done and passed
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1714 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/sahi/ipa/src/com/redhat/qe/ipa/sahi/tasks/GroupTasks.java
+++ ipa-tests/sahi/ipa/src/com/redhat/qe/ipa/sahi/tasks/GroupTasks.java
@@ -8,23 +8,20 @@ public class GroupTasks {
 	private static Logger log = Logger.getLogger(GroupTasks.class.getName());
 		
 	/*
-	 * Create a Group and then delete it.
+	 * Create a Group, the purpose of this is to provide a public interface to other test suite to create a new group.
 	 * @param sahiTasks 
 	 * @param groupName - user group name
 	 * @param groupDescription - group description
 	 */
-	public static void createGroup(SahiTasks sahiTasks, String groupName, String groupDescription) {
-
+	public static void createGroupService(SahiTasks sahiTasks, String groupName, String groupDescription) {
+		// FIXME: i need a way to navigate to this create group page, then, after create a new group success, i need redirect the web page to where it starts
 		sahiTasks.link("User Groups").click();
 		sahiTasks.span("ui-icon add-icon[1]").click();
-		sahiTasks.textbox("cn").setValue("auto_sahi_java_001");
-		sahiTasks.textbox("description").setValue("auto sahi 001 in java");
+		sahiTasks.textbox("cn").setValue(groupName);
+		sahiTasks.textbox("description").setValue(groupDescription);
 		sahiTasks.button("Add").click();
-		sahiTasks.checkbox("select[4]").click();
-		sahiTasks.link("Delete[1]").click();
-		sahiTasks.button("Delete").click();
 		
-	}//createGroup
+	}//createGroupService
 
 
 	/*
@@ -54,7 +51,64 @@ public class GroupTasks {
 	 * @param groupDescription - group description
 	 */
 	public static void addAndAddAnotherGroup(SahiTasks sahiTasks, String groupName, String groupDescription) {
+			// over all, this test case will covering the following scenario:
+			// 1. ensure "add and add another group" works
+			// 2. ensure "add and add another group" can be canceled 
+			// 3. ensure deletion of one group works
+			// 4. ensure deletion can be canceled
+			// 5. ensure deletion of multiple groups works
+			// the reasons to contain "deletion" test step are: 
+			//		(1) ensure the clean data exit, make sure no left over data that cause other test suite to fail or fake success 
+			//		(2) ensure the success of  adding groups
 		
+			// this test contains the following logic/page flow path:
+			// add user group -> fill the form to create a new user group -> click "add and add another" to retain the same dialog and continue add 
+			// --> after add total 3 new user groups, click "cancel" to go back to main user group page
+			// --> then start to delete the newly created user groups: click the first one and delete it : sahi_auto_addandaddanother_001
+			// --> select the second one, but click "cancel" to NOT delete it, in UI, the second one is still in "selected" status (sahi_auto_addandaddanother_002)
+			// --> then select the third one, at this point, there will be total 2 newly created user groups being selected"
+			// --> click "delete" to delete the remaining two newly created user groups (sahi_auto_addandaddanother_002, sahi_auto_addandaddanother_003)
+		
+			// get into add user group page
+			sahiTasks.link("User Groups").click();
+			
+			// enter simple add group dialog, and add the first user group: sahi_auto_addandaddanother_001
+			sahiTasks.link("Add[1]").click();
+			sahiTasks.textbox("cn").setValue("sahi_auto_addandaddanother_001");
+			sahiTasks.textbox("description").setValue("sahi auto, add and add another 001");
+			
+			// click Add and Add Another to create a new group without leave the current dialog , this the main purpose of this test case
+			sahiTasks.button("Add and Add Another").click();	
+			sahiTasks.textbox("cn").setValue("sahi_auto_addandaddanother_002");
+			sahiTasks.textbox("description").setValue("sahi auto, add and add another 002");
+			sahiTasks.checkbox("posix").click();
+			
+			// add another user group
+			sahiTasks.button("Add and Add Another").click();
+			sahiTasks.textbox("cn").setValue("sahi_auto_addandaddanother_003");
+			sahiTasks.textbox("description").setValue("sahi auto, add and add another 003");
+			sahiTasks.checkbox("posix").click();
+			
+			// finally cancel to go back to main user group page
+			sahiTasks.button("Add and Add Another").click();
+			sahiTasks.button("Cancel").click();
+				
+			// deletion test starts here: 
+			//					test 1: single deletion test
+			sahiTasks.checkbox("select[6]").click();
+			sahiTasks.link("Delete[1]").click();
+			sahiTasks.button("Delete").click();
+			
+			//					test 2: single deletion canceling test	
+			sahiTasks.checkbox("select[6]").click();
+			sahiTasks.link("Delete[1]").click();
+			sahiTasks.button("Cancel").click();
+
+			//					test 3: multiple  deletion test
+			sahiTasks.checkbox("select[7]").click();
+			sahiTasks.link("Delete[1]").click();
+			sahiTasks.button("Delete").click(); 
+
 	}// addAndAddAnotherGroup
 
 
@@ -64,8 +118,75 @@ public class GroupTasks {
 	 * @param groupName - user group name
 	 * @param groupDescription - group description
 	 */
-	public static void addAndAddAnotherGroup(SahiTasks sahiTasks, String groupName, String groupDescription) {
+	public static void addAndEditGroup(SahiTasks sahiTasks, String groupName, String groupDescription) {
+		// overall , this test will cover the following scenario:
+		//	1. create a new default (posix) user group and immediately move to editing page, make one change and go back
+		//	2. create a new non-posix user group and immediately move to editing page, make one change and go back
+		//	3. create a new default (posix) user group and immediately move to editing page, make change, undo the change and go back
+		//	4. delete above 3 newly created group to ensure the success of creation
+		// the reasons to contain "deletion" test step are: 
+		//		(1) ensure the clean data exit, make sure no left over data that cause other test suite to fail or fake success 
+		//		(2) ensure the success of adding groups
 		
+		// Navigate to create user group page
+		sahiTasks.link("User Groups").click();
+		
+		// add first group
+		sahiTasks.link("Add[1]").click();
+		sahiTasks.textbox("cn").setValue("sahi_auto_add_and_edit_001");
+		sahiTasks.textbox("description").setValue("sahi auto, add and edit 001");
+		
+		// confirm first group adding and move to edit page for this group
+		sahiTasks.button("Add and Edit").click();
+		
+		// do some minimum editing to ensure page navigation is what we are intend to do
+		sahiTasks.link("Settings").click();
+		sahiTasks.textbox("description").setValue("sahi auto, add and edit 001, modified");
+		sahiTasks.link("Update").click();
+		
+		// go back to use group list page
+		sahiTasks.link("User Groups[4]").click();
+		
+		// add another user group, this time, it would be non-posix group
+		sahiTasks.link("Add[1]").click();
+		sahiTasks.textbox("cn[1]").setValue("sahi_auto_add_and_edit_002");
+		sahiTasks.textbox("description[1]").setValue("sahi auto, add and edit 002");
+		sahiTasks.checkbox("posix").click();
+		
+		// confirm second group adding and go to editing page
+		sahiTasks.button("Add and Edit").click();
+		
+		// do some minimum editing to ensure page navigation is what we are intend to do
+		sahiTasks.link("Settings").click();
+		sahiTasks.textbox("description").setValue("sahi auto, add and edit 002, modified");
+		sahiTasks.link("Update").click();
+		
+		// go back to user group list page
+		sahiTasks.link("User Groups[4]").click();
+		
+		// add third user group
+		sahiTasks.link("Add[1]").click();
+		sahiTasks.textbox("cn[1]").setValue("sahi_auto_add_and_edit_003");
+		sahiTasks.textbox("description[1]").setValue("sahi auto, add and edit 003");
+		
+		// confirm add and move to edit page
+		sahiTasks.button("Add and Edit").click();
+		
+		// make change to description, and reset the change
+		sahiTasks.link("Settings").click();
+		sahiTasks.textbox("description").setValue("sahi, auto, 003, modified");
+		sahiTasks.link("Reset").click();
+		
+		// go back to user group list page
+		sahiTasks.link("User Groups[4]").click();
+		
+		// select the newly created 3 user groups and delete all of them -- to confirm the success of previous creation
+		sahiTasks.checkbox("select[6]").click();
+		sahiTasks.checkbox("select[7]").click();
+		sahiTasks.checkbox("select[8]").click();
+		sahiTasks.link("Delete[1]").click();
+		sahiTasks.button("Delete").click();
+
 	}// addAndEditGroup
 
 
