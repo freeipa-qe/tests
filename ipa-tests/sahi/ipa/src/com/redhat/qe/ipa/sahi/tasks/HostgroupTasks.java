4528c8bb5c387f30eb8cfcd26b3adce3d1e79b6e
Jenny Galipeau <jgalipea@REDHAT.COM>
2011-07-22 20:02:39 +0000

    checking in the shell for my next test suite
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1744 aeb79ed8-e03d-0410-be94-af332b95afa2

---
+++ ipa-tests/sahi/ipa/src/com/redhat/qe/ipa/sahi/tasks/HostgroupTasks.java
@@ -0,0 +1,21 @@
+package com.redhat.qe.ipa.sahi.tasks;
+
+import java.util.logging.Logger;
+import com.redhat.qe.ipa.sahi.tasks.SahiTasks;
+
+
+
+public class HostgroupTasks {
+	private static Logger log = Logger.getLogger(HostgroupTasks.class.getName());
+	
+	/*
+	 * Create a host group
+	 * @param sahiTasks 
+	 * @param groupname - groupname
+	 * @param description -  description for group
+	 * @param button - Add or Cancel
+	 */
+	public static void addHostGroup(SahiTasks sahiTasks, String groupName, String description, String Button) {
+
+	}
+}
