f14a2e21d2110f1bee7846af26ac9b98a0001339
Namita Soman <nsoman@REDHAT.COM>
2011-07-22 06:03:44 +0000

    update to match SahiTestScript
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1735 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/sahi/ipa/src/com/redhat/qe/ipa/sahi/tasks/SahiTasks.java
+++ ipa-tests/sahi/ipa/src/com/redhat/qe/ipa/sahi/tasks/SahiTasks.java
@@ -3,7 +3,7 @@ package com.redhat.qe.ipa.sahi.tasks;
 import com.redhat.qe.auto.sahi.ExtendedSahi;
 
 public class SahiTasks extends ExtendedSahi {
-	public SahiTasks(String browserPath, String browserName, String browserOpt, String sahiBaseDir, String sahiUserdataDir) {
+	public SahiTasks(String browserType, String browserPath, String browserName, String browserOpt, String sahiBaseDir, String sahiUserdataDir) {
 		super(browserPath, browserName, browserOpt, sahiBaseDir, sahiUserdataDir);
 	}
 
