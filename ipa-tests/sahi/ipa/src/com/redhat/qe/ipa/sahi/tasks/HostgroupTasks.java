aab2787acc2997baebe0360a992abd334f023a9c
Jenny Galipeau <jgalipea@REDHAT.COM>
2011-07-25 21:00:12 +0000

    checking in first tests
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1750 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/sahi/ipa/src/com/redhat/qe/ipa/sahi/tasks/HostgroupTasks.java
+++ ipa-tests/sahi/ipa/src/com/redhat/qe/ipa/sahi/tasks/HostgroupTasks.java
@@ -1,12 +1,10 @@
 package com.redhat.qe.ipa.sahi.tasks;
 
-import java.util.logging.Logger;
 import com.redhat.qe.ipa.sahi.tasks.SahiTasks;
 
 
 
 public class HostgroupTasks {
-	private static Logger log = Logger.getLogger(HostgroupTasks.class.getName());
 	
 	/*
 	 * Create a host group
@@ -15,7 +13,64 @@ public class HostgroupTasks {
 	 * @param description -  description for group
 	 * @param button - Add or Cancel
 	 */
-	public static void addHostGroup(SahiTasks sahiTasks, String groupName, String description, String Button) {
-
+	public static void addHostGroup(SahiTasks sahiTasks, String groupName, String description, String button) {
+		sahiTasks.span("Add[1]").click();
+		sahiTasks.textbox("cn").setValue(groupName);
+		sahiTasks.textbox("description").setValue(description);
+		sahiTasks.button(button).click();
+	}
+	
+	/*
+	 * add a host and add another
+	 * @param sahiTasks 
+	 * @param hostname - hostname1
+	 * @param hostname - hostname2
+	 * @param hostname - hostname2
+	 */
+	public static void addAndAddAnotherHost(SahiTasks sahiTasks, String groupName1, String groupName2, String groupName3) {
+		String description1 = groupName1 + " description";
+		sahiTasks.span("Add[1]").click();
+		sahiTasks.textbox("cn").setValue(groupName1);
+		sahiTasks.textbox("description").setValue(description1);
+		sahiTasks.button("Add and Add Another").click();
+		
+		String description2 = groupName2 + " description";
+		sahiTasks.textbox("cn").setValue(groupName2);
+		sahiTasks.textbox("description").setValue(description2);
+		sahiTasks.button("Add and Add Another").click();
+		
+		String description3 = groupName3 + " description";
+		sahiTasks.textbox("cn").setValue(groupName3);
+		sahiTasks.textbox("description").setValue(description3);
+		sahiTasks.button("Add").click();
+	}
+	
+	/*
+	 * Delete the host group.
+	 * @param sahiTasks
+	 * @param groupName - name of the host group
+	 * @param button - Delete or Cancel
+	 */
+	public static void deleteHostgroup(SahiTasks sahiTasks, String groupName, String button) {
+		sahiTasks.checkbox(groupName).click();
+		sahiTasks.span("Delete[1]").click();
+		sahiTasks.button(button).click();
+		
+		if (button == "Cancel"){
+			sahiTasks.checkbox(groupName).click();
+		}
+	}
+	
+	/*
+	 * Delete multiple host groups.
+	 * @param sahiTasks
+	 * @param groupnames - the array of groupnames to delete
+	 */
+	public static void deleteHostgroup(SahiTasks sahiTasks, String [] groupnames) {
+		for (String groupname : groupnames) {
+			sahiTasks.checkbox(groupname).click();
+		}
+		sahiTasks.link("Delete[1]").click();
+		sahiTasks.button("Delete").click();
 	}
 }
