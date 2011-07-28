6d07f8b7b19ab4dfd7a8637a253368c89c5b2678
Gowrishankar Rajaiyan <grajaiya@REDHAT.COM>
2011-07-28 12:54:57 +0000

    fixing beaker issue
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1771 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-automount/t.ipa-automount.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-automount/t.ipa-automount.sh
@@ -849,7 +849,7 @@ rlPhaseStartTest "automountmap_add_008: ipa automountmap-add-indirect LOCATION M
 	rlRun "ipa -d automountlocation-add pune"
         rlRun "ipa -d automountmap-add pune pune.map"
 
-        rlRun "ipa automountmap-add-indirect pune punechild.map --mount=/usr/share/man --parentmap=pune.map --all --raw > $TmpDir/automountmap_add_008.out"
+        rlRun "ipa -d automountmap-add-indirect pune punechild.map --mount=/usr/share/man --parentmap=pune.map --all --raw > $TmpDir/automountmap_add_008.out"
 	rlAssertGrep "dn: automountmapname=punechild.map,cn=pune,cn=automount,$basedn" "$TmpDir/automountmap_add_008.out"
 	rlAssertGrep "Map: punechild.map" "$TmpDir/automountmap_add_008.out"
 	rlAssertGrep "objectclass: automountmap" "$TmpDir/automountmap_add_008.out"
