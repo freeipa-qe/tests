230efaaa37c9ab309503a4d180ddbe3a2e42aa89
Jenny Galipeau <jgalipea@REDHAT.COM>
2011-08-02 20:00:31 +0000

    additional tests for add and add - add and edit settings - add host members - and add host group members
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1817 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/sahi/ipa/src/com/redhat/qe/ipa/sahi/tests/netgroup/NetgroupTests.java
+++ ipa-tests/sahi/ipa/src/com/redhat/qe/ipa/sahi/tests/netgroup/NetgroupTests.java
@@ -71,6 +71,177 @@ public class NetgroupTests extends SahiTestScript{
 		}
 	}
 	
+	/*
+	 * Add and Add Another net group
+	 */
+	@Test (groups={"addAndAddAnotherNetGroupTests"}, dataProvider="getAddAndAddAnotherNetGroupTestObjects")	
+	public void testNetGroupAddAndAddAnother(String testName, String groupName1, String groupName2, String groupName3) throws Exception {
+		
+		//verify host group doesn't exist
+		com.redhat.qe.auto.testng.Assert.assertFalse(sahiTasks.link(groupName1).exists(), "Verify net group " + groupName1 + " doesn't already exist");
+		com.redhat.qe.auto.testng.Assert.assertFalse(sahiTasks.link(groupName2).exists(), "Verify net group " + groupName2 + " doesn't already exist");
+		com.redhat.qe.auto.testng.Assert.assertFalse(sahiTasks.link(groupName3).exists(), "Verify net group " + groupName3 + " doesn't already exist");
+		
+		//add new host group
+		HostgroupTasks.addAndAddAnotherHostGroup(sahiTasks, groupName1, groupName2, groupName3);
+	
+		//verify host group exists
+		com.redhat.qe.auto.testng.Assert.assertTrue(sahiTasks.link(groupName1).exists(), "Verify net group " + groupName1 + " exists");
+		com.redhat.qe.auto.testng.Assert.assertTrue(sahiTasks.link(groupName2).exists(), "Verify net group " + groupName2 + " exists");
+		com.redhat.qe.auto.testng.Assert.assertTrue(sahiTasks.link(groupName3).exists(), "Verify net group " + groupName3 + " exists");
+	
+	}
+	
+	/*
+	 * Delete multiple net groups positive tests
+	 */
+	@Test (groups={"deleteMultipleNetGroupTests"}, dataProvider="getAddAndAddAnotherNetGroupTestObjects",  dependsOnGroups="addAndAddAnotherNetGroupTests")	
+	public void testNetGroupDeleteMultiple(String testName, String groupName1, String groupName2, String groupName3) throws Exception {
+		//verify host group exists
+		com.redhat.qe.auto.testng.Assert.assertTrue(sahiTasks.link(groupName1).exists(), "Verify net group " + groupName1 + " exists");
+		com.redhat.qe.auto.testng.Assert.assertTrue(sahiTasks.link(groupName2).exists(), "Verify net group " + groupName2 + " exists");
+		com.redhat.qe.auto.testng.Assert.assertTrue(sahiTasks.link(groupName3).exists(), "Verify net group " + groupName3 + " exists");
+		
+		String [] groupnames = {groupName1, groupName2, groupName3};
+		//delete net group
+		NetgroupTasks.deleteNetgroup(sahiTasks, groupnames);
+
+		//verify net group doesn't exist
+		com.redhat.qe.auto.testng.Assert.assertFalse(sahiTasks.link(groupName1).exists(), "Verify net group " + groupName1 + " was deleted successfully");
+		com.redhat.qe.auto.testng.Assert.assertFalse(sahiTasks.link(groupName2).exists(), "Verify net group " + groupName2 + " was deleted successfully");
+		com.redhat.qe.auto.testng.Assert.assertFalse(sahiTasks.link(groupName3).exists(), "Verify net group " + groupName3 + " was deleted successfully");
+		
+	}
+	
+	/*
+	 * Add and edit net group settings
+	 */
+	@Test (groups={"addAndEditNetGroupSettingsTest"}, dataProvider="getAddAndEditNetGroupTestObjects")	
+	public void testnetGroupAddAndEdit(String testName, String groupName, String description1, String description2, String nisdomain) throws Exception {
+		//verify net group doesn't exist
+		com.redhat.qe.auto.testng.Assert.assertFalse(sahiTasks.link(groupName).exists(), "Verify net group " + groupName + " doesn't already exist");
+		
+		//add new net group
+		NetgroupTasks.addAndEditNetGroup(sahiTasks, groupName, description1, description2, nisdomain);
+		
+		//verify the net group exists
+		com.redhat.qe.auto.testng.Assert.assertTrue(sahiTasks.link(groupName).exists(), "Verify net group " + groupName + " exists");
+		
+		//verify the net group setting
+		NetgroupTasks.verifyNetGroupSettings(sahiTasks, groupName, description2, nisdomain);
+		
+		//clean up delete net group
+		NetgroupTasks.deleteNetgroup(sahiTasks, groupName, "Delete");
+
+	}
+	
+	/*
+	 * Host Members tests
+	 */
+	@Test (groups={"netGroupHostMembersTests"}, dataProvider="getNetGroupHostMembersTestObjects")
+	public void testNetGroupHostMembers(String testName) throws Exception {
+		String devwebserver = "webserver_dev." + domain;
+		String qewebserver = "webserver_qe." + domain;
+		String devhost = "laptop_dev." + domain;
+		String qehost = "laptop_qa." + domain;
+		String engwebserver = "webserver_eng." + domain;
+		
+		String [] hostnames = {devwebserver, qewebserver, devhost, qehost, engwebserver};
+		
+		String devgroup = "development hosts";
+		String qegroup = "quality hosts";
+		String enggroup = "engineering hosts";
+		
+		String [] netgroups = {devgroup, qegroup, enggroup};
+		
+		//add hosts for host group members
+		sahiTasks.navigateTo(System.getProperty("ipa.server.url")+hostPage, true);
+		for (String hostname : hostnames) {
+			HostTasks.addHost(sahiTasks, hostname, "");
+		}
+		
+		//add net groups
+		sahiTasks.navigateTo(System.getProperty("ipa.server.url")+netgroupPage, true);
+		for (String netgroup : netgroups) {
+			String description = netgroup + " group";
+			NetgroupTasks.addNetGroup(sahiTasks, netgroup, description, "Add");
+		}
+		
+		String [] devhosts = {devwebserver, devhost};
+		String [] qehosts = {qewebserver, qehost};
+		String [] enghosts = {engwebserver};
+		
+		// add the host members
+		NetgroupTasks.addMembers(sahiTasks, devgroup, "host", devhosts, "Enroll");
+		NetgroupTasks.addMembers(sahiTasks, qegroup, "host", qehosts, "Enroll");
+		NetgroupTasks.addMembers(sahiTasks, enggroup, "host", enghosts, "Enroll");
+		
+		//verify the host members
+		NetgroupTasks.verifyMembers(sahiTasks, devgroup, "host", devhosts, "YES");
+		NetgroupTasks.verifyMembers(sahiTasks, qegroup, "host", qehosts, "YES");
+		NetgroupTasks.verifyMembers(sahiTasks, enggroup, "host", enghosts, "YES");
+		 
+		//delete hosts
+		sahiTasks.navigateTo(System.getProperty("ipa.server.url")+hostPage, true);
+		HostTasks.deleteHost(sahiTasks, hostnames);
+		
+		//delete host groups
+		sahiTasks.navigateTo(System.getProperty("ipa.server.url")+netgroupPage, true);
+		NetgroupTasks.deleteNetgroup(sahiTasks, netgroups);
+
+	}
+	
+	/*
+	 * Host Group Members tests
+	 */
+	@Test (groups={"netGroupHostGroupMembersTests"}, dataProvider="getNetGroupHostGroupMembersTestObjects")
+	public void testNetGroupHostGroupMembers(String testName) throws Exception {
+		String hostgroup1A = "hostgroup1a";
+		String hostgroup1B = "hostgroup1b";
+		String hostgroup2 = "hostgroup2";
+		
+		String [] hostgroups = {hostgroup1A, hostgroup1B, hostgroup2};
+		
+		String netgroup1 = "netgroup one";
+		String netgroup2 = "netgroup two";
+		
+		String [] netgroups = {netgroup1, netgroup2};
+		
+		//add host groups for net group members
+		sahiTasks.navigateTo(System.getProperty("ipa.server.url")+hostgroupPage, true);
+		for (String hostgroup : hostgroups) {
+			String description = hostgroup + " description";
+			HostgroupTasks.addHostGroup(sahiTasks, hostgroup, description, "Add");
+		}
+		
+		//add net groups
+		sahiTasks.navigateTo(System.getProperty("ipa.server.url")+netgroupPage, true);
+		for (String netgroup : netgroups) {
+			String description = netgroup + " description";
+			NetgroupTasks.addNetGroup(sahiTasks, netgroup, description, "Add");
+		}
+		
+		String [] grp1members = {hostgroup1A, hostgroup1B};
+		String [] grp2members = {hostgroup2};
+		
+		// add the host group members
+		NetgroupTasks.addMembers(sahiTasks, netgroup1, "hostgroup", grp1members, "Enroll");
+		NetgroupTasks.addMembers(sahiTasks, netgroup2, "hostgroup", grp2members, "Enroll");
+		
+		//verify the host group members
+		NetgroupTasks.verifyMembers(sahiTasks, netgroup1, "hostgroup", grp1members, "YES");
+		NetgroupTasks.verifyMembers(sahiTasks, netgroup2, "hostgroup", grp2members, "YES");
+		 
+		//delete hosts
+		sahiTasks.navigateTo(System.getProperty("ipa.server.url")+hostgroupPage, true);
+		HostgroupTasks.deleteHostgroup(sahiTasks, hostgroups);
+		
+		//delete host groups
+		sahiTasks.navigateTo(System.getProperty("ipa.server.url")+netgroupPage, true);
+		NetgroupTasks.deleteNetgroup(sahiTasks, netgroups);
+
+	}
+	
 	
 
 /*******************************************************
@@ -110,6 +281,66 @@ public class NetgroupTests extends SahiTestScript{
 		ll.add(Arrays.asList(new Object[]{ 		"delete_netgroup_cancel",			"NewNetGroup",															"Delete" } ));
 		ll.add(Arrays.asList(new Object[]{ 		"delete_netgroup_longname",			"thisisanetgroupwithaveryveryveryveryveryveryveryveryverylongname", 	"Delete" } ));
 		ll.add(Arrays.asList(new Object[]{ 		"delete_netgroup_longdesc",			"long description",			 											"Delete" } ));
+		return ll;	
+	}
+	
+	/*
+	 * Data to be used when add and add another net groups and delete multiple
+	 */
+	@DataProvider(name="getAddAndAddAnotherNetGroupTestObjects")
+	public Object[][] getAddAndAddAnotherNetGroupTestObjects() {
+		return TestNGUtils.convertListOfListsTo2dArray(createAddAndAddAnotherNetGroupTestObjects());
+	}
+	protected List<List<Object>> createAddAndAddAnotherNetGroupTestObjects() {		
+		List<List<Object>> ll = new ArrayList<List<Object>>();
+	
+		//										testname									groupanme1					groupname2			groupname3
+		ll.add(Arrays.asList(new Object[]{ 		"add_and_add_another_then_del_multiple",	"this is group one",		"engineering",	 	"sales" } ));
+		return ll;	
+	}
+	
+	/*
+	 * Data to be used when adding and editing net groups
+	 */
+	@DataProvider(name="getAddAndEditNetGroupTestObjects")
+	public Object[][] getAddAndEditNetGroupTestObjects() {
+		return TestNGUtils.convertListOfListsTo2dArray(createAddAndEditNetGroupTestObjects());
+	}
+	protected List<List<Object>> createAddAndEditNetGroupTestObjects() {		
+		List<List<Object>> ll = new ArrayList<List<Object>>();
+	
+		//										testname						groupanme1			description1				description2						nisdomain
+		ll.add(Arrays.asList(new Object[]{ 		"add_and_edit_setting",			"engineering",		"engineering net group",	"red hat engineering net group",  	"new.nis.domain"} ));
+		return ll;	
+	}
+	
+	/*
+	 * Data to be used when adding host members
+	 */
+	@DataProvider(name="getNetGroupHostMembersTestObjects")
+	public Object[][] getNetGroupHostMembersTestObjects() {
+		return TestNGUtils.convertListOfListsTo2dArray(createNetGroupHostMembersTestObjects());
+	}
+	protected List<List<Object>> createNetGroupHostMembersTestObjects() {		
+		List<List<Object>> ll = new ArrayList<List<Object>>();
+	
+		//										testname					
+		ll.add(Arrays.asList(new Object[]{ 		"host_memberships" } ));
+		return ll;	
+	}
+	
+	/*
+	 * Data to be used when adding host members
+	 */
+	@DataProvider(name="getNetGroupHostGroupMembersTestObjects")
+	public Object[][] getNetGroupHostGroupMembersTestObjects() {
+		return TestNGUtils.convertListOfListsTo2dArray(createNetGroupHostGroupMembersTestObjects());
+	}
+	protected List<List<Object>> createNetGroupHostGroupMembersTestObjects() {		
+		List<List<Object>> ll = new ArrayList<List<Object>>();
+	
+		//										testname					
+		ll.add(Arrays.asList(new Object[]{ 		"hostgroup_memberships" } ));
 		return ll;	
 	}
 	
