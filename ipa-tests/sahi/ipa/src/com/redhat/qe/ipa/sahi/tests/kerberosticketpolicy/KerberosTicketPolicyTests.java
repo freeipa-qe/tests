cf6d45e720a785e5492783f65606b8d50f665eed
Yi Zhang <yzhang@REDHAT.COM>
2011-08-30 20:33:22 +0000

    first official complete check in, all test cases have been implemented, all test case are passed against 8-30-2011 daily build
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@1980 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/sahi/ipa/src/com/redhat/qe/ipa/sahi/tests/kerberosticketpolicy/KerberosTicketPolicyTests.java
+++ ipa-tests/sahi/ipa/src/com/redhat/qe/ipa/sahi/tests/kerberosticketpolicy/KerberosTicketPolicyTests.java
@@ -2,7 +2,6 @@ package com.redhat.qe.ipa.sahi.tests.kerberosticketpolicy;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -11,13 +10,9 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 
-import com.redhat.qe.auto.testng.Assert;
 import com.redhat.qe.auto.testng.TestNGUtils;
 import com.redhat.qe.ipa.sahi.base.SahiTestScript;
-import com.redhat.qe.ipa.sahi.tasks.CommonTasks;
-import com.redhat.qe.ipa.sahi.tasks.SahiTasks;
 import com.redhat.qe.ipa.sahi.tasks.KerberosTicketPolicyTasks;
-import com.redhat.qe.auto.testng.*;
 
 public class KerberosTicketPolicyTests extends SahiTestScript{
 	private static Logger log = Logger.getLogger(KerberosTicketPolicyTests.class.getName());
@@ -27,12 +22,12 @@ public class KerberosTicketPolicyTests extends SahiTestScript{
 	public void initialize() throws CloneNotSupportedException {	
 		sahiTasks.navigateTo(commonTasks.kerberosTicketPolicyPage, true);
 		sahiTasks.setStrictVisibilityCheck(true);
-	}
+	}//initialize
 	
 	@BeforeMethod (alwaysRun=true)
 	public void checkURL(){
-		String currentURL = sahiTasks.fetch("top.location.href");
-		//TODO: yi: add alternate page 
+		// ensure the starting page for each test case is the kerberos ticket policy page
+		String currentURL = sahiTasks.fetch("top.location.href"); 
 		if (!currentURL.equals(commonTasks.kerberosTicketPolicyPage)){
 			log.info("current url=("+currentURL + "), is not a starting position, move to url=("+commonTasks.kerberosTicketPolicyPage +")");
 			sahiTasks.navigateTo(commonTasks.kerberosTicketPolicyPage, true);
@@ -71,8 +66,8 @@ public class KerberosTicketPolicyTests extends SahiTestScript{
 	protected List<List<Object>> createKerberosTicketPolicyDetails() {		
 		List<List<Object>> ll = new ArrayList<List<Object>>();  
 											// testName, policy name, fieldName, fieldValue
-		ll.add(Arrays.asList(new Object[]{"password policy base test","krbmaxrenewableage","6408000"} )); 
-		ll.add(Arrays.asList(new Object[]{"password policy base test","krbmaxticketlife","864000"} )); 
+		ll.add(Arrays.asList(new Object[]{"kerberos policy test for max renewable age","krbmaxrenewableage","6408000"} )); 
+		ll.add(Arrays.asList(new Object[]{"kerberos policy test for max kerberos ticket life","krbmaxticketlife","864000"} )); 
 		return ll;	
 	}//Data provider: getKerberosTicketPolicyDetails 
 	
@@ -83,11 +78,13 @@ public class KerberosTicketPolicyTests extends SahiTestScript{
 	protected List<List<Object>> createKerberosTicketPolicyDetailsNegative() {		
 		List<List<Object>> ll = new ArrayList<List<Object>>();  
 											// testName, policy name, fieldName, fieldValue
-		ll.add(Arrays.asList(new Object[]{"password policy base test","krbmaxrenewableage","abc", "Must be an integer"} )); 
-		ll.add(Arrays.asList(new Object[]{"password policy base test","krbmaxrenewableage","20000000000000", "Maximum value is 2147483647"}));
+		ll.add(Arrays.asList(new Object[]{"kerberos policy max renewable age: invalid data: string used instead of integer","krbmaxrenewableage","abc", "Must be an integer"} )); 
+		ll.add(Arrays.asList(new Object[]{"kerberos policy max renewable age: bigger than max value","krbmaxrenewableage","20000000000000", "Maximum value is 2147483647"}));
+		ll.add(Arrays.asList(new Object[]{"kerberos policy max renewable age: smaller than min value","krbmaxrenewableage","0", "Minimum value is 1"}));
 		
-		ll.add(Arrays.asList(new Object[]{"password policy base test","krbmaxticketlife","edf", "Must be an integer"} )); 
-		ll.add(Arrays.asList(new Object[]{"password policy base test","krbmaxticketlife","20000000000000", "Maximum value is 2147483647"}));
+		ll.add(Arrays.asList(new Object[]{"kerberos policy max ticket life, invalid data: string used instead of integer","krbmaxticketlife","edf", "Must be an integer"} )); 
+		ll.add(Arrays.asList(new Object[]{"kerberos policy max ticket life, bigger than max value","krbmaxticketlife","20000000000000", "Maximum value is 2147483647"}));
+		ll.add(Arrays.asList(new Object[]{"kerberos policy max ticket life, smaller than min value","krbmaxticketlife","0", "Minimum value is 1"}));
 		return ll;	
 	}//Data provider: createKerberosTicketPolicyDetailsNegative 
 	 
