4c7c4a2dd9e765448722ca7cb7a7238e8a25b72b
Michael Gregg <mgregg@REDHAT.COM>
2011-09-29 01:12:21 +0000

    expanding and completing the nis ipa tests a bit
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@2120 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/nis-cli/t.nistests.sh
+++ ipa-tests/beaker/ipa-server/acceptance/nis-cli/t.nistests.sh
@@ -9,37 +9,44 @@ group4=grpeeww
 ngroup1=ngrp7664
 setup()
 {        
-	rlPhaseStartTest "ipa-nis-cli-setup: SETUP"
+	rlPhaseStartTest "add users and groups for use with nis tests"
         	rlRun "kinitAs $ADMINID $ADMINPW" 0 "Kinit as admin user"
 
-        	ipa user-add --first=aa --last=bb $user1
-        	ipa user-add --first=aa --last=bb $user2
-        	ipa user-add --first=aa --last=bb $user3
-        	ipa user-add --first=aa --last=bb $user4
-        	ipa group-add --desc=testtest $group1
-        	ipa group-add --desc=testtest $group2
-        	ipa group-add --desc=testtest $group3
-        	ipa group-add --desc=testtest $group4
+        	rlRun "ipa user-add --first=aa --last=bb $user1" 0 "adding user $user1 for use in ipa test"
+        	rlRun "ipa user-add --first=aa --last=bb $user2" 0 "adding user $user2 for use in ipa test"
+        	rlRun "ipa user-add --first=aa --last=bb $user3" 0 "adding user $user3 for use in ipa test"
+        	rlRun "ipa user-add --first=aa --last=bb $user4" 0 "adding user $user4 for use in ipa test"
+        	rlRun "ipa group-add --desc=testtest $group1" 0 "adding group $group1 for use in ipa test"
+        	rlRun "ipa group-add --desc=testtest $group2" 0 "adding group $group2 for use in ipa test"
+        	rlRun "ipa group-add --desc=testtest $group3" 0 "adding group $group3 for use in ipa test"
+        	rlRun "ipa group-add --desc=testtest $group4" 0 "adding group $group4 for use in ipa test"
 
         	addNetgroup $ngroup1 test-group-1
+	rlPhaseEnd
+	
+	rlPhaseStartTest "ipa-nis-cli-01: enable nis listening."
+		rlRun "echo $ADMINPW | ipa-compat-manage enable" 0 "Enabling ipa compatibility"
+		rlRun "echo $ADMINPW | ipa-nis-manage enable" 0 "Enabling nis listening"
+		rlRun "/etc/init.d/rpcbind restart" 0 "restarting rcpbind"
+		rlRun "/etc/init.d/ipa restart" 0 "restarting IPA"
 	rlPhaseEnd
 }
 
 runtests()
 {
-        rlPhaseStartTest "ipa-nis-cli-01: check to see if ypcat can enumerate passwd"
+        rlPhaseStartTest "ipa-nis-cli-02: check to see if ypcat can enumerate passwd"
                 rlRun "ypcat -h $MASTER -d $DOMAIN passwd" 0 "Check to see that passwd can be enumerated"
         rlPhaseEnd
 
-        rlPhaseStartTest "ipa-nis-cli-02: check to see if ypcat can enumerate group"
+        rlPhaseStartTest "ipa-nis-cli-03: check to see if ypcat can enumerate group"
                 rlRun "ypcat -h $MASTER -d $DOMAIN group" 0 "Check to see that group can be enumerated"
         rlPhaseEnd
 
-        rlPhaseStartTest "ipa-nis-cli-03: check to see if ypcat can enumerate netgroup"
+        rlPhaseStartTest "ipa-nis-cli-04: check to see if ypcat can enumerate netgroup"
                 rlRun "ypcat -h $MASTER -d $DOMAIN netgroup" 0 "Check to see that netgroup can be enumerated"
         rlPhaseEnd
 
-        rlPhaseStartTest "ipa-nis-cli-04: check to see if ypcat cannot enumerate badgroup"
+        rlPhaseStartTest "ipa-nis-cli-05: check to see if ypcat cannot enumerate badgroup"
                 rlRun "ypcat -h $MASTER -d $DOMAIN badgroup" 1 "Check to see that badgroup can not be enumerated"
         rlPhaseEnd
 
@@ -48,21 +55,39 @@ runtests()
         ypcat -h $MASTER -d $DOMAIN group > /dev/shm/group-map
         ypcat -h $MASTER -d $DOMAIN netgroup > /dev/shm/netgroup-map
 
-        rlPhaseStartTest "ipa-nis-cli-05: check to ensure all users are in nis"
+        rlPhaseStartTest "ipa-nis-cli-06: check to ensure user $user1 is in nis"
                 rlRun "grep $user1 /dev/shm/passwd-map" 0 "Verifying that user1 is in the nis passwd map"
+	rlPhaseEnd 
+
+        rlPhaseStartTest "ipa-nis-cli-07: check to ensure user $user2 is in nis"
                 rlRun "grep $user2 /dev/shm/passwd-map" 0 "Verifying that user2 is in the nis passwd map"
+	rlPhaseEnd 
+
+        rlPhaseStartTest "ipa-nis-cli-08: check to ensure user $user3 is in nis"
                 rlRun "grep $user3 /dev/shm/passwd-map" 0 "Verifying that user3 is in the nis passwd map"
+	rlPhaseEnd 
+
+        rlPhaseStartTest "ipa-nis-cli-09: check to ensure user $user4 is in nis"
                 rlRun "grep $user4 /dev/shm/passwd-map" 0 "Verifying that user4 is in the nis passwd map"
         rlPhaseEnd
 
-        rlPhaseStartTest "ipa-nis-cli-06: check to ensure that groups are in nis passwd map"
+        rlPhaseStartTest "ipa-nis-cli-10: check to ensure that group $group1 in the nis group map"
                 rlRun "grep $group1 /dev/shm/group-map" 0 "Verifying that group1 is in the nis password map"
+        rlPhaseEnd
+
+        rlPhaseStartTest "ipa-nis-cli-11: check to ensure that group $group2 in the nis group map"
                 rlRun "grep $group2 /dev/shm/group-map" 0 "Verifying that group2 is in the nis password map"
+        rlPhaseEnd
+
+        rlPhaseStartTest "ipa-nis-cli-12: check to ensure that group $group3 in the nis group map"
                 rlRun "grep $group3 /dev/shm/group-map" 0 "Verifying that group3 is in the nis password map"
+        rlPhaseEnd
+
+        rlPhaseStartTest "ipa-nis-cli-13: check to ensure that group $group4 in the nis group map"
                 rlRun "grep $group4 /dev/shm/group-map" 0 "Verifying that group4 is in the nis password map"
         rlPhaseEnd
 
-        rlPhaseStartTest "ipa-nis-cli-07: check to ensure invalid users are not in nis"
+        rlPhaseStartTest "ipa-nis-cli-14: check to ensure invalid users are not in nis"
                 rlRun "grep baduser1 /dev/shm/passwd-map" 1 "Verifying that user1 is in the nis passwd map"
         rlPhaseEnd
 
@@ -94,6 +119,8 @@ cleanup()
 {
     rlPhaseStartCleanup "nis-cli cleanup"
         rlRun "setenforce 1" 0 "reenable enforcing selinux"
+	rlRun "echo $ADMINPW | ipa-nis-manage disable" 0 "Disabling nis listening"
+	rlRun "echo $ADMINPW | ipa-compat-manage disable" 0 "Disabling ipa compatibility"
         ipa user-del $user1
         ipa user-del $user2
         ipa user-del $user3
