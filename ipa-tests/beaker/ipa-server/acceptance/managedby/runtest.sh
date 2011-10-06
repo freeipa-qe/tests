4b953824e4f9f2fbdfa7e39d5570d06d1ac4cc1b
Michael Gregg <mgregg@REDHAT.COM>
2011-10-06 02:14:16 +0000

    creating managedby from replication test
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@2162 aeb79ed8-e03d-0410-be94-af332b95afa2

---
+++ ipa-tests/beaker/ipa-server/acceptance/managedby/runtest.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+# vim: dict=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   runtest.sh of /CoreOS/ipa-tests/acceptance/replication
+#   Description: IPA replication acceptance tests
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# The following ipa will be tested:
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Author: Michael Gregg <mgregg@redhat.com>
+#   Date  : Sept 10, 2010
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2010 Red Hat, Inc. All rights reserved.
+#
+#   This copyrighted material is made available to anyone wishing
+#   to use, modify, copy, or redistribute it subject to the terms
+#   and conditions of the GNU General Public License version 2.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free
+#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#   Boston, MA 02110-1301, USA.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Include data-driven test data file:
+
+# Include rhts environment
+. /usr/bin/rhts-environment.sh
+. /usr/share/beakerlib/beakerlib.sh
+. /dev/shm/ipa-server-shared.sh
+. /dev/shm/env.sh
+. /dev/shm/lib.user-cli.sh
+. /dev/shm/ipa-group-cli-lib.sh
+. /dev/shm/ipa-host-cli-lib.sh
+. /dev/shm/ipa-hostgroup-cli-lib.sh
+. /dev/shm/ipa-netgroup-cli-lib.sh
+. /dev/shm/ipa-service-cli-lib.sh
+. /dev/shm/ipa-hbac-cli-lib.sh
+
+#Include the data file for the tests
+. ./data.replication
+. ./data.replication.master
+. ./data.replication.slave
+
+# Include test case file
+. ./t.replicationonmasterslave.sh
+. ./lib.replication.sh
+
+PACKAGE="ipa-admintools"
+
+startDate=`date "+%F %r"`
+satrtEpoch=`date "+%s"`
+hdir=/var/www/html
+
+setupApache()
+{
+	rm -Rf $hdir/rt
+	mkdir $hdir/rt
+	chmod 755 $hdir/rt
+	service httpd restart
+}
+##########################################
+#   test main 
+#########################################
+
+rlJournalStart
+    rlPhaseStartSetup "replication startup: Check for ipa-server package"
+        rlAssertRpm $PACKAGE
+        rlRun "TmpDir=\`mktemp -d\`" 0 "Creating tmp directory"
+        rlRun "pushd $TmpDir"
+    rlPhaseEnd
+
+	if [ -z $SLAVE ]; then
+		echo "ERROR - This test suite must be run on a setup involving at lease one master, and one replica server"
+		rlFail "This test suite must be run on a setup involving at lease one master, and one replica server"
+	else
+#		setupApache
+	        testReplicationOnMasterAndSlave
+	fi
+
+    rlPhaseStartCleanup "replication cleanup"
+        rlRun "popd"
+        rlRun "rm -r $TmpDir" 0 "Removing tmp directory"
+    rlPhaseEnd
+
+    makereport
+rlJournalEnd
+
+
