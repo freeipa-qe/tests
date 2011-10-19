ad77797b29acb50262c1a2c13aedd713c790610f
Yi Zhang <yzhang@REDHAT.COM>
2011-10-19 22:38:15 +0000

    initial check in, this is not runable at this point
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@2205 aeb79ed8-e03d-0410-be94-af332b95afa2

---
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-selfservice/runtest.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# vim: dict=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   runtest.sh of /CoreOS/ipa-tests/acceptance/ipa-selfservice
+#   Description: IPA ipa-selfservice acceptance tests
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# The following ipa will be tested:
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Author: Yi Zhang <yzhang@redhat.com>
+#   Date  : Sept 10, 2010
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2010 Red Hat, Inc. All rights reserved.
+#
+#   This copyrighted material is made available to anyone wishing
+#   to use, modify, copy, or redistribute it subject to the terms
+#   and conditions of the GNU General Public License version 2.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free
+#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#   Boston, MA 02110-1301, USA.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Include data-driven test data file:
+
+# Include rhts environment
+. /usr/bin/rhts-environment.sh
+. /usr/share/beakerlib/beakerlib.sh
+. /dev/shm/ipa-server-shared.sh
+. /dev/shm/env.sh
+
+# Include test case file
+. ./t.ipa-selfservice.sh
+
+PACKAGE="ipa-server"
+
+startDate=`date "+%F %r"`
+satrtEpoch=`date "+%s"`
+##########################################
+#   test main 
+#########################################
+
+rlJournalStart
+    rlPhaseStartSetup "ipa-selfservice startup: Check for ipa-server package"
+        rlAssertRpm $PACKAGE
+        rlRun "TmpDir=\`mktemp -d\`" 0 "Creating tmp directory"
+        rlRun "pushd $TmpDir"
+    rlPhaseEnd
+
+    # r2d2_test_starts
+    selfservice
+    # r2d2_test_ends
+
+    rlPhaseStartCleanup "ipa-selfservice cleanup"
+        rlRun "popd"
+        rlRun "rm -r $TmpDir" 0 "Removing tmp directory"
+    rlPhaseEnd
+
+    makereport
+rlJournalEnd
+
+
+ 
+# manifest:
+
+
