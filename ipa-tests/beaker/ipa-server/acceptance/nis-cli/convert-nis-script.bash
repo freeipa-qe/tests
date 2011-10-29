538cb0f32f2432d0f33b6b4654df44e7549d37c8
Michael Gregg <mgregg@REDHAT.COM>
2011-10-29 01:23:35 +0000

    added group section to convert script
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@2221 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/nis-cli/convert-nis-script.bash
+++ ipa-tests/beaker/ipa-server/acceptance/nis-cli/convert-nis-script.bash
@@ -23,6 +23,22 @@ ypcat -d $NISDOMAIN -h 127.0.0.1 passwd  | while read users; do
 	shell=$(echo $users | cut -d: -f7)
 	echo "user=$user uid=$uid gid=$gid home=$home shell=$shell"
 	ipa user-add --first=$user --last=$user --uid=$uid --gid=$gid --homedir=$home --shell=$shell --title=$title $user 
+	ipa user-del $user
+done
+	
+
+ypcat -d $NISDOMAIN -h 127.0.0.1 group  | while read groups; do
+	group=$(echo $groups | cut -d: -f1)
+	bang=$(echo $groups | cut -d: -f2)
+	gid=$(echo $groups | cut -d: -f3)
+	members=$(echo $groups | cut -d: -f4)
+	echo "group=$group gid=$gid members=$members"
+	ipa group-add --desc="$group" $group
+	ipa group-del $group
+	if [ ! -x $members ]; then
+		echo "there are members to this group that we should add"
+		echo ".... now what?"
+	fi 
 done
 	
 
