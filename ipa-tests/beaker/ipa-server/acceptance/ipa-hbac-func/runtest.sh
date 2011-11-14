a5c22fe55d3ff9c79aab47e80efdc78e2663e852
Gowrishankar Rajaiyan <grajaiya@REDHAT.COM>
2011-11-14 20:05:08 +0000

    fixing typos while adding dnsrecord
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@2251 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-hbac-func/runtest.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-hbac-func/runtest.sh
@@ -525,11 +525,11 @@ rlJournalStart
 
 		# Adding forward and reverse record.
 		rlRun "ipa dnsrecord-add $DOMAIN $BEAKERCLIENT_SH --a-rec=$BEAKERCLIENT_IP"
-		rlRun "ipa dnsrecore-add $REVERSE_ZONE $BEAKERCLIENT_PTR --ptr-rec=$CLIENT."
+		rlRun "ipa dnsrecord-add $REVERSE_ZONE $BEAKERCLIENT_PTR --ptr-rec=$CLIENT."
 
 		# Adding forward and reverse record.
 		rlRun "ipa dnsrecord-add $DOMAIN $BEAKERCLIENT2_SH --a-rec=$BEAKERCLIENT2_IP"
-		rlRun "ipa dnsrecore-add $REVERSE_ZONE $BEAKERCLIENT2_PTR --ptr-rec=$CLIENT2."
+		rlRun "ipa dnsrecord-add $REVERSE_ZONE $BEAKERCLIENT2_PTR --ptr-rec=$CLIENT2."
 
 
         	# kinit as admin and creating users
