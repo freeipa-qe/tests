21c85edcbfdae6034d140576656063ea1d6dad2c
Jenny Galipeau <jgalipea@REDHAT.COM>
2011-12-09 16:54:30 +0000

    changing package check to be for server not admintools
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@2311 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-ctl/runtest.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-ctl/runtest.sh
@@ -40,7 +40,7 @@
 . /dev/shm/env.sh
 
 
-PACKAGE="ipa-admintools"
+PACKAGE="ipa-server"
 
 ##########################################
 getServicePIDs()
@@ -65,9 +65,9 @@ rlJournalStart
     rlPhaseStartSetup "ipa-ctl-setup: Check for ipa-server package and add test user"
         rpm -qa | grep $PACKAGE
         if [ $? -eq 0 ] ; then
-                rlPass "ipa-admintools package is installed"
+                rlPass "ipa-server package is installed"
         else
-                rlFail "ipa-admintools package NOT found!"
+                rlFail "ipa-server package NOT found!"
         fi
 	rlRun "useradd testuserqa" 0 "Add test user"
 	# get initial service pids
