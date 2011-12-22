2d0ea568da22ea29378b8085b9fec4d8854df97d
Gowrishankar Rajaiyan <grajaiya@REDHAT.COM>
2011-12-22 11:29:13 +0000

    fixing getting IP command
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@2371 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-ca-install/t.ca-install.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-ca-install/t.ca-install.sh
@@ -70,13 +70,14 @@ installMaster()
                 if [ "$s" != "" ]; then
                         # Determine the IP of the slave to be used when creating the replica file.
                         ipofs=$(dig +noquestion $s  | grep $s | grep IN | grep A | awk '{print $5}')
+			ipofs2=`cat /etc/hosts | grep $s | cut -f 1 -d " "`
 
                         # put the short form of the hostname for server $s into s_short
                         hostname_s=$(echo $s | cut -d. -f1)
 
-                        rlLog "IP of server $s is resolving as $ipofs, using short hostname of $hostname_s"
-                        rlLog "Running: ipa-replica-prepare -p $ADMINPW --ip-address=$ipofs $hostname_s.$DOMAIN"
-                        rlRun "ipa-replica-prepare -p $ADMINPW --ip-address=$ipofs $hostname_s.$DOMAIN" 0 "Creating replica package"
+                        rlLog "IP of server $s is resolving as $ipofs2, using short hostname of $hostname_s"
+                        rlLog "Running: ipa-replica-prepare -p $ADMINPW --ip-address=$ipofs2 $hostname_s.$DOMAIN"
+                        rlRun "ipa-replica-prepare -p $ADMINPW --ip-address=$ipofs2 $hostname_s.$DOMAIN" 0 "Creating replica package"
                         rlRun "service named restart" 0 "Restarting named as work around when adding new reverse zone"
 
                 else
