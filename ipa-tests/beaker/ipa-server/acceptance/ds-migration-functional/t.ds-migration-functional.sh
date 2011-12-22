62c1c0daa6156e5906b286590785e6ced8938dcf
Jenny Galipeau <jgalipea@REDHAT.COM>
2011-12-22 17:06:08 +0000

    fixing ssh command in cleanup
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@2374 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ds-migration-functional/t.ds-migration-functional.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ds-migration-functional/t.ds-migration-functional.sh
@@ -112,7 +112,7 @@ hashedpwdmigration_http()
 cleanup()
 {
 	rlPhaseStartTest "CLEANUP FUNCTIONAL TESTING"
-		rlRun "ssh root@$CLIENT /usr/sbin/remove-ds.pl -i $INSTANCE" 0 "Removing directory server instance"
+		rlRun "ssh -o StrictHostKeyChecking=no root@$CLIENT /usr/sbin/remove-ds.pl -i $INSTANCE" 0 "Removing directory server instance"
 		rlRun "SetMigrationConfig FALSE" 0 "Set migration mode to FALSE"
 	rlPhaseEnd
 }
