2656722c0663cd04badf5dd6d96e021d85f8b69b
Gowrishankar Rajaiyan <grajaiya@REDHAT.COM>
2012-01-19 10:16:54 +0000

    initial revision for ipa-replica-install
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@2442 aeb79ed8-e03d-0410-be94-af332b95afa2

---
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-replica-install/t.replica-install.sh
@@ -0,0 +1,287 @@
+#!/bin/bash
+# vim: dict=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   runtest.sh of /CoreOS/ipa-tests/acceptance/ipa-replica-install
+#   Description: IPA Replica install tests
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Authors: 
+#	     Gowrishankar Rajaiyan <gsr@redhat.com>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2010 Red Hat, Inc. All rights reserved.
+#
+#   This copyrighted material is made available to anyone wishing
+#   to use, modify, copy, or redistribute it subject to the terms
+#   and conditions of the GNU General Public License version 2.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free
+#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#   Boston, MA 02110-1301, USA.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Include data-driven test data file:
+
+# Include rhts environment
+. /usr/bin/rhts-environment.sh
+. /usr/share/beakerlib/beakerlib.sh
+. /dev/shm/ipa-server-shared.sh
+. ./install-lib.sh
+
+
+installMaster()
+{
+   rlPhaseStartTest "Install IPA MASTER Server"
+
+        rlRun "/etc/init.d/ntpd stop" 0 "Stopping the ntp server"
+        rlRun "ntpdate $NTPSERVER" 0 "Synchronzing clock with valid time server"
+        rlRun "fixHostFile" 0 "Set up /etc/hosts"
+	rlRun "fixhostname" 0 "Fix hostname"
+
+        # Determine the IP of the slave to be used when creating the replica file.
+        ipofs=$(dig +noquestion $SLAVE  | grep $SLAVE | grep IN | grep A | awk '{print $5}')
+	rlLog "IP address of SLAVE: $SLAVE is $ipofs"
+
+	rlRun "yum install -y ipa-server bind-dyndb-ldap bind"
+	echo "ipa-server-install --setup-dns --forwarder=$DNSFORWARD --hostname=$hostname_s.$DOMAIN -r $RELM -n $DOMAIN -p $ADMINPW -P $ADMINPW -a $ADMINPW -U" > /dev/shm/installipa.bash
+
+	rlLog "EXECUTING: ipa-server-install --setup-dns --forwarder=$DNSFORWARD --hostname=$hostname_s.$DOMAIN -r $RELM -n $DOMAIN -p $ADMINPW -P $ADMINPW -a $ADMINPW -U"
+
+        rlRun "setenforce 1" 0 "Making sure selinux is enforced"
+        rlRun "chmod 755 /dev/shm/installipa.bash" 0 "Making ipa install script executable"
+        rlRun "/bin/bash /dev/shm/installipa.bash" 0 "Installing IPA Server"
+
+        if [ -f /var/log/ipaserver-install.log ]; then
+                rhts-submit-log -l /var/log/ipaserver-install.log
+        fi
+
+	rlRun "service ipa status"
+   rlPhaseEnd
+}
+
+createReplica1()
+{
+
+   rlPhaseStartTest "Create Replica Package(s) without --ip-address option"
+        for s in $SLAVE; do
+                if [ "$s" != "" ]; then
+
+                        # put the short form of the hostname for server $s into s_short
+                        hostname_s=$(echo $s | cut -d. -f1)
+
+			# Preparing replica without --ip-address option
+			rlRun "ipa dnsrecord-add $DOMAIN --a-rec=$ipofs2"
+                        rlLog "Running: ipa-replica-prepare -p $ADMINPW $hostname_s.$DOMAIN"
+			rlRun "ipa-replica-prepare -p $ADMINPW $hostname_s.$DOMAIN"
+                        rlRun "service named restart" 0 "Restarting named as work around when adding new reverse zone"
+
+                else
+
+                        rlLog "No SLAVES in current recipe set."
+
+                fi
+        done
+
+   rlPhaseEnd
+}
+
+createReplica2() 
+{
+
+   rlPhaseStartTest "Create Replica Package(s) with --ip-address option"
+        for s in $SLAVE; do
+                if [ "$s" != "" ]; then
+
+			# Preparing replica with --ip-address option
+                        rlLog "IP of server $s is resolving as $ipofs, using short hostname of $hostname_s"
+                        rlLog "Running: ipa-replica-prepare -p $ADMINPW --ip-address=$ipofs2 $hostname_s.$DOMAIN"
+                        rlRun "ipa-replica-prepare -p $ADMINPW --ip-address=$ipofs $hostname_s.$DOMAIN" 0 "Creating replica package"
+                        rlRun "service named restart" 0 "Restarting named as work around when adding new reverse zone"
+
+                else
+
+                        rlLog "No SLAVES in current recipe set."
+
+                fi
+        done
+
+   rlPhaseEnd
+}
+
+createReplica3()
+{
+   rlPhaseStartTest "Create Replica Package(s) with pkcs#12 options"
+        for s in $SLAVE; do
+                if [ "$s" != "" ]; then
+
+			# Preparing replica with pkcs#12 options
+
+			http_nss_cert_db_pin=`cat /etc/httpd/alias/pwdfile.txt`
+			dirsrv_nss_cert_db_pin=`cat /etc/dirsrv/slapd-PKI-IPA/pwdfile.txt`
+
+			rlLog "Executing: /usr/bin/pk12util -o http_pkcs.p12 -d /etc/httpd/alias/ -n Server-Cert"
+
+			expfile=/tmp/pk12util.exp
+			expout=/tmp/pk12util.out
+
+			rm -rf $expfile $expout
+
+echo 'set timeout 30
+set send_slow {1 .1}' > $expfile
+echo "spawn /usr/bin/pk12util -o http_pkcs.p12 -d /etc/httpd/alias/ -n Server-Cert" >> $expfile
+echo 'match_max 100000' >> $expfile
+echo 'expect "*: "' >> $expfile
+echo 'sleep .5' >> $expfile
+echo "send -s -- \"$http_nss_cert_db_pin\"" >> $expfile
+echo 'send -s -- "\r"' >> $expfile
+echo 'expect "*: "' >> $expfile
+echo 'sleep .5' >> $expfile
+echo "send -s -- \"$ADMINPW\"" >> $expfile
+echo 'send -s -- "\r"' >> $expfile
+echo 'expect "*: "' >> $expfile
+echo 'sleep .5' >> $expfile
+echo "send -s -- \"$ADMINPW\"" >> $expfile
+echo 'send -s -- "\r"' >> $expfile
+echo 'expect eof ' >> $expfile
+
+		        rlLog "Constructed expect file as:"
+        	        rlRun "/bin/cat $expfile"
+		        rlLog "Executing: /usr/bin/expect $expfile >> $expout 2>&1"
+        	        rlRun "/usr/bin/expect $expfile >> $expout 2>&1"
+		        rlLog "pk12util command output:"
+                	rlRun "/bin/cat $expout"
+
+			rlAssertGrep "pk12util: PKCS12 EXPORT SUCCESSFUL" "$expout"
+
+
+			rlLog "Executing: /usr/bin/pk12util -o dirsrv_pkcs.p12 -d /etc/dirsrv/slapd-PKI-IPA/ -n Server-Cert"
+
+                        rm -rf $expfile $expout
+                        
+echo 'set timeout 30
+set send_slow {1 .1}' > $expfile
+echo "spawn /usr/bin/pk12util -o dirsrv_pkcs.p12 -d /etc/dirsrv/slapd-PKI-IPA/ -n Server-Cert" >> $expfile
+echo 'match_max 100000' >> $expfile
+echo 'expect "*: "' >> $expfile
+echo 'sleep .5' >> $expfile
+echo "send -s -- \"$dirsrv_nss_cert_db_pin\"" >> $expfile
+echo 'send -s -- "\r"' >> $expfile
+echo 'expect "*: "' >> $expfile
+echo 'sleep .5' >> $expfile
+echo "send -s -- \"$ADMINPW\"" >> $expfile
+echo 'send -s -- "\r"' >> $expfile
+echo 'expect "*: "' >> $expfile
+echo 'sleep .5' >> $expfile
+echo "send -s -- \"$ADMINPW\"" >> $expfile
+echo 'send -s -- "\r"' >> $expfile
+echo 'expect eof ' >> $expfile
+        
+                        rlLog "Constructed expect file as:"
+                        rlRun "/bin/cat $expfile"
+                        rlLog "Executing: /usr/bin/expect $expfile >> $expout 2>&1"
+                        rlRun "/usr/bin/expect $expfile >> $expout 2>&1"
+                        rlLog "pk12util command output:"
+                        rlRun "/bin/cat $expout"
+
+                        rlAssertGrep "pk12util: PKCS12 EXPORT SUCCESSFUL" "$expout"
+
+
+			rlLog "IP of server $s is resolving as $ipofs, using short hostname of $hostname_s"
+			rlLog "Executing: ipa-replica-prepare -p $ADMINPW --ip-address=$ipofs $hostname_s.$DOMAIN --dirsrv_pkcs12=dirsrv_pkcs.p12 --dirsrv_pin=Secret123 --http_pkcs12=http_pkcs.p12 --http_pin=Secret123"
+			rlRun "ipa-replica-prepare -p $ADMINPW --ip-address=$ipofs $hostname_s.$DOMAIN --dirsrv_pkcs12=dirsrv_pkcs.p12 --dirsrv_pin=Secret123 --http_pkcs12=http_pkcs.p12 --http_pin=Secret123"
+                        rlRun "service named restart" 0 "Restarting named as work around when adding new reverse zone"
+
+                else
+
+                        rlLog "No SLAVES in current recipe set."
+
+                fi
+        done
+
+        rlRun "appendEnv" 0 "Append the machine information to the env.sh with the information for the machines in the recipe set"
+
+        # stop the firewall
+        service iptables stop
+        service ip6tables stop
+
+   rlPhaseEnd
+
+}
+
+	
+installSlave()
+{
+   rlPhaseStartSetup "Install IPA REPLICA Server"
+	
+	yum install -y openssh-clients
+	yum install -y ipa-server bind-dyndb-ldap bind
+        
+	rlRun "/etc/init.d/ntpd stop" 0 "Stopping the ntp server"
+        # stop the firewall
+        service iptables stop
+        service ip6tables stop
+        rlRun "ntpdate $NTPSERVER" 0 "Synchronzing clock with valid time server"
+        rlRun "AddToKnownHosts $MASTER" 0 "Adding master to known hosts"
+        cd /dev/shm/
+        hostname_s=$(hostname -s)
+        rlRun "sftp root@$MASTER:/var/lib/ipa/replica-info-$hostname_s.$DOMAIN.gpg" 0 "Get replica package"
+        rlLog "sftp root@$MASTER:/var/lib/ipa/replica-info-$hostname_s.$DOMAIN.gpg"
+        rlLog "Checking for existance of replica gpg file"
+        ls /dev/shm/replica-info-$hostname_s.$DOMAIN.gpg
+        if [ $? -ne 0 ] ; then
+                rlFail "ERROR: Replica Package not found"
+        else
+                rlRun "/etc/init.d/ntpd stop" 0 "Stopping the ntp server"
+                rlRun "ntpdate $NTPSERVER" 0 "Synchronzing clock with valid time server"
+                rlLog "SKIPINSTALL: $SKIPINSTALL"       
+                rlRun "fixResolv" 0 "fixing the reoslv.conf to contain the correct nameserver lines"
+                rlRun "fixHostFile" 0 "Set up /etc/hosts"
+
+                rlRun "fixhostname" 0 "Fix hostname"
+                echo "ipa-replica-install -U --setup-dns --forwarder=$DNSFORWARD -w $ADMINPW -p $ADMINPW /dev/shm/replica-info-$hostname_s.$DOMAIN.gpg" > /dev/shm/replica-install.bash
+                chmod 755 /dev/shm/replica-install.bash
+                rlLog "EXECUTING: ipa-replica-install -U --setup-dns --forwarder=$DNSFORWARD -w $ADMINPW -p $ADMINPW /dev/shm/replica-info-$hostname_s.$DOMAIN.gpg"
+                rlRun "/bin/bash /dev/shm/replica-install.bash" 0 "Replica installation"
+                rlRun "kinitAs $ADMINID $ADMINPW" 0 "Testing kinit as admin"
+
+                rlRun "appendEnv" 0 "Append the machine information to the env.sh with the information for the machines in the recipe set"
+        fi
+
+        if [ -f /var/log/ipareplica-install.log ]; then
+                rhts-submit-log -l /var/log/ipareplica-install.log
+        fi
+        # stop the firewall
+        service iptables stop
+        service ip6tables stop
+   rlPhaseEnd
+ 
+}
+
+
+installCA()
+{
+
+   rlPhaseStartSetup "Installing CA Replica"
+
+	rlLog "Executing: ipa-ca-install -p $ADMINPW -w $ADMINPW --skip-conncheck --unattended /dev/shm/replica-info-$hostname_s.$DOMAIN.gpg"
+	echo "ipa-ca-install -p $ADMINPW -w $ADMINPW --skip-conncheck --unattended /dev/shm/replica-info-$hostname_s.$DOMAIN.gpg" > /dev/shm/replica-ca-install.bash
+	chmod 755 /dev/shm/replica-ca-install.bash
+	rlRun "kinitAs $ADMINID $ADMINPW" 0 "Testing kinit as admin"
+	rlRun "/bin/bash /dev/shm/replica-ca-install.bash" 0 "CA Replica installation"
+
+	if [ -f /var/log/ipareplica-ca-install.log ]; then
+		rhts-submit-log -l /var/log/ipareplica-ca-install.log
+	fi
+
+}
