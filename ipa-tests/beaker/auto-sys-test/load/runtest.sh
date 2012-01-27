dee394f4acb8eebc74a2f65f84fbce7425ce1517
Michael Gregg <mgregg@REDHAT.COM>
2012-01-27 19:50:52 +0000

    adding first postfix-setup files
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@2495 aeb79ed8-e03d-0410-be94-af332b95afa2

---
+++ ipa-tests/beaker/auto-sys-test/load/runtest.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+# vim: dict=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   runtest.sh of /CoreOS/tests/automation-and-system-test/load/postfix-setup
+#   Description: IPA nis-cli acceptance tests
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# The following ipa will be tested:
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Author: Michael Gregg <mgregg@redhat.com>
+#   Date  : Sept 10, 2010
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2010 Red Hat, Inc. All rights reserved.
+#
+#   This copyrighted material is made available to anyone wishing
+#   to use, modify, copy, or redistribute it subject to the terms
+#   and conditions of the GNU General Public License version 2.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free
+#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#   Boston, MA 02110-1301, USA.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Include data-driven test data file:
+
+# Include rhts environment
+. /usr/bin/rhts-environment.sh
+. /usr/share/beakerlib/beakerlib.sh
+. /dev/shm/env.sh
+
+# Include test case file
+. ./t.postfix-setup.sh
+
+rlJournalStart
+	HOSTNAME=$(hostname -s)
+        myhostname=`hostname`
+        rlLog "hostname command: $myhostname"
+        rlLog "HOSTNAME: $HOSTNAME"
+        rlLog "MASTER: $MASTER"
+        rlLog "SLAVE: $SLAVE"
+        rlLog "CLIENT: $CLIENT"
+
+        #####################################################################
+        #               IS THIS MACHINE A MASTER?                           #
+        #####################################################################
+        rc=0
+        echo $MASTER | grep $HOSTNAME
+        rc=$?
+        if [ $rc -eq 0 ] ; then
+		pwdfile=/dev/shm/password.txt
+		echo $ADMINPW > $pwdfile
+		yum -y install postfix cyrus-imapd-utils cyrus-imapd
+		/etc/init.d/sendmail stop
+		/sbin/chkconfig --levels 2345 sendmail off
+		/sbin/chkconfig --levels 2345 postfix on
+		setup-dns
+		setup-postfix
+        	cleanup
+        else
+                rlLog "Machine in recipe is not the MASTER - not running setup"
+        fi
+
+	rhts-sync-set -s READY
+
+   rlJournalPrintText
+   report=/tmp/rhts.report.$RANDOM.txt
+   makereport $report
+   rhts-submit-log -l $report
+rlJournalEnd
+
