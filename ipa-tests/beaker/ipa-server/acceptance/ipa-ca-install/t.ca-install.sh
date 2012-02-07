bb63c0ea1e058b216533ef3c21646b622ef01806
Gowrishankar Rajaiyan <grajaiya@REDHAT.COM>
2012-02-07 06:40:07 +0000

    fixing beaker issues
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@2598 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-ca-install/t.ca-install.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-ca-install/t.ca-install.sh
@@ -188,10 +188,10 @@ echo 'expect eof ' >> $expfile
 
 	rlRun "nslookup $MASTER"
 	rlRun "nslookup $SLAVE"
-	rlLog "Executing: ipa-ca-install -d -p $ADMINPW -w $ADMINPW --skip-conncheck --unattended --no-host-dns /dev/shm/replica-info-$hostname_s.$DOMAIN.gpg"
+	rlLog "Executing: ipa-ca-install -p $ADMINPW -w $ADMINPW --skip-conncheck --unattended --no-host-dns /dev/shm/replica-info-$hostname_s.$DOMAIN.gpg"
 
 	rlLog "Verifying bug https://bugzilla.redhat.com/show_bug.cgi?id=757681"
-	echo "ipa-ca-install -d -p $ADMINPW -w $ADMINPW --skip-conncheck --unattended --no-host-dns /dev/shm/replica-info-$hostname_s.$DOMAIN.gpg" > /dev/shm/replica-ca-install.bash
+	echo "ipa-ca-install -p $ADMINPW -w $ADMINPW --skip-conncheck --unattended --no-host-dns /dev/shm/replica-info-$hostname_s.$DOMAIN.gpg" > /dev/shm/replica-ca-install.bash
         chmod 755 /dev/shm/replica-ca-install.bash
         rlRun "kinitAs $ADMINID $ADMINPW" 0 "Testing kinit as admin"
         rlRun "/bin/bash /dev/shm/replica-ca-install.bash" 0 "CA Replica installation with --no-host-dns"
