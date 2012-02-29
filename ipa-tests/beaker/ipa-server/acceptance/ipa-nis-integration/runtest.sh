b64ba05683493e58457e948e49b15cc639687f5c
Scott Poore <spoore@REDHAT.COM>
2012-02-29 20:28:36 +0000

    ipa-nis-integration: re-enable tests after migration
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@2857 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-nis-integration/runtest.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-nis-integration/runtest.sh
@@ -138,11 +138,11 @@ rlJournalStart
 	# NIS Migration
 	##############################################################
 	nisint_nisclient_migration # Switch NIS Client to use SSSD/IPA
-	#nisint_user_tests
-	#nisint_group_tests
-	#nisint_netgroup_tests
-	#nisint_automount_tests
-	#nisint_client_is_ipa_bz_tests
+	nisint_user_tests
+	nisint_group_tests
+	nisint_netgroup_tests
+	nisint_automount_tests
+	nisint_client_is_ipa_bz_tests
 
 	nisint_end
 		
