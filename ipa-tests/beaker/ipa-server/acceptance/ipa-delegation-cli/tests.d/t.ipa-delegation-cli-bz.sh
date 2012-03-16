fda1dd195bf2954dd0af02cd19038898e50cef0d
Scott Poore <spoore@REDHAT.COM>
2012-03-16 21:23:16 +0000

    ipa-delegation-cli: 1) added delegation_bz_784468 to delegation_bz function so that test gets executed
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@2993 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-delegation-cli/tests.d/t.ipa-delegation-cli-bz.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-delegation-cli/tests.d/t.ipa-delegation-cli-bz.sh
@@ -59,6 +59,7 @@ delegation_bz()
 	delegation_bz_783548
 	delegation_bz_783554
 	delegation_bz_782974
+	delegation_bz_784468
 }
 
 ######################################################################
