b7771cc94516eb5eefb67bf4b3826b9d80663894
Asha Akkiangady <aakkiang@REDHAT.COM>
2012-03-29 16:38:09 +0000

    New tests for user-mod --rename
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@3107 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-user-cli/moduser/runtest.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-user-cli/moduser/runtest.sh
@@ -565,9 +565,8 @@ rlJournalStart
 
     rlPhaseStartTest "ipa-user-cli-mod-068: Rename a user that does not exist"
         command="ipa user-mod --rename=new_user_name doesntexist"
-        rlAssertGrep "Group name: $newname" "/tmp/rename_upg1.out"
         expmsg="ipa: ERROR: doesntexist: user not found
-        rlRun "verifyErrorMsg \"$command\" \"$expmsg\"" 0 "Verify expected error message for $rename_user"
+        rlRun "verifyErrorMsg \"$command\" \"$expmsg\"" 0 "Verify expected error message for user doesntexist"
     rlPhaseEnd
 
     rlPhaseStartCleanup "ipa-user-cli-mod-cleanup"
