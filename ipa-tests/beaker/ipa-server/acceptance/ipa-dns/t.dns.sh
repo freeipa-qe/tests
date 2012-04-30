e5e2f193d034cacb041ae91b3b7c9dd3d79df660
Jenny Galipeau <jgalipea@REDHAT.COM>
2012-04-30 17:50:26 +0000

    increasing time out for replica prepare test 4 still not long enough for 32 bit server executions
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@3331 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-dns/t.dns.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-dns/t.dns.sh
@@ -125,7 +125,7 @@ dnsreplicaprepare()
 	rlPhaseEnd
 
 	rlPhaseStartTest "ipa-dns-replicaprepare-04 check reverse entry of the replica was created in dns correctly with dig"
-		sleep 5
+		sleep 15
 		rlLog "EXECUTING: dig -x $newfakehostip | grep newfakehost$newip.$DOMAIN"
 		rlRun "dig -x $newfakehostip | grep newfakehost$newip.$DOMAIN" 0 "Checking to ensure that reverse of newfakehost$newip.$DOMAIN is set up correctly"
 	rlPhaseEnd
