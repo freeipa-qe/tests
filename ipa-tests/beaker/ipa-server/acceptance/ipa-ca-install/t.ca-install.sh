6e1c8efb751b84f182e0bd0e0b362126f59866e2
Gowrishankar Rajaiyan <grajaiya@REDHAT.COM>
2012-05-31 10:07:13 +0000

    Clear all dns records using --dns-all
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@3572 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-ca-install/t.ca-install.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-ca-install/t.ca-install.sh
@@ -171,7 +171,7 @@ echo 'send "\r"' >> $expfile
 echo 'expect "*d: "' >> $expfile
 echo "send \"$ADMINPW\"" >> $expfile
 echo 'send "\r"' >> $expfile
-echo "send \"ipa dnsrecord-del $FORWARD_ZONE `hostname -s` --a-rec=$SLAVEIP\"" >> $expfile
+echo "send \"ipa dnsrecord-del $FORWARD_ZONE `hostname -s` --del-all\"" >> $expfile
 echo 'send "\r"' >> $expfile
 echo 'expect "#: "' >> $expfile
 echo "send \"service named restart\"" >> $expfile
