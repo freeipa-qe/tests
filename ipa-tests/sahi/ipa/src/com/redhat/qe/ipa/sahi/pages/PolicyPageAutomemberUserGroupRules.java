8d7171a154e394be87bc60b73cc726dad2b28de3
Xiyang Dong <xdong@REDHAT.COM>
2012-07-09 18:15:12 +0000

    Adding user group page for automember
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@3749 aeb79ed8-e03d-0410-be94-af332b95afa2

---
+++ ipa-tests/sahi/ipa/src/com/redhat/qe/ipa/sahi/pages/PolicyPageAutomemberUserGroupRules.java
@@ -0,0 +1,35 @@
+package com.redhat.qe.ipa.sahi.pages;
+
+import java.util.*;
+import java.util.logging.Logger;
+
+import com.redhat.qe.ipa.sahi.tasks.CommonTasks;
+import com.redhat.qe.ipa.sahi.tasks.SahiTasks;
+
+public class PolicyPageAutomemberUserGroupRules extends IPAWebPage {
+	private static Logger log = Logger.getLogger(PolicyPageAutomemberUserGroupRules.class.getName());
+	private static String url = CommonTasks.automemberUserGroupPage;
+	
+	public PolicyPageAutomemberUserGroupRules (SahiTasks browser, String testPropertyFile)
+	{
+		super(browser, url, testPropertyFile);
+		backLink = "User group rules";
+		duplicateErrorMsgStartsWith = "User Group Rule with name";
+		
+		addPage = "Add Automember User";//when using "Add Automember User" in the properties,it will use all the methods that contain pageName=addPage,and the methods are defined in standardTests
+		duplicatePage = "Add Duplicate Automember User";
+		modifySettingsPage = "Modify Automember User";
+		modifyNegativePage = "Modify Automember User Negative";
+		modifyConditionInclusiveAddPage = "Modify Automember Condition User Inclusive Add";
+		modifyConditionInclusiveDeletePage ="Modify Automember Condition User Inclusive Delete";
+		modifyConditionExclusiveAddPage ="Modify Automember Condition User Exclusive Add";
+		modifyConditionExclusiveDeletePage ="Modify Automember Condition User Exclusive Delete";
+		setDefaultGroupPage = "Set Default User Group";
+		delPage = "Delete Automember User"; 
+	
+		
+		registerStandardTestCases();
+		System.out.println("New instance of " + PolicyPageAutomemberUserGroupRules.class.getName() + " is ready");
+	}
+
+}
