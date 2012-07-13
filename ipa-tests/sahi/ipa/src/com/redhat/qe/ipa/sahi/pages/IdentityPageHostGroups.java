eeb81117275235587122d59df38820ce454bf9be
Xiyang Dong <xdong@REDHAT.COM>
2012-07-13 20:01:15 +0000

    some page modifications
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@3797 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/sahi/ipa/src/com/redhat/qe/ipa/sahi/pages/IdentityPageHostGroups.java
+++ ipa-tests/sahi/ipa/src/com/redhat/qe/ipa/sahi/pages/IdentityPageHostGroups.java
@@ -17,11 +17,26 @@ public class IdentityPageHostGroups extends IPAWebPage{
 		duplicateErrorMsgStartsWith = "host group with name";
 		backLink = "Host Groups";
 		addPage = "Add Host Group";
+		addOnePage = "Add One Host Group";
 		duplicatePage = "Add Duplicate Host Group";
 		modifySettingsPage = "Modify Host Group";
 		delPage = "Delete Host Group";
 
 		registerStandardTestCases();
+		//registerTestCases("add","addSingle");//xdong for automember use
+		//registerTestCases("add","addAndAddAnother");//xdong for automember use
+		//registerTestCases("add","addThenEdit");//xdong for automember use
+		//registerTestCases("add","addThenCancel");//xdong for automember use
+		//registerTestCases("add","addOne");//xdong for automember use
+		//registerTestCases("modify","modifyConditionInclusiveAdd");//xdong for automember use
+		//registerTestCases("modify","modifyConditionInclusiveDelete");//xdong for automember use
+		//registerTestCases("modify","modifyConditionExclusiveAdd");//xdong for automember use
+		//registerTestCases("modify","modifyConditionExclusiveDelete");//xdong for automember use
+		//registerTestCases("modify","setDefaultGroup");//xdong for automember use
+		//registerTestCases("modify","modifyUpdateResetCancel");//xdong for automember use
+		//registerTestCases("delete","deleteSingle");//xdong for automember use
+		//registerTestCases("delete","deleteMultiple");//xdong for automember use
+		
 		System.out.println("New instance of " + IdentityPageHostGroups.class.getName() + " is ready"); 
 	} 
 }
