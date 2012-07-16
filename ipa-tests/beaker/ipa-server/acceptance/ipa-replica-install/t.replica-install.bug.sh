25a473557bc1ecc83c70d877d7063248e09e9417
Scott Poore <spoore@REDHAT.COM>
2012-07-16 14:34:57 +0000

    ipa-replica-install: fix grep in replicaBugCheck_bz830314...was space, should have been tab, using dot star
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@3806 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-replica-install/t.replica-install.bug.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-replica-install/t.replica-install.bug.sh
@@ -129,7 +129,7 @@ replicaBugCheck_bz830314()
 {
 	rlPhaseStartTest "replicaBugCheck_bz830314 - ipa-replica-install named failed to start"
 		if [ -f /var/log/ipareplica-install.log ]; then
-			if [ $(grep "Starting named: [FAILED]" /var/log/ipareplica-install.log |wc -l) -gt 0 ]; then
+			if [ $(grep "Starting named:.*[FAILED]" /var/log/ipareplica-install.log |wc -l) -gt 0 ]; then
 				rlFail "BZ 830314 found...ipa-replica-install named failed to start"
 				rlRun "sed -n '/restarting named/,/7\/8/p' /var/log/ipareplica-install.log" 
 			else
@@ -139,6 +139,6 @@ replicaBugCheck_bz830314()
 			rlFail "Cannot find ipareplica-install.log to check BZ 830314"
 		fi
 		####### debugging here #################
-		rlRun "grep named /var/log/ipareplica-install.log"
+		#rlRun "grep named /var/log/ipareplica-install.log"
 	rlPhaseEnd
 }
