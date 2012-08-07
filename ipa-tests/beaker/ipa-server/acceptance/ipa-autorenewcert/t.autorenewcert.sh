504c07a9091d45e6cdb5bf465270e5390cc5fd4b
Yi Zhang <yzhang@REDHAT.COM>
2012-08-07 16:43:34 +0000

    initial checkin, test suite is not runable
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@3975 aeb79ed8-e03d-0410-be94-af332b95afa2

---
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-autorenewcert/t.autorenewcert.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+######################################################
+# test suite  : ipa-autorenewcert
+# description : IPA auto-renew certificate test suite
+# created by  : Yi Zhang (yzhang@redhat.com)
+# created date: Tue Aug  7 09:37:45 PDT 2012
+######################################################
+
+autorenewcert()
+{
+
+}
