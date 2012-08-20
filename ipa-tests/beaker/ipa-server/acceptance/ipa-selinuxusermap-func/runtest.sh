e46eb4450c02d2659eddf263098319da45c17d0b
Jenny Galipeau <jgalipea@REDHAT.COM>
2012-08-20 21:32:40 +0000

    adding functional tests
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@4061 aeb79ed8-e03d-0410-be94-af332b95afa2

---
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-selinuxusermap-func/runtest.sh
@@ -0,0 +1,290 @@
+#!/bin/bash
+# vim: dict=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   runtest.sh of /CoreOS/ipa-tests/acceptance/ipa-selinuxusermap-func
+#   Description: IPA Selinuxusermap Func acceptance tests
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Author: Asha Akkiangady <aakkiang@redhat.com>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2010 Red Hat, Inc. All rights reserved.
+#
+#   This copyrighted material is made available to anyone wishing
+#   to use, modify, copy, or redistribute it subject to the terms
+#   and conditions of the GNU General Public License version 2.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free
+#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#   Boston, MA 02110-1301, USA.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+##########################################################################
+
+# Include rhts environment
+. /usr/bin/rhts-environment.sh
+. /usr/share/beakerlib/beakerlib.sh
+. /dev/shm/ipa-host-cli-lib.sh
+. /dev/shm/ipa-group-cli-lib.sh
+. /dev/shm/ipa-hostgroup-cli-lib.sh
+. /dev/shm/ipa-hbac-cli-lib.sh
+. /dev/shm/ipa-selinuxusermap-cli-lib.sh
+. /dev/shm/ipa-server-shared.sh
+. /dev/shm/env.sh
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Include tests file
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+########################################################################
+# Test Suite Globals
+########################################################################
+
+REALM=`os_getdomainname | tr "[a-z]" "[A-Z]"`
+DOMAIN=`os_getdomainname`
+
+user1="user1"
+user2="user2"
+user3="user3"
+
+########################################################################
+
+#Checking hostnames of all hosts
+echo "The hostname of IPA Server is $MASTER"
+echo "The hostname of IPA Client 1 is $CLIENT"
+echo "The hostname of IPA Client 2 is $CLIENT2"
+
+echo "The beaker hostname of IPA Server is $BEAKERMASTER"
+echo "The beaker hostname of IPA Client 1 is $BEAKERCLIENT"
+echo "The beaker hostname of IPA Client 2 is $BEAKERCLIENT2"
+
+cat /dev/shm/env.sh
+########################################################################
+
+
+
+PACKAGELIST="ipa-admintools ipa-client httpd mod_nss mod_auth_kerb 389-ds-base expect"
+
+rlJournalStart
+        #####################################################################
+        #               IS THIS MACHINE CLIENT1?                            #
+        #####################################################################
+        rc=0
+
+	echo "Hostname of this machine is $HOSTNAME"
+	echo "Hostname of client is $CLIENT"
+
+        echo $CLIENT | grep $HOSTNAME
+        if [ $? -eq 0 ] ; then
+
+	rlPhaseStartSetup "ipa-selinuxusermapsvc-func: Checking client"
+                rlLog "Machine in recipe is CLIENT"
+                rlRun "service iptables stop" 0 "Stop the firewall on the client"
+		rlRun "yum install -y ftp"
+		rlRun "cat /etc/krb5.conf"
+		rlRun "authconfig --enablemkhomedir --updateall"
+
+                rlRun "rhts-sync-block -s DONE_master_setup $BEAKERMASTER"
+                rlRun "rhts-sync-set -s DONE_client1_setup -m $BEAKERCLIENT"
+
+		# Adding the next lines because this test will stall forever if $BEAKERCLIENT or $BEAKERCLIENT2 are empty
+		if [ -x $BEAKERCLIENT ]; then
+			echo "ERROR - This test must be run on a config with \$BEAKERCLIENT defined"
+			rlFail "ERROR - This test must be run on a config with \$BEAKERCLIENT defined"
+			exit
+		fi
+		if [ -x $BEAKERCLIENT2 ]; then
+			echo "ERROR - This test must be run on a config with \$BEAKERCLIENT2 defined"
+			rlFail "ERROR - This test must be run on a config with \$BEAKERCLIENT2 defined"
+			exit
+		fi
+		MASTER_IP=`nslookup $MASTER | grep Address | grep -v "#" | awk '{print $2}'`
+		BEAKERCLIENT_IP=`nslookup $BEAKERCLIENT | grep Address | grep -v "#" | awk '{print $2}'`
+		BEAKERCLIENT2_IP=`nslookup $BEAKERCLIENT2 | grep Address | grep -v "#" | awk '{print $2}'`
+
+                echo $MASTER_IP        $MASTER >> /etc/hosts
+                echo $BEAKERCLIENT2_IP  $CLIENT2 >> /etc/hosts
+
+                rlRun "cat /etc/hosts"
+
+
+	rlPhaseEnd
+
+        rlPhaseStartTest "CLIENT1 tests start"
+
+	# selinuxusermapsvc_client_001
+                rlRun "rhts-sync-block -s DONE_selinuxusermapsvc_master_001 $BEAKERMASTER"
+                selinuxusermapsvc_client_001
+                rlRun "rhts-sync-set -s DONE_selinuxusermapsvc_client_001 -m $BEAKERCLIENT"
+
+	rlPhaseEnd
+
+        rlPhaseStartCleanup "ipa-selinuxusermap-func-cleanup: Destroying admin credentials."
+                rlRun "kdestroy" 0 "Destroying admin credentials."
+                rlRun "cat /var/log/secure | grep \"pam_sss(sshd:auth)\""
+        rlPhaseEnd
+
+        else
+
+                rlLog "Machine in recipe in not a CLIENT"
+
+        fi
+
+        #####################################################################
+
+        #####################################################################
+        #               IS THIS MACHINE CLIENT2?                            #
+        #####################################################################
+        rc=0
+
+	echo "Hostname of this machine is $HOSTNAME"
+	echo "Hostname of client2 is $CLIENT2"
+
+        echo $CLIENT2 | grep $HOSTNAME
+        if [ $? -eq 0 ] ; then
+
+	rlPhaseStartSetup "ipa-selinuxusermapsvc-func: Checking client"
+                rlLog "Machine in recipe is CLIENT2"
+                rlRun "service iptables stop" 0 "Stop the firewall on the client"
+		rlRun "yum install -y ftp"
+		rlRun "cat /etc/krb5.conf"
+		rlRun "authconfig --enablemkhomedir --updateall"
+
+		rlRun "rhts-sync-block -s DONE_master_setup $BEAKERMASTER"
+		rlRun "rhts-sync-set -s DONE_client2_setup -m $BEAKERCLIENT2"
+
+		MASTER_IP=`nslookup $MASTER | grep Address | grep -v "#" | awk '{print $2}'`
+                BEAKERCLIENT_IP=`nslookup $BEAKERCLIENT | grep Address | grep -v "#" | awk '{print $2}'`
+                BEAKERCLIENT2_IP=`nslookup $BEAKERCLIENT2 | grep Address | grep -v "#" | awk '{print $2}'`
+
+		echo $MASTER_IP	$MASTER >> /etc/hosts
+		echo $BEAKERCLIENT_IP	$CLIENT >> /etc/hosts
+
+		rlRun "cat /etc/hosts"
+	rlPhaseEnd
+
+        rlPhaseStartTest "CLIENT2 tests start"
+
+	# selinuxusermapsvc_client2_001
+		rlRun "rhts-sync-block -s DONE_selinuxusermapsvc_master_001 $BEAKERMASTER"
+		selinuxusermapsvc_client2_001
+                rlRun "rhts-sync-set -s DONE_selinuxusermapsvc_client2_001 -m $BEAKERCLIENT2"
+
+	rlPhaseEnd
+
+        rlPhaseStartCleanup "ipa-selinuxusermap-func-cleanup: Destroying admin credentials."
+                rlRun "kdestroy" 0 "Destroying admin credentials."
+                rlRun "cat /var/log/secure | grep \"pam_sss(sshd:auth)\""
+        rlPhaseEnd
+
+        else
+
+                rlLog "Machine in recipe is not a CLIENT2"
+
+        fi
+
+        #####################################################################
+
+        #####################################################################
+        #               IS THIS MACHINE A MASTER?                           #
+        #####################################################################
+        rc=0
+
+	echo "Hostname of this machine is $HOSTNAME"
+	echo "Hostname of master is $MASTER"
+
+        echo $MASTER | grep $HOSTNAME
+        if [ $? -eq 0 ] ; then
+                rlLog "Machine in recipe is MASTER"
+
+	rlPhaseStartSetup "ipa-selinuxusermapsvc-func: Setup of users"
+
+                rlRun "service iptables stop" 0 "Stop the firewall on the client"
+		rlRun "authconfig --enablemkhomedir --updateall"
+        	rlRun "cat /dev/shm/env.sh"
+	        rlRun "TmpDir=\`mktemp -d\`" 0 "Creating tmp directory"
+        	rlRun "pushd $TmpDir"
+	        rlRun "kinitAs $ADMINID $ADMINPW" 0 "Kinit as admin user"
+
+		BEAKERCLIENT_IP=`nslookup $BEAKERCLIENT | grep Address | grep -v "#" | awk '{print $2}'`
+		BEAKERCLIENT2_IP=`nslookup $BEAKERCLIENT2 | grep Address | grep -v "#" | awk '{print $2}'`
+		BEAKERCLIENT_SH=`echo $BEAKERCLIENT | cut -d "." -f 1`
+		BEAKERCLIENT2_SH=`echo $BEAKERCLIENT2 | cut -d "." -f 1`
+		BEAKERCLIENT_PTR=`nslookup $BEAKERCLIENT | grep Address | grep -v "#" | awk '{print $2}' | cut -d "." -f 4`
+		BEAKERCLIENT2_PTR=`nslookup $BEAKERCLIENT2 | grep Address | grep -v "#" | awk '{print $2}' | cut -d "." -f 4`
+
+		echo $BEAKERCLIENT_IP	$CLIENT	>> /etc/hosts
+		echo $BEAKERCLIENT2_IP	$CLIENT2 >> /etc/hosts
+
+		rlRun "cat /etc/hosts"
+
+		REVERSE_ZONE=`ipa dnszone-\find | grep -i "Zone name" | head -1 | awk '{print $4}'`
+		rlLog "REVERSE_ZONE is $REVERSE_ZONE"
+
+		ipa dnszone-find | grep -i "Zone name" | head -1 | awk '{print $4}' > /tmp/reverse_zone.out
+		REVERSE_ZONE=`cat /tmp/reverse_zone.out`
+		rlLog "REVERSE_ZONE now is $REVERSE_ZONE"
+
+		rlRun "ipa dnszone-find > /tmp/rev.out 2>&1"
+		REVERSE_ZONE=`cat /tmp/rev.out | grep -i "Zone name" | head -1 | awk '{print $3}'`
+		rlRun "cat /tmp/rev.out"
+		rlLog "REVERSE_ZONE now again is $REVERSE_ZONE"
+
+		# Adding forward and reverse record.
+		# rlRun "ipa dnsrecord-add $DOMAIN $BEAKERCLIENT_SH --a-rec=$BEAKERCLIENT_IP"
+		rlRun "ipa dnsrecord-add $REVERSE_ZONE $BEAKERCLIENT_PTR --ptr-rec=$CLIENT."
+
+		# Adding forward and reverse record.
+		# echo "ipa dnsrecord-add $DOMAIN $BEAKERCLIENT2_SH --a-rec=$BEAKERCLIENT2_IP"
+		# echo "ipa dnsrecord-add $REVERSE_ZONE $BEAKERCLIENT2_PTR --ptr-rec=$CLIENT2."
+		# rlRun "ipa dnsrecord-add $DOMAIN $BEAKERCLIENT2_SH --a-rec=$BEAKERCLIENT2_IP"
+		rlRun "ipa dnsrecord-add $REVERSE_ZONE $BEAKERCLIENT2_PTR --ptr-rec=$CLIENT2."
+
+
+		# adding additional sync-set and sync-block so that tests are not
+		# executed before the clients are ready
+		rlRun "rhts-sync-set -s DONE_master_setup -m $BEAKERMASTER"
+		rlRun "rhts-sync-block -s DONE_client1_setup -s DONE_client2_setup $BEAKERCLIENT $BEAKERCLIENT2"
+	rlPhaseEnd
+
+	rlPhaseStartTest "MASTER tests start"
+
+	# selinuxusermapsvc_master_001
+		selinuxusermapsvc_master_001
+		rlRun "rhts-sync-set -s DONE_selinuxusermapsvc_master_001 -m $BEAKERMASTER"
+                rlRun "rhts-sync-block -s DONE_selinuxusermapsvc_client_001 -s DONE_selinuxusermapsvc_client2_001 $BEAKERCLIENT $BEAKERCLIENT2"
+		selinuxusermapsvc_master_001_cleanup
+
+	rlPhaseEnd
+
+
+	rlPhaseStartCleanup "ipa-selunxusermap-func-cleanup: Destroying admin credentials."
+
+	        rlRun "kdestroy" 0 "Destroying admin credentials."
+		rlRun "cat /var/log/secure | grep \"pam_sss(sshd:auth)\""
+	rlPhaseEnd
+
+
+        else
+                rlLog "Machine in recipe in not a MASTER"
+        fi
+
+        #####################################################################
+
+
+rlJournalPrintText
+report=$TmpDir/rhts.report.$RANDOM.txt
+makereport $report
+rhts-submit-log -l $report
+rlJournalEnd
---
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-selinuxusermap-func/t.selinuxusermapsvc.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+# vim: dict=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   runtest.sh of /CoreOS/ipa-tests/acceptance/ipa-hbacfunc
+#   Description: Functional test cases for ipa hbca
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# The following ipa will be tested:
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Authors: 
+#	     Gowrishankar Rajaiyan <gsr@redhat.com>
+#   Date   : August 11, 2011
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2010 Red Hat, Inc. All rights reserved.
+#
+#   This copyrighted material is made available to anyone wishing
+#   to use, modify, copy, or redistribute it subject to the terms
+#   and conditions of the GNU General Public License version 2.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free
+#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#   Boston, MA 02110-1301, USA.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Include data-driven test data file:
+
+# Include rhts environment
+. /usr/bin/rhts-environment.sh
+. /usr/share/beakerlib/beakerlib.sh
+. /dev/shm/ipa-server-shared.sh
+
+selinuxusermapsvc_master_001() {
+
+	rlPhaseStartTest "ipa-selinuxusermapsvc-001: $user1 part of selinuxusermap1 is allowed to access $CLIENT from $CLIENT - SSHD Service"
+
+                # kinit as admin and creating users
+		rlRun "kinitAs $ADMINID $ADMINPW" 0 "Kinit as admin user"
+        	for i in {1..3}; do
+                	rlRun "create_ipauser user$i user$i user$i $userpw"
+                	sleep 5
+                	rlRun "export user$i=user$i"
+	        done
+
+        	rlRun "kinitAs $ADMINID $ADMINPW" 0 "Kinit as admin user"
+		rlRun "ssh_auth_success $user1 testpw123@ipa.com $MASTER"
+		rlRun "ssh_auth_success $user2 testpw123@ipa.com $MASTER"
+		rlRun "ssh_auth_success $user3 testpw123@ipa.com $MASTER"
+		ipa_selinuxuser="staff_u:s0-s0:c0.c1023"
+		ipa_selinuxuser_verif="staff_u:.*s0-s0:c0.c1023"
+		ipa_default_selinuxuser_verif="guest_u:.*s0"
+		
+
+		rlRun "ipa selinuxusermap-add selinuxusermaprule1 --selinuxuser=$ipa_selinuxuser"
+		rlRun "ipa selinuxusermap-add-user selinuxusermaprule1 --users=$user1"
+		rlRun "ipa selinuxusermap-add-host selinuxusermaprule1 --hosts=$CLIENT"
+		rlRun "ipa selinuxusermap-show selinuxusermaprule1 --all"
+
+		# ipa selinuxusermap test:
+		rlRun "rlDistroDiff keyctl" 
+		rlRun "kinitAs $user1 $userpw" 0 "Kinit as $user1"	
+		rlRun "verify_ssh_auth_success_selinuxuser_krbcred $user1 $CLIENT $ipa_selinuxuser_verif" 0 "Authentication of $user1 to $CLIENT has selinux policy $ipa_selinuxuser"
+		rlRun "rlDistroDiff keyctl" 
+		rlRun "kinitAs $user2 $userpw" 0 "Kinit as $user2"       
+                rlRun "verify_ssh_auth_failure_selinuxuser_krbcred $user2 $CLIENT $ipa_selinuxuser_verif" 0 "Authentication of $user2 to $CLIENT does not have selinux policy $ipa_selinuxuser "
+		rlRun "verify_ssh_auth_success_selinuxuser_krbcred $user2 $CLIENT $ipa_default_selinuxuser_verif" 0 "Authentication of $user1 to $CLIENT has selinux policy $ipa_default_selinuxuser"
+                rlRun "rlDistroDiff keyctl"
+		rlRun "kinitAs $user1 $userpw" 0 "Kinit as $user1"
+                rlRun "verify_ssh_auth_failure_selinuxuser_krbcred $user1 $CLIENT2 $ipa_selinuxuser_verif" 0 "Authentication of $user1 to $CLIENT2 does not have selinux policy $ipa_selinuxuser"
+		rlRun "verify_ssh_auth_success_selinuxuser_krbcred $user2 $CLIENT2 $ipa_default_selinuxuser_verif" 0 "Authentication of $user1 to $CLIENT has selinux policy $ipa_default_selinuxuser"
+                rlRun "rlDistroDiff keyctl"
+        	rlRun "kinitAs $ADMINID $ADMINPW" 0 "Kinit as admin user"
+	rlPhaseEnd
+}
+
+selinuxusermapsvc_master_001_cleanup() {
+        # Cleanup
+	rlRun "kinitAs $ADMINID $ADMINPW" 0 "Kinit as admin user"
+	for i in {1..3}; do
+		rlRun "ipa user-del user$i"
+        done
+	rlRun "rm -fr /tmp/krb5cc_*_*"
+	rlRun "ipa selinuxusermap-del selinuxusermaprule1"
+}
+
+selinuxusermapsvc_client_001() {
+
+        rlPhaseStartTest "ipa-selinuxusermapsvc-client1-001: $user1 accessing $CLIENT from $CLIENT using SSHD service."
+
+                rlRun "kinitAs $ADMINID $ADMINPW" 0 "Kinit as admin user"
+                rlRun "getent -s sss passwd $user1"
+		sleep 5
+                rlRun "verify_ssh_auth_success_selinuxuser $user1 testpw123@ipa.com $CLIENT $ipa_selinuxuser_verif"
+                rlRun "verify_ssh_auth_failure_selinuxuser $user2 testpw123@ipa.com $CLIENT $ipa_selinuxuser_verif"
+                rlRun "verify_ssh_auth_success_selinuxuser $user2 testpw123@ipa.com $CLIENT $ipa_default_selinuxuser_verif"
+        rlPhaseEnd
+}
+
+selinuxusermapsvc_client2_001() {
+
+        rlPhaseStartTest "ipa-selinuxusermapsvc-client2-001: $user1 accessing $CLIENT from $CLIENT2 using SSHD service."
+
+                rlRun "kinitAs $ADMINID $ADMINPW" 0 "Kinit as admin user"
+                rlRun "getent -s sss passwd $user1"
+                rlRun "verify_ssh_auth_failure_selinuxuser $user1 testpw123@ipa.com $CLIENT2 $ipa_selinuxuser_verif"
+                rlRun "verify_ssh_auth_success_selinuxuser $user1 testpw123@ipa.com $CLIENT2 $ipa_default_selinuxuser_verif"
+        rlPhaseEnd
+}
+
