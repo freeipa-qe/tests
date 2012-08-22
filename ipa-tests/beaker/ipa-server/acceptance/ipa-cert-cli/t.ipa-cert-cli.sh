ca612376ee2ff47e0683037242864afc921154e8
Michael Gregg <mgregg@REDHAT.COM>
2012-08-22 23:40:01 +0000

    adding some additions on the advisement of Rob
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@4102 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-cert-cli/t.ipa-cert-cli.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-cert-cli/t.ipa-cert-cli.sh
@@ -37,8 +37,8 @@ certcli_envsetup()
 		mv /etc/ipa/default.conf-original-$dc
 		echo '[global]' >> /etc/ipa/server.conf
 		echo 'debug=True' >> /etc/ipa/server.conf
-		#echo 'debug=True' >> /etc/ipa/default.conf
-		#rlRun "/usr/sbin/ipactl restart" 0 "restarting IPA to enable debug mode"
+		echo 'debug=True' >> /etc/ipa/default.conf
+		rlRun "/usr/sbin/ipactl restart" 0 "restarting IPA to enable debug mode"
 	rlPhaseEnd
 		
 } #certcli_envsetup
@@ -49,7 +49,7 @@ certcli_basic()
 		kdestroy
 		keyctl purge user # Purging keys to be certain that the user-find populates the keyring properly.
 		KinitAsUser $u1 $u1pass
-		rlRun "ipa user-find $u1" 0 "show this user to populate the keyring"
+		rlRun "ipa user-find $u1" 0 "user-find this user to populate the keyring"
 		rlRun "keyctl list @s | grep ipa_session_cookie | grep $u1" 0 "ensure that the ipa session cookie was created"
 	rlPhaseEnd
 
@@ -85,7 +85,7 @@ certcli_basic()
 	rlPhaseStartTest "kinit as u2 and verify that the keyring gets created"
 		kdestroy
 		KinitAsUser $u2 $u2pass
-		rlRun "ipa user-find $u1" 0 "show this user to populate the keyring"
+		rlRun "ipa user-find $u1" 0 "user-find this user to populate the keyring"
 		rlRun "keyctl list @s | grep ipa_session_cookie | grep $u1" 0 "ensure that the ipa session cookie was created"
 	rlPhaseEnd
 
@@ -168,18 +168,38 @@ certcli_basic()
 	rlPhaseEnd
 	
 	# This Section verifies that multiple principals are supported at the same time 
+	rlPhaseStartTest "Populate keyring with keys from two different users"
+		kdestroy
+		keyctl purge user # Purging keys to be certain that the user-find populates the keyring properly.
+		KinitAsUser $u1 $u1pass
+		ipa user-find $u1 &> /dev/null
+		KinitAsUser $u2 $u2pass
+		ipa user-find $u2 &> /dev/null
+		rlRun "keyctl show @s | grep ipa_session_cookie | grep $u1" 0 "Verify that u1 has a valid session key"
+		rlRun "keyctl show @s | grep ipa_session_cookie | grep $u2" 0 "Verify that u2 has a valid session key"
+	rlPhaseEnd
 
 	rlPhaseStartTest "Populate keyring for u1. restart ipa_memcache. Ensure that the ipa session id changes"
 		kdestroy
 		KinitAsUser $u1 $u1pass
-		rlRun "ipa user-find $u1 &> /dev/null" 0 "show this user to populate the keyring"
+		rlRun "ipa user-find $u1 &> /dev/null" 0 "user-find this user to populate the keyring"
 		rlRun "keyctl list @s | grep ipa_session_cookie | grep $u1" 0 "ensure that the ipa session cookie was created"
 		# Get current current session ID
 		sessid=$(keyctl list @s | grep ipa_session_cookie | grep $u1 |cut -d\  -f1)
+		if [ -x $sessid	]; then
+			# sometimes the grep and cut do not work, do it again.
+			sleep 1
+			sessid=$(keyctl list @s | grep ipa_session_cookie | grep admin |cut -d\  -f1)
+		fi
 		rlLog "current session ID is $sessid"
 		rlRun "/bin/systemctl restart ipa_memcached.service" 0 "Restart memcached to break current session keys"
 		rlRun "ipa user-find $u1 &> /dev/null" 0 "rerun user find to generate new session ID"
 		newsessid=$(keyctl list @s | grep ipa_session_cookie | grep $u1 |cut -d\  -f1) # Get new session ID
+		if [ -x $newsessid	]; then
+			# sometimes the grep and cut do not work, do it again.
+			sleep 1
+			newsessid=$(keyctl list @s | grep ipa_session_cookie | grep admin |cut -d\  -f1)
+		fi
 		rlLog "new session ID is $newsessid"
 		rlRun "echo $newsessid | grep $sessid" 1 "make sure the old session is not the same as the current sessionid"
 	rlPhaseEnd
@@ -187,18 +207,59 @@ certcli_basic()
 	rlPhaseStartTest "Populate keyring for admin. restart ipa_memcache. Ensure that the ipa session id changes"
 		kdestroy
 		KinitAsAdmin
-		rlRun "ipa user-find $u1 &> /dev/null" 0 "show this user to populate the keyring"
+		rlRun "ipa user-find $u1 &> /dev/null" 0 "user-find this user to populate the keyring"
 		rlRun "keyctl list @s | grep ipa_session_cookie | grep $u1" 0 "ensure that the ipa session cookie was created"
 		# Get current current session ID
 		sessid=$(keyctl list @s | grep ipa_session_cookie | grep admin |cut -d\  -f1)
+		if [ -x $sessid	]; then
+			# sometimes the grep and cut do not work, do it again.
+			sleep 1
+			sessid=$(keyctl list @s | grep ipa_session_cookie | grep admin |cut -d\  -f1)
+		fi
+		keyctl list @s | grep ipa_session_cookie | grep admin |cut -d\  -f1
 		rlLog "current session ID is $sessid"
 		rlRun "/bin/systemctl restart ipa_memcached.service" 0 "Restart memcached to break current session keys"
 		rlRun "ipa user-find $u1 &> /dev/null" 0 "rerun user find to generate new session ID"
 		newsessid=$(keyctl list @s | grep ipa_session_cookie | grep admin |cut -d\  -f1) # Get new session ID
+		if [ -x $newsessid	]; then
+			# sometimes the grep and cut do not work, do it again.
+			sleep 1
+			newsessid=$(keyctl list @s | grep ipa_session_cookie | grep admin |cut -d\  -f1)
+		fi
+		keyctl list @s | grep ipa_session_cookie | grep admin |cut -d\  -f1
 		rlLog "new session ID is $newsessid"
 		rlRun "echo $newsessid | grep $sessid" 1 "make sure the old session is not the same as the current sessionid"
 	rlPhaseEnd
 	
+	rlPhaseStartTest "Create a valid keyring, then try issuing a command that will fallback to kerberos auth. Verify that the fallback happens"
+		kdestroy
+		keyctl purge user # Purging keys to be certain that the user-find populates the keyring properly.
+		rlRun "keyctl clear @s" 0 "Clear local session keyring"
+		rlRun "keyctl clear @u" 0 "Clear local user keyring"
+		KinitAsUser $u1 $u1pass
+		outf="/dev/shm/outfileg.txt"
+		ipa -vv user-find $u1 &> $outf # running ipa user-find to populate the keyring. 
+		ipa user-find admin &> /dev/null
+		sleep 5
+		rlRun "keyctl list @s | grep ipa_session_cookie | grep $u1" 0 "verify u1's ipa session cookie was created"
+		rlRun "grep Authorization:\ negotiate $outf" 0 "This first user-find should complete a full kerberos auth."
+		outf="/dev/shm/outfileh.txt"
+		sleep 5
+		ipa -vv user-find $u1 &>/dev/shm/1
+		ipa -vv user-find $u1 &> $outf # This command should work off of the current session.
+		ipa -vv user-find $u1 &>/dev/shm/2
+		ipa -vv user-find $u1 &>/dev/shm/3
+		sleep 30
+		ipa -vv user-find $u1 &>/dev/shm/4
+		sleep 30
+		ipa -vv user-find $u1 &>/dev/shm/5
+
+		rlRun "grep Authorization:\ negotiate $outf" 1 "Re-verify that a normal user-find does not do a full kerberos auth"
+		outf="/dev/shm/outfilei.txt"
+		ipa -vv --delegate user-find &> $outf # this command should force a full kerberos auth
+		rlRun "grep Authorization:\ negotiate $outf" 0 "ipa delegate should force a full kerberos auth. Verify that it happened."
+	rlPhaseEnd		
+
 	rlPhaseStartTest "If this test contains a slave, stop the server on the master."
 		if [ -x $BEAKERSLAVE ]; then 
 			rlLog "This test does not contain a slave. Not runnign multimaster tests."
@@ -233,17 +294,17 @@ certcli_basic()
 			else
 				kdestroy
 				KinitAsUser $u1 $u1pass
-				rlRun "ipa user-find $u1" 0 "show this user to populate the keyring"
+				rlRun "ipa user-find $u1" 0 "user-find this user to populate the keyring"
 				rlRun "keyctl list @s | grep ipa_session_cookie | grep $u1" 0 "ensure that the ipa session cookie was created"
 				rlRun "keyctl clear @s" 0 "Clear local session keyring"
 				rlRun "keyctl list @s | grep ipa_session_cookie" 1 "Make sure that session keyring appears clear"
-				outf="/dev/shm/outfilema.txt"
+				outf="/dev/shm/outfilej.txt"
 				ipa user-find $u1 &> $outf
 				rlRun "grep 'keyctl_search: Required key not available' $outf" 0 "look for session key lookup failure in output"
 				rlRun "grep 'padd user' $outf" 0 "Ensure that keyctl seems to be populating local session key"
 				rlRun "grep 'keyctl pipe' $outf" 1 "Ensure that keyctl pipe is not found"
 				rlRun "grep 'keyctl pupdate' $outf" 1 "Ensure that keyctl update is not found"
-				outf="/dev/shm/outfilemb.txt"
+				outf="/dev/shm/outfilek.txt"
 				ipa user-find $u1 &> $outf
 				rlRun "grep 'keyctl_search: Required key not available' $outf" 1 "look for session key lookup failure is not in output"
 				rlRun "grep 'padd user' $outf" 1 "Ensure that keyctl is not populating local session key, as it should be popluated already."
@@ -279,10 +340,10 @@ certcli_envcleanup()
 		delete_ipauser $u2
 		rm -f /etc/ipa/server.conf-backup
 		rlRun "mv /etc/ipa/server.conf /etc/ipa/server.conf-backup" 0 "copying server.conf to a backup"
-		#cat /etc/ipa/default.conf | grep -v debug > /dev/shm/default.conf
-		#rm -f /etc/ipa/default.conf
+		cat /etc/ipa/default.conf | grep -v debug > /dev/shm/default.conf
+		rm -f /etc/ipa/default.conf
 		rlRun "cp -a /dev/shm/default.conf /etc/ipa/default.conf" 0 "Restoring default.conf"
-		#rlRun "/usr/sbin/ipactl restart" 0 "restarting IPA to disable debug mode"
+		rlRun "/usr/sbin/ipactl restart" 0 "restarting IPA to disable debug mode"
 		#environment cleanup ends   here
 	rlPhaseEnd
 } #certcli_envcleanup
