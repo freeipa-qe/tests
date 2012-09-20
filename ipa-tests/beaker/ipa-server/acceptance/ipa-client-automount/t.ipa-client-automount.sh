88ba349340038dff7b8938b1e90fcd08e74b3416
Yi Zhang <yzhang@REDHAT.COM>
2012-09-20 21:14:38 +0000

    initial checkin, not runnable at this point
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@4309 aeb79ed8-e03d-0410-be94-af332b95afa2

---
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-client-automount/t.ipa-client-automount.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+######################################################
+# test suite  : ipa-client-automount
+# description : IPA client side tool ipa-client-automount test
+# created by  : Yi Zhang (yzhang@redhat.com)
+# created date: Thu Sep 20 14:10:03 PDT 2012
+######################################################
