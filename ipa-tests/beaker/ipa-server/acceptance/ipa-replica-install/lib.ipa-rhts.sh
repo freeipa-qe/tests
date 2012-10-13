ed1f87a64ec49e3047573694519dfa5d7e9a70c4
Michael Gregg <mgregg@REDHAT.COM>
2012-10-13 01:54:59 +0000

    adding rhts delay lib for testing
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@4441 aeb79ed8-e03d-0410-be94-af332b95afa2

---
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-replica-install/lib.ipa-rhts.sh
@@ -0,0 +1,39 @@
+# A set of experimental tools to get a alternate rhts sync going on
+
+export IPARHTSDIR="/var/www/html/iparhtsdir"
+
+setup_iparhts_sync()
+{
+	# Make sure apache is installed
+	yum -y install httpd
+	rm -Rf $IPARHTSDIR
+	mkdir $IPARHTSDIR
+	chmod 755 $IPARHTSDIR
+	/etc/init.d/httpd restart
+	systemctl restart httpd.service
+}
+
+iparhts-sync-set()
+{
+	rlLog "touching $IPARHTSDIR/$3 for RHTS sync"
+	touch $IPARHTSDIR/$3
+	chmod 755 $IPARHTSDIR/$3
+}
+
+iparhts-sync-block()
+{
+iparhts-sync-set -s READY_REPLICA1 $BEAKERMASTER
+	rlLog "Blocking waiting for $3 to post state $2"
+	done=0;
+	while [ $done -eq 0 ]; do
+		rlLog "Attempting to get http://$3/$2"
+		wget http://$4/$3
+		if [ $? -eq 0 ]; then 
+			rlLog "Success! Got $2 from $3. Proceeding."
+			done=1;
+		else
+			rlLog "WAITING - Get of http://$3/$2 failed. Sleeping 60 sec"
+			sleep 60
+		fi
+	done
+}
