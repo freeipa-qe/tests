92aab9702c61b7a58c7e8e1ef6fc73abc97b742a
Michael Gregg <mgregg@REDHAT.COM>
2012-10-15 18:25:41 +0000

    updating ipa rhts sync block in replica install test
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@4442 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-replica-install/lib.ipa-rhts.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-replica-install/lib.ipa-rhts.sh
@@ -15,9 +15,9 @@ setup_iparhts_sync()
 
 iparhts-sync-set()
 {
-	rlLog "touching $IPARHTSDIR/$3 for RHTS sync"
-	touch $IPARHTSDIR/$3
-	chmod 755 $IPARHTSDIR/$3
+	rlLog "NOTICE - touching $IPARHTSDIR/$2 for RHTS sync"
+	touch $IPARHTSDIR/$2
+	chmod 755 $IPARHTSDIR/$2
 }
 
 iparhts-sync-block()
@@ -26,13 +26,13 @@ iparhts-sync-set -s READY_REPLICA1 $BEAKERMASTER
 	rlLog "Blocking waiting for $3 to post state $2"
 	done=0;
 	while [ $done -eq 0 ]; do
-		rlLog "Attempting to get http://$3/$2"
+		rlLog "Attempting to get http://$3/iparhtsdir/$2"
 		wget http://$4/$3
 		if [ $? -eq 0 ]; then 
 			rlLog "Success! Got $2 from $3. Proceeding."
 			done=1;
 		else
-			rlLog "WAITING - Get of http://$3/$2 failed. Sleeping 60 sec"
+			rlLog "WAITING - Get of http://$3/iparhtsdir/$2 failed. Sleeping 60 sec"
 			sleep 60
 		fi
 	done
