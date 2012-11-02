7b2fab1548e4b6e07579f1b0fbd71848d5badaf4
Scott Poore <spoore@REDHAT.COM>
2012-11-02 15:38:16 +0000

    quickinstall: ipa-install.sh: add kinit admin to workaround BZ 872372
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@4564 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/quickinstall/ipa-install.sh
+++ ipa-tests/beaker/ipa-server/acceptance/quickinstall/ipa-install.sh
@@ -937,8 +937,10 @@ ipa_install_master()
 		
 		rlRun "ipa-server-install $IPAOPTIONS --setup-dns --forwarder=$DNSFORWARD --hostname=$hostname_s.$DOMAIN -r $RELM -n $DOMAIN -p $ADMINPW -P $ADMINPW -a $ADMINPW -U"
         if [ $(dig +short download.devel.redhat.com|wc -l) -eq 0 ]; then 
+            KinitAsAdmin
             rlLog "[FAIL]: BZ 872372 found...IPA server DNS forwarding broken with bind-dyndb-ldap-2.2-1.el6.x86_64"
             rlLog "Adding workaround for BZ 872372 to fix broken forwarding"
+            rlRun "echo $ADMINPW|kinit admin"
             rlRun "ipa dnsconfig-mod --forwarder=$DNSFORWARD"
             rlRun "service named restart"
         fi
