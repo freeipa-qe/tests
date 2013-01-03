2a8515c5a00255443449916e947f4f3e2a04ae48
Scott Poore <spoore@REDHAT.COM>
2013-01-03 21:10:05 +0000

    ipa-nis-integration: add some extra code to ensure nfs starts for netgroup tests
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@4854 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-nis-integration/tests.d/t.nisint_netgroup_tests.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-nis-integration/tests.d/t.nisint_netgroup_tests.sh
@@ -450,6 +450,9 @@ nisint_netgroup_test_1009()
 			rlRun "service rpcbind restart"
 		fi
 		
+		if [ $(mount|grep /proc/fs/nfsd|wc -l) -eq 0 ]; then
+			rlRun "mount -t nfsd nfsd /proc/fs/nfsd"
+		fi
 		if [ -f /usr/lib/systemd/system/nfs-server.service ]; then
 			rlRun "systemctl restart nfs-server.service"
 		else
