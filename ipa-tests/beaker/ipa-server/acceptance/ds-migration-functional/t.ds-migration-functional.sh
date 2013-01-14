090d06ec62263c6e19150b5a4120484520f18294
Jenny Galipeau <jgalipea@REDHAT.COM>
2013-01-14 22:15:45 +0000

    tests added for 822608 were not correctly placed based on flow of tests looks like they were never actually executed to verify fixed I think
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@4935 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ds-migration-functional/t.ds-migration-functional.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ds-migration-functional/t.ds-migration-functional.sh
@@ -19,7 +19,6 @@ ds-migration-functional()
     setup
     hashedpwdmigration_sssd
     hashedpwdmigration_http
-    bz_tests
     cleanup
 }
 
@@ -52,15 +51,26 @@ hashedpwdmigration_sssd()
         rlPhaseStartTest "ds-migration-functional-002 SSSD password migration $USER1"
 		rlRun "ssh_auth_success $USER1 $USER1PWD $HOSTNAME"
 		rlRun "verifyUserAttr $USER1 \"Kerberos keys available\" True" 0 "Verify migrated user $USER1 now has a keytab"
-		KinitAsUser $USER1 $USER1PWD 
+		# https://bugzilla.redhat.com/show_bug.cgi?id=822608
+                KinitAsUser $USER1 $USER1PWD
+                rlRun "klist | grep $USER1" 0 "Ensuring that kinit as $USER1 worked"
+                rlLog "Running kinit"
+                KinitAsUser $USER1 $USER1PWD
+
         rlPhaseEnd
 
         rlPhaseStartTest "ds-migration-functional-003 SSSD password migration $USER2"
 		rlRun "ssh_auth_success $USER2 $USER2PWD $HOSTNAME"
 		rlRun "verifyUserAttr $USER2 \"Kerberos keys available\" True" 0 "Verify migrated user $USER2 now has a keytab"
+                # https://bugzilla.redhat.com/show_bug.cgi?id=822608
+                KinitAsUser $USER2 $USER2PWD
+                rlRun "klist | grep $USER2" 0 "Ensuring that kinit as $USER2 worked"
+                rlLog "Running kinit"
+                KinitAsUser $USER2 $USER2PWD
         rlPhaseEnd
 
         rlPhaseStartTest "ds-migration-functional-004 Cleanup SSSD Migration"
+		rlRun "kinitAs $ADMINID $ADMINPW" 0 "Get administrator credentials"
                 ipa user-del $USER1
 		ipa user-del $USER2
 		ipa group-del $GROUP1
@@ -81,6 +91,7 @@ hashedpwdmigration_sssd()
 hashedpwdmigration_http()
 {
         rlPhaseStartTest "ds-migration-functional-005 Re-Migrate users with hashed passwords"
+		rlRun "kinitAs $ADMINID $ADMINPW" 0 "Get administrator credentials"
                 rlLog "EXECUTING: ipa migrate-ds --user-container=\"$USERCONTAINER\" --group-container=\"$GROUPCONTAINER\" --with-compat ldap://$CLIENT:389"
                 rlRun "echo $ADMINPW | ipa migrate-ds --user-container=\"$USERCONTAINER\" --group-container=\"$GROUPCONTAINER\" --with-compat ldap://$CLIENT:389" 0
 
@@ -91,14 +102,26 @@ hashedpwdmigration_http()
         rlPhaseStartTest "ds-migration-functional-006 HTTP password migration $USER1"
                 rlRun "curl -v -e https://$MASTER/ipa/migration/ https://$MASTER/ipa/migration/migration.py --form-string 'username=$USER1' --form-string 'password=$USER1PWD' --cacert $CACERT" 0 "Hitting the migration page via curl"
 		rlRun "verifyUserAttr $USER1 \"Kerberos keys available\" True" 0 "Verify migrated user $USER1 now has a keytab"
+                # https://bugzilla.redhat.com/show_bug.cgi?id=822608
+                KinitAsUser $USER1 $USER1PWD 
+                rlRun "klist | grep $USER1" 0 "Ensuring that kinit as $USER1 worked" 
+                rlLog "Running kinit"
+                KinitAsUser $USER1 $USER1PWD 
         rlPhaseEnd
 
         rlPhaseStartTest "ds-migration-functional-007 HTTP password migration $USER2"
 		rlRun "curl -v -e https://$MASTER/ipa/migration/ https://$MASTER/ipa/migration/migration.py --form-string 'username=$USER2' --form-string 'password=$USER2PWD' --cacert $CACERT" 0 "Hitting the migration page via curl"
                 rlRun "verifyUserAttr $USER2 \"Kerberos keys available\" True" 0 "Verify migrated user $USER2 now has a keytab"
+		# https://bugzilla.redhat.com/show_bug.cgi?id=822608
+                KinitAsUser $USER2 $USER2PWD
+                rlRun "klist | grep $USER2" 0 "Ensuring that kinit as $USER2 worked"
+                rlLog "Running kinit"
+                KinitAsUser $USER2 $USER2PWD
+
         rlPhaseEnd
 
         rlPhaseStartTest "ds-migration-functional-008 Cleanup HTTP Migration"
+		rlRun "kinitAs $ADMINID $ADMINPW" 0 "Get administrator credentials"
                 ipa user-del $USER1
                 ipa user-del $USER2
                 ipa group-del $GROUP1
@@ -108,25 +131,6 @@ hashedpwdmigration_http()
                 rlRun "ipa user-show $USER2" 2 "Make sure $USER2 was deleted"
                 rlRun "ipa group-show $GROUP1" 2 "Make sure $GROUP1 was deleted"
                 rlRun "ipa group-show $GROUP2" 2 "Make sure $GROUP2 was deleted"
-        rlPhaseEnd
-}
-
-bz_tests()
-{
-        rlPhaseStartTest "ds-migration-functional-008 bz 822608 part 1 password test for migrated user $USER1"
-		# https://bugzilla.redhat.com/show_bug.cgi?id=822608
-		KinitAsUser $USER1 $USER1PWD 
-		rlRun "klist | grep $USER1" 0 "Ensuring that kinit as $USER1 worked" 
-		rlLog "Running kinit"
-		KinitAsUser $USER1 $USER1PWD 
-        rlPhaseEnd
-
-        rlPhaseStartTest "ds-migration-functional-009 bz 822608 part 2 password test for migrated user $USER2"
-		# https://bugzilla.redhat.com/show_bug.cgi?id=822608
-		KinitAsUser $USER2 $USER2PWD 
-		rlRun "klist | grep $USER2" 0 "Ensuring that kinit as $USER2 worked" 
-		rlLog "Running kinit"
-		KinitAsUser $USER1 $USER1PWD 
         rlPhaseEnd
 }
 
