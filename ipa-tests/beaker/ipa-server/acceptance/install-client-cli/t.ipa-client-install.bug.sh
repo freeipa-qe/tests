9d2fe3ee43f8b6f20d17c36cc6df8cfc1e8bd189
Scott Poore <spoore@REDHAT.COM>
2013-01-29 20:27:43 +0000

    install-client-cli: fixing spacing in ipaclientinstall_dirty_keytab
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@5040 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/install-client-cli/t.ipa-client-install.bug.sh
+++ ipa-tests/beaker/ipa-server/acceptance/install-client-cli/t.ipa-client-install.bug.sh
@@ -112,41 +112,42 @@ ipaclientinstall_bugcheck_845691_fulltest()
 ipaclientinstall_dirty_keytab()
 {
     rlPhaseStartTest "ipa_client_install-39 - Install with a dirty keytab"
-	rlLog "Test for BZ 817869, install ipa-client with a dirty keytab"
+        rlLog "Test for BZ 817869, install ipa-client with a dirty keytab"
         uninstall_fornexttest
         #rlLog "EXECUTING: ipa-client-install --domain=$DOMAIN --realm=$RELM --ntp-server=$NTPSERVER -p $ADMINID -w $ADMINPW --unattended --server=$MASTER"
         #rlRun "ipa-client-install --domain=$DOMAIN --realm=$RELM --ntp-server=$NTPSERVER -p $ADMINID -w $ADMINPW --unattended --server=$MASTER" 0 "Installing ipa client and configuring - with all params"
         rlLog "EXECUTING: ipa-client-install --domain=$DOMAIN --realm=$RELM -p $ADMINID -w $ADMINPW --unattended --server=$MASTER"
         rlRun "ipa-client-install --domain=$DOMAIN --realm=$RELM -p $ADMINID -w $ADMINPW --unattended --server=$MASTER" 0 "Installing ipa client and configuring - with all params"
         verify_install true
-	# Backup keytab 
-	bkup="/dev/shm/ipa-client-backup-keytab"
-	ktab="/etc/krb5.keytab"
-	rm -f $bkup
-	cp -a $ktab $bkup
+        # Backup keytab 
+        bkup="/dev/shm/ipa-client-backup-keytab"
+        ktab="/etc/krb5.keytab"
+        rm -f $bkup
+        cp -a $ktab $bkup
         # Now uninstall
         uninstall_fornexttest
         verify_install false 
-	rlLog "$command"
-	rlRun "$command" 0 "Uninstalling ipa client - after a force install"
-	rm -f $ktab
-	if [ ! -f $ktab ]; then
-		cp -a $kbup $ktab
-	fi
-	klist -kt /etc/krb5.keytab
+
+        rlLog "$command"
+        rlRun "$command" 0 "Uninstalling ipa client - after a force install"
+        rm -f $ktab
+        if [ ! -f $ktab ]; then
+            cp -a $kbup $ktab
+        fi
+
+        klist -kt /etc/krb5.keytab
         rlRun "ipa-client-install --domain=$DOMAIN --realm=$RELM -p $ADMINID -w $ADMINPW --unattended --server=$MASTER" 0 "Installing ipa client and configuring - with all params"
         verify_install true
 
-	klist -kt /etc/krb5.keytab
-	diff $bkup $ktab
-	if [ $? -eq 0 ]; then 
-		rlFail "FAIL - $bkup and $ktab match when they should not."
-		cont=$(cat $bkup)
-		rlLog "Contents of $bkup are $cont"
-		cont=$(cat $ktab)
-		rlLog "Contents of $ktab are $cont"
-	fi
-
+        klist -kt /etc/krb5.keytab
+        diff $bkup $ktab
+        if [ $? -eq 0 ]; then 
+            rlFail "FAIL - $bkup and $ktab match when they should not."
+            cont=$(cat $bkup)
+            rlLog "Contents of $bkup are $cont"
+            cont=$(cat $ktab)
+            rlLog "Contents of $ktab are $cont"
+        fi
     rlPhaseEnd
 }
 
