e4806dbc55214b46553731e829c69be5be266b26
Yi Zhang <yzhang@REDHAT.COM>
2013-03-19 22:31:09 +0000

    align code with fixed indention
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@5283 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/install-client-cli/t.client-install-primary-server.sh
+++ ipa-tests/beaker/ipa-server/acceptance/install-client-cli/t.client-install-primary-server.sh
@@ -1,6 +1,6 @@
 
 #################################################################################
-#                      test suite                                #
+#                      test suite                                               #
 #   perform the various combinations of install with fixed-primary parameter    #
 #################################################################################
 
@@ -12,37 +12,37 @@ SLAVE3=`echo $SLAVE|cut -d " " -f3 | xargs echo`
 
 clientinstall_primary_server()
 {
- setup #; touch /tmp/IPAQE_DEBUG_PAUSE
- ipaclientinstall_fixed_primary_param_TC_1  ; debug_pause
- ipaclientinstall_fixed_primary_param_TC_2  ; debug_pause
- ipaclientinstall_fixed_primary_param_TC_3  ; debug_pause
- ipaclientinstall_fixed_primary_param_TC_4  ; debug_pause
- ipaclientinstall_fixed_primary_param_TC_5  ; debug_pause
- ipaclientinstall_fixed_primary_param_TC_6  ; debug_pause
- ipaclientinstall_fixed_primary_param_TC_7  ; debug_pause
- ipaclientinstall_fixed_primary_param_TC_8  ; debug_pause
- ipaclientinstall_fixed_primary_param_TC_9  ; debug_pause
- ipaclientinstall_fixed_primary_param_TC_10  ; debug_pause
- ipaclientinstall_fixed_primary_param_TC_11  ; debug_pause
- ipaclientinstall_fixed_primary_param_TC_12  ; debug_pause
- ipaclientinstall_fixed_primary_param_TC_13  ; debug_pause
- ipaclientinstall_fixed_primary_param_TC_14  ; debug_pause
- ipaclientinstall_fixed_primary_param_TC_15  ; debug_pause
- ipaclientinstall_fixed_primary_param_TC_16  ; debug_pause
- ipaclientinstall_fixed_primary_param_TC_17  ; debug_pause
- ipaclientinstall_fixed_primary_param_TC_18  ; debug_pause
- ipaclientinstall_fixed_primary_param_TC_19  ; debug_pause
- #Added following test cases from t.ipa-client-install.sh here because it enables iptables on IPA Servers
- #ipaclientinstall_server_unreachableserver    ; debug_pause
+    setup #; touch /tmp/IPAQE_DEBUG_PAUSE
+    ipaclientinstall_fixed_primary_param_TC_1  ; debug_pause
+    ipaclientinstall_fixed_primary_param_TC_2  ; debug_pause
+    ipaclientinstall_fixed_primary_param_TC_3  ; debug_pause
+    ipaclientinstall_fixed_primary_param_TC_4  ; debug_pause
+    ipaclientinstall_fixed_primary_param_TC_5  ; debug_pause
+    ipaclientinstall_fixed_primary_param_TC_6  ; debug_pause
+    ipaclientinstall_fixed_primary_param_TC_7  ; debug_pause
+    ipaclientinstall_fixed_primary_param_TC_8  ; debug_pause
+    ipaclientinstall_fixed_primary_param_TC_9  ; debug_pause
+    ipaclientinstall_fixed_primary_param_TC_10 ; debug_pause
+    ipaclientinstall_fixed_primary_param_TC_11 ; debug_pause
+    ipaclientinstall_fixed_primary_param_TC_12 ; debug_pause
+    ipaclientinstall_fixed_primary_param_TC_13 ; debug_pause
+    ipaclientinstall_fixed_primary_param_TC_14 ; debug_pause
+    ipaclientinstall_fixed_primary_param_TC_15 ; debug_pause
+    ipaclientinstall_fixed_primary_param_TC_16 ; debug_pause
+    ipaclientinstall_fixed_primary_param_TC_17 ; debug_pause
+    ipaclientinstall_fixed_primary_param_TC_18 ; debug_pause
+    ipaclientinstall_fixed_primary_param_TC_19 ; debug_pause
+    #Added following test cases from t.ipa-client-install.sh here because it enables iptables on IPA Servers
+    #ipaclientinstall_server_unreachableserver    ; debug_pause
 }
 
 setup()
 {
-       rpm -qa|grep sssd-tools
-       if [ $? -eq 1 ];then
-         rlRun "yum install sssd-tools -y" 0 "Installing sssd-tools for clearing sssd cache"
-       fi
-       sleep 60
+        rpm -qa|grep sssd-tools
+        if [ $? -eq 1 ];then
+            rlRun "yum install sssd-tools -y" 0 "Installing sssd-tools for clearing sssd cache"
+        fi
+        sleep 60
 }
 
 ipaclientinstall_fixed_primary_param_TC_1()
@@ -57,9 +57,8 @@ ipaclientinstall_fixed_primary_param_TC_1()
         sssd_set_config_level
         rlRun "id admin;getent passwd admin;echo Secret123|kinit admin"
         rlAssertGrep "Option ipa_server has value $SERVER" "$sssd_log_file"
-	rlAssertGrep "Added Server $SERVER" "$sssd_log_file"
-	rlAssertGrep "Marking server '$SERVER' as 'working'" "$sssd_log_file"
-      
+        rlAssertGrep "Added Server $SERVER" "$sssd_log_file"
+        rlAssertGrep "Marking server '$SERVER' as 'working'" "$sssd_log_file"
     rlPhaseEnd
 }
 
@@ -73,8 +72,8 @@ ipaclientinstall_fixed_primary_param_TC_2()
         sssd_set_config_level
         rlRun "id admin;getent passwd admin;echo Secret123|kinit admin"
         rlAssertGrep "Option ipa_server has value $MASTER" "$sssd_log_file"
-	rlAssertGrep "Added Server $MASTER" "$sssd_log_file"
-	rlAssertGrep "Marking server '$MASTER' as 'working'" "$sssd_log_file"
+        rlAssertGrep "Added Server $MASTER" "$sssd_log_file"
+        rlAssertGrep "Marking server '$MASTER' as 'working'" "$sssd_log_file"
     rlPhaseEnd
 }
 
@@ -82,7 +81,7 @@ ipaclientinstall_fixed_primary_param_TC_3()
 {
     rlPhaseStartTest "client-install-fixed-primary-server 03 [Negative] fixed primary with invalid server"
        uninstall_fornexttest
-        host_del
+       host_del
        rlLog "EXECUTING: ipa-client-install -p $ADMINID -w $ADMINPW --fixed-primary --server=invalid --domain=$DOMAIN -U"
        command="ipa-client-install -p $ADMINID -w $ADMINPW --fixed-primary --server=invalid --domain=$DOMAIN -U"
        expmsg="invalid is not an IPA v2 Server.
@@ -103,8 +102,8 @@ ipaclientinstall_fixed_primary_param_TC_4()
         sssd_set_config_level
         rlRun "id admin;getent passwd admin;echo Secret123|kinit admin"
         rlAssertGrep "Option ipa_server has value $SLAVE1" "$sssd_log_file"
-	rlAssertGrep "Added Server $SLAVE1" "$sssd_log_file"
-	rlAssertGrep "Marking server '$SLAVE1' as 'working'" "$sssd_log_file"
+        rlAssertGrep "Added Server $SLAVE1" "$sssd_log_file"
+        rlAssertGrep "Marking server '$SLAVE1' as 'working'" "$sssd_log_file"
     rlPhaseEnd
 }
 
@@ -115,7 +114,7 @@ ipaclientinstall_fixed_primary_param_TC_5()
         host_del
         rlLog "EXECUTING: ipa-client-install -p $ADMINID -w $ADMINPW --fixed-primary --server=$MASTER --server=$SLAVE2 --domain=$DOMAIN --realm=$RELM -U"
         rlRun "ipa-client-install -p $ADMINID -w $ADMINPW --fixed-primary --server=$MASTER --server=$SLAVE2 --domain=$DOMAIN --realm=$RELM -U"
-		rlRun "cat $SSSD"
+        rlRun "cat $SSSD"
         rlAssertGrep "ipa_server = $MASTER, $SLAVE2" "$SSSD"
         ipaclientinstall_bugcheck_910410 $MASTER $SLAVE2
         sssd_set_config_level
@@ -125,7 +124,7 @@ ipaclientinstall_fixed_primary_param_TC_5()
         rlAssertGrep "Added Server $SLAVE2" "$sssd_log_file"
         rlAssertGrep "Marking server '$MASTER' as 'working'" "$sssd_log_file"
         rlAssertNotGrep "Marking server '$SLAVE2' as 'working'" "$sssd_log_file"
-		debug_log
+        debug_log
     rlPhaseEnd
 }
 
@@ -134,10 +133,10 @@ ipaclientinstall_fixed_primary_param_TC_6()
     rlPhaseStartTest "client-install-fixed-primary-server 06 [Positive] fixed primary with --server=SLAVE --server=MASTER"
         uninstall_fornexttest
         host_del
-		rlRun "sleep 60"
+        rlRun "sleep 60"
         rlLog "EXECUTING: ipa-client-install -p $ADMINID -w $ADMINPW --fixed-primary --server=$SLAVE3 --server=$MASTER --domain=$DOMAIN --realm=$RELM -U"
         rlRun "ipa-client-install -p $ADMINID -w $ADMINPW --fixed-primary --server=$SLAVE3 --server=$MASTER --domain=$DOMAIN --realm=$RELM -U"
-		rlRun "cat $SSSD"
+        rlRun "cat $SSSD"
         rlAssertGrep "ipa_server = $SLAVE3, $MASTER" "$SSSD"
         ipaclientinstall_bugcheck_910410 $SLAVE3 $MASTER
         sssd_set_config_level
@@ -147,7 +146,7 @@ ipaclientinstall_fixed_primary_param_TC_6()
         rlAssertGrep "Added Server $MASTER" "$sssd_log_file"
         rlAssertGrep "Marking server '$SLAVE3' as 'working'" "$sssd_log_file"
         rlAssertNotGrep "Marking server '$MASTER' as 'working'" "$sssd_log_file"
-		debug_log
+        debug_log
     rlPhaseEnd
 }
 
@@ -156,18 +155,18 @@ ipaclientinstall_fixed_primary_param_TC_7()
     rlPhaseStartTest "client-install-fixed-primary-server 07 [Positive] fixed primary with --server=MASTER --server=SLAVE --server=invalid"
         uninstall_fornexttest
         rlLog "EXECUTING: ipa-client-install -p $ADMINID -w $ADMINPW --fixed-primary --server=$MASTER --server=$SLAVE1 --server=invalid --domain=$DOMAIN --realm=$RELM -U"
-       command="ipa-client-install -p $ADMINID -w $ADMINPW --fixed-primary --server=$MASTER --server=$SLAVE1 --server=invalid --domain=$DOMAIN --realm=$RELM -U"
-       expmsg="Client configuration complete"
-       local tmpout=$TmpDir/ipaclientinstall_multiple_servers_one_invalidserver.out
-       qaRun "$command" "$tmpout" 0 "$expmsg" "Verify IPA Install with invalid server"
-       rlAssertNotGrep "ipa_server.*invalid" /etc/sssd/sssd.conf
-       if [ $? -gt 0 ]; then
-           rlFail "BZ 905626 found...ipa-client-install failed to fall over to replica with master down"
-           rlFail "BZ 905626 fix will prevent invalid servers from being included list"
-       else
-           rlPass "BZ 905626 not found"
-           rlPass "--server entry invalid not seen in sssd.conf"
-       fi
+        command="ipa-client-install -p $ADMINID -w $ADMINPW --fixed-primary --server=$MASTER --server=$SLAVE1 --server=invalid --domain=$DOMAIN --realm=$RELM -U"
+        expmsg="Client configuration complete"
+        local tmpout=$TmpDir/ipaclientinstall_multiple_servers_one_invalidserver.out
+        qaRun "$command" "$tmpout" 0 "$expmsg" "Verify IPA Install with invalid server"
+        rlAssertNotGrep "ipa_server.*invalid" /etc/sssd/sssd.conf
+        if [ $? -gt 0 ]; then
+            rlFail "BZ 905626 found...ipa-client-install failed to fall over to replica with master down"
+            rlFail "BZ 905626 fix will prevent invalid servers from being included list"
+        else
+            rlPass "BZ 905626 not found"
+            rlPass "--server entry invalid not seen in sssd.conf"
+        fi
     rlPhaseEnd
 }
 
@@ -186,7 +185,7 @@ ipaclientinstall_fixed_primary_param_TC_8()
         rlRun "ipa-client-install --fixed-primary -p $ADMINID -w $ADMINPW -U --no-sssd" 0 "Installing ipa client and configuring - with no SSSD configured"
         verify_install true nosssd
         rlAssertNotGrep "Option ipa_server has value $MASTER" "$sssd_log_file"
-		debug_log
+        debug_log
     rlPhaseEnd
 }
 
@@ -198,68 +197,68 @@ ipaclientinstall_fixed_primary_param_TC_9()
 
         # To set up an sssd.conf that can be preserved: 
         # create a sssd.conf
-          writesssdconf
-          chmod 0600 $SSSD
-          ls -l /etc/sssd
+        writesssdconf
+        chmod 0600 $SSSD
+        ls -l /etc/sssd
         # update /etc/nsswitch.conf, and vim /etc/pam.d/system-auth to use sssd
-          rlLog "Executing: authconfig --enablesssd --enablesssdauth --updateall"
-          rlRun "authconfig --enablesssd --enablesssdauth --updateall" 0 "Authconfig"
+        rlLog "Executing: authconfig --enablesssd --enablesssdauth --updateall"
+        rlRun "authconfig --enablesssd --enablesssdauth --updateall" 0 "Authconfig"
         # restart sssd service
-          rlServiceStop "sssd"
-          if [ $? -ne 0 ]; then
-             rlLog "Failed to stop sssd service"
-          fi
-          rlServiceStart "sssd"
-          if [ $? -ne 0 ]; then
-             rlLog "Failed to start sssd service"
-          fi
+        rlServiceStop "sssd"
+        if [ $? -ne 0 ]; then
+            rlLog "Failed to stop sssd service"
+        fi
+        rlServiceStart "sssd"
+        if [ $? -ne 0 ]; then
+            rlLog "Failed to start sssd service"
+        fi
         # edit /etc/krb5.conf
-          rlLog "Executing: perl -pi -e 's/EXAMPLE.COM/TESTRELM.COM/g' $KRB5"
-          rlRun "perl -pi -e 's/EXAMPLE.COM/TESTRELM.COM/g' $KRB5" 0 "Updating $KRB5"
-          rlLog "Executing: perl -pi -e 's/example.com/testrelm.com/g' $KRB5"
-          rlRun "perl -pi -e 's/example.com/testrelm.com/g' $KRB5" 0 "Updating $KRB5"
-          rlLog "Executing: perl -pi -e 's/kerberos.example.com/$MASTER/g' $KRB5"
-          rlRun "perl -pi -e 's/kerberos.example.com/$MASTER/g' $KRB5" 0 "Updating $KRB5"
+        rlLog "Executing: perl -pi -e 's/EXAMPLE.COM/TESTRELM.COM/g' $KRB5"
+        rlRun "perl -pi -e 's/EXAMPLE.COM/TESTRELM.COM/g' $KRB5" 0 "Updating $KRB5"
+        rlLog "Executing: perl -pi -e 's/example.com/testrelm.com/g' $KRB5"
+        rlRun "perl -pi -e 's/example.com/testrelm.com/g' $KRB5" 0 "Updating $KRB5"
+        rlLog "Executing: perl -pi -e 's/kerberos.example.com/$MASTER/g' $KRB5"
+        rlRun "perl -pi -e 's/kerberos.example.com/$MASTER/g' $KRB5" 0 "Updating $KRB5"
 
-          rlLog "Check SELinux context of /etc/krb5.conf"
-          rlRun "ls -lZ /etc/krb5.conf"
+        rlLog "Check SELinux context of /etc/krb5.conf"
+        rlRun "ls -lZ /etc/krb5.conf"
 
-     #   rlRun "kinitAs $ADMINID $ADMINPW" 0 "Get administrator credentials before installing"
+        #   rlRun "kinitAs $ADMINID $ADMINPW" 0 "Get administrator credentials before installing"
 
         #install ipa-client with --preserve-sssd
-       #saving files to compare for troubleshooting later, if needed
-       cp $SSSD $TmpDir
-       mv $TmpDir/sssd.conf $TmpDir/sssd.conf_beforeinstall
-       cp $KRB5 $TmpDir
-       mv $TmpDir/krb5.conf $TmpDir/krb5.conf_beforeinstall
-       AVCCHKTS=$(date +%H:%M:%S)
-       rlLog "EXECUTING: ipa-client-install --fixed-primary --domain=$DOMAIN --realm=$RELM  -p $ADMINID -w $ADMINPW -U --server=$MASTER --preserve-sssd"
-       rlRun "ipa-client-install --fixed-primary --domain=$DOMAIN --realm=$RELM  -p $ADMINID -w $ADMINPW -U --server=$MASTER --preserve-sssd" 0 "Installing ipa client with preserve-sssd"
-       rlRun "cat /etc/sssd/sssd.conf"
-       rlRun "cat $SSSD"
-       rlAssertGrep "ipa_server = $MASTER" "$SSSD"
-       sssd_set_config_level
-       rlRun "id admin@LDAP-KRB5;getent passwd admin@LDAP-KRB5;echo Secret123|kinit admin"
-       rlAssertGrep "Option ipa_server has value $MASTER" "$sssd_log_file"
-       rlAssertGrep "Marking server '$MASTER' as 'working'" "$sssd_log_file"
-       submit_log $sssd_log_file
-       rlAssertGrep "Added Server $MASTER" "/var/log/sssd/sssd_LDAP-KRB5.log"
-       rlAssertGrep "Marking server '$MASTER' as 'working'" "/var/log/sssd/sssd_LDAP-KRB5.log"
+        #saving files to compare for troubleshooting later, if needed
+        cp $SSSD $TmpDir
+        mv $TmpDir/sssd.conf $TmpDir/sssd.conf_beforeinstall
+        cp $KRB5 $TmpDir
+        mv $TmpDir/krb5.conf $TmpDir/krb5.conf_beforeinstall
+        AVCCHKTS=$(date +%H:%M:%S)
+        rlLog "EXECUTING: ipa-client-install --fixed-primary --domain=$DOMAIN --realm=$RELM  -p $ADMINID -w $ADMINPW -U --server=$MASTER --preserve-sssd"
+        rlRun "ipa-client-install --fixed-primary --domain=$DOMAIN --realm=$RELM  -p $ADMINID -w $ADMINPW -U --server=$MASTER --preserve-sssd" 0 "Installing ipa client with preserve-sssd"
+        rlRun "cat /etc/sssd/sssd.conf"
+        rlRun "cat $SSSD"
+        rlAssertGrep "ipa_server = $MASTER" "$SSSD"
+        sssd_set_config_level
+        rlRun "id admin@LDAP-KRB5;getent passwd admin@LDAP-KRB5;echo Secret123|kinit admin"
+        rlAssertGrep "Option ipa_server has value $MASTER" "$sssd_log_file"
+        rlAssertGrep "Marking server '$MASTER' as 'working'" "$sssd_log_file"
+        submit_log $sssd_log_file
+        rlAssertGrep "Added Server $MASTER" "/var/log/sssd/sssd_LDAP-KRB5.log"
+        rlAssertGrep "Marking server '$MASTER' as 'working'" "/var/log/sssd/sssd_LDAP-KRB5.log"
 
-       cp $SSSD $TmpDir
-       mv $TmpDir/sssd.conf $TmpDir/sssd.conf_afterinstall
-       cp $KRB5 $TmpDir
-       mv $TmpDir/krb5.conf $TmpDir/krb5.conf_afterinstall
+        cp $SSSD $TmpDir
+        mv $TmpDir/sssd.conf $TmpDir/sssd.conf_afterinstall
+        cp $KRB5 $TmpDir
+        mv $TmpDir/krb5.conf $TmpDir/krb5.conf_afterinstall
 
         # be able to kinit
-          verify_kinit true
+        verify_kinit true
         sleep 20
         # verify sssd contents were preserved
         verify_sssd true preserve
 
         rlRun "kinitAs $ADMINID $ADMINPW" 0 "Get administrator credentials before uninstalling"
         #uninstall_fornexttest
-		debug_log
+        debug_log
     rlPhaseEnd
 }
 
@@ -286,7 +285,7 @@ ipaclientinstall_fixed_primary_param_TC_10()
         rlRun "echo \"ipactl start\" > $TmpDir/local.sh"
         rlRun "chmod +x $TmpDir/local.sh"
         rlRun "ssh -o StrictHostKeyChecking=no root@$MASTERIP 'bash -s' < $TmpDir/local.sh" 0 "Start MASTER IPA server"
-		debug_log
+        debug_log
     rlPhaseEnd
 }
 
@@ -316,7 +315,7 @@ ipaclientinstall_fixed_primary_param_TC_11()
         rlRun "echo \"ipactl start\" > $TmpDir/local.sh"
         rlRun "chmod +x $TmpDir/local.sh"
         rlRun "ssh -o StrictHostKeyChecking=no root@$MASTERIP 'bash -s' < $TmpDir/local.sh" 0 "Start MASTER IPA server"
-		debug_log
+        debug_log
     rlPhaseEnd
 }
 
@@ -624,24 +623,24 @@ ipaclientinstall_fixed_primary_param_TC_19()
 
 sssd_set_config_level()
 {
-  rlRun "sed '/cache_credentials/ a debug_level = 9' $SSSD > /tmp/sssd.conf"
-  rlRun "cp /tmp/sssd.conf $SSSD"
-  rlRun "cat /dev/null > $sssd_log_file"
-  rlRun "service sssd restart"
-  rlRun "sss_cache -u admin"
+    rlRun "sed '/cache_credentials/ a debug_level = 9' $SSSD > /tmp/sssd.conf"
+    rlRun "cp /tmp/sssd.conf $SSSD"
+    rlRun "cat /dev/null > $sssd_log_file"
+    rlRun "service sssd restart"
+    rlRun "sss_cache -u admin"
 }
 
 host_del()
 {
-        rlRun "echo \"echo Secret123|kinit admin;ipa host-del $CLIENT\" > $TmpDir/local.sh"
-        rlRun "chmod +x $TmpDir/local.sh"
-        rlRun "ssh -o StrictHostKeyChecking=no root@$MASTERIP 'bash -s' < $TmpDir/local.sh" 0,1,2
-		#0 "Deleting host from MASTER IPA server"
-		rlRun "sleep 10"
+    rlRun "echo \"echo Secret123|kinit admin;ipa host-del $CLIENT\" > $TmpDir/local.sh"
+    rlRun "chmod +x $TmpDir/local.sh"
+    rlRun "ssh -o StrictHostKeyChecking=no root@$MASTERIP 'bash -s' < $TmpDir/local.sh" 0,1,2
+    #0 "Deleting host from MASTER IPA server"
+    rlRun "sleep 10"
 }
 
 debug_log()
 {
-	cp /var/log/ipaclient-install.log /var/log/ipaclient-install.log.$FUNC
-	submit_log /var/log/ipaclient-install.log.$FUNC
+    cp /var/log/ipaclient-install.log /var/log/ipaclient-install.log.$FUNC
+    submit_log /var/log/ipaclient-install.log.$FUNC
 }
