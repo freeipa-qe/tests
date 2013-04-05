ecfcd003c90c7ca6f23325416affcda7f9a3fb8a
Xiyang Dong <xdong@REDHAT.COM>
2013-04-05 14:38:59 +0000

    verifying bz_869741
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@5384 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-trust-cli/t.ipa-trust-cli.bug.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-trust-cli/t.ipa-trust-cli.bug.sh
@@ -1,12 +1,23 @@
 . ./adlib.sh
 . ./Config
+. ./t.ipa-trust-cli.sh
 
 ADnetbios="ADLAB"
 
 
 bz_867442() {
-	rlPhaseStartTest "Adtrust install with AD  netbios name" 
+	rlPhaseStartTest "Adtrust install with AD netbios name" 
 		rlRun "NBAD_Exp" 0 "Creating expect script" 
 		rlRun "$exp $expfile netbios-name $ADnetbios $ADdomain $ADadmin $ADpswd" 2 "Giving AD Netbios name fails as expected"
+		rlRun "$trust_bin --netbios-name=$NBname -a $adminpw -U" 0 "Set netbios name back"
 	rlPhaseEnd
-} 
+}
+
+bz_869741() {
+        rlPhaseStartTest "Re-adding an existing entry in trust"
+                rlRun "Readd_Exp" 0 "Creating expect script"
+                rlRun "$exp $expfile $ADdomain $ADadmin $ADpswd" 1 "Reading trust as expected"
+		rlRun 'ipa help trust-add|head -11 |grep "multiple times against same domain"' 0 "Trust-add help page optimized as expected"
+        rlPhaseEnd
+}
+ 
