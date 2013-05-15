f7da269e328456b5ab64f9dc935b19809b39c9b8
Scott Poore <spoore@REDHAT.COM>
2013-05-15 15:13:37 +0000

    ipa_upgrade: remove ldap debugging for replication issue for selinuxusermap entry missing in ipa_upgrade_client_replica_master_all_3: for BZ 962885
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@5509 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-upgrade/tests.d/t.ipa_upgrade_tests.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-upgrade/tests.d/t.ipa_upgrade_tests.sh
@@ -283,66 +283,13 @@ ipa_upgrade_client_replica_master_all()
     rlPhaseEnd
 
     rlPhaseStartTest "ipa_upgrade_client_replica_master_all_2: test upgrade with old master, new replica, and new client"
-        rlRun "ipa-replica-manage -p $ADMINPW list -v $MASTER_S.$DOMAIN"
-        rlRun "ipa-replica-manage -p $ADMINPW list -v $REPLICA1_S.$DOMAIN"
-        ldapsearch -xLLL -h localhost \
-            -D "$ROOTDN" \
-            -w "$ROOTDNPWD" \
-            -b "cn=mapping tree,cn=config" \
-            objectClass=nsDS5ReplicationAgreement \
-            > /var/log/repagreements.$(hostname -s).replica.0
-        rlRun "rhts-submit-log -l /var/log/repagreements.$(hostname -s).replica.0"
-
         upgrade_replica
-
-        ldapsearch -xLLL -h localhost \
-            -D "$ROOTDN" \
-            -w "$ROOTDNPWD" \
-            -b "cn=mapping tree,cn=config" \
-            objectClass=nsDS5ReplicationAgreement \
-            > /var/log/repagreements.$(hostname -s).replica.1
-        rlRun "rhts-submit-log -l /var/log/repagreements.$(hostname -s).replica.1"
-
-        log="/var/log/dirsrv/slapd-TESTRELM-COM/errors"
-        if [ $(echo "$MYROLE" |egrep "REPLICA|MASTER"|wc -l) -gt 0 ]; then
-            rlLog "DEBUGGING:"
-            rlRun "cp $log $log.ipa_upgrade_client_replica_master_all_2"
-            rlRun "rhts-submit-log -l $log.ipa_upgrade_client_replica_master_all_2"
-        fi
         ipa_upgrade_data_check $MYBEAKERREPLICA1 $LATESTVER old
     rlPhaseEnd
 
     rlPhaseStartTest "ipa_upgrade_client_replica_master_all_3: test upgrade with new master, new replica, and new client [BZ962885]"
-        rlRun "ipa-replica-manage -p $ADMINPW list -v $MASTER_S.$DOMAIN"
-        rlRun "ipa-replica-manage -p $ADMINPW list -v $REPLICA1_S.$DOMAIN"
-
-        ldapsearch -xLLL -h localhost \
-            -D "$ROOTDN" \
-            -w "$ROOTDNPWD" \
-            -b "cn=mapping tree,cn=config" \
-            objectClass=nsDS5ReplicationAgreement \
-            > /var/log/repagreements.$(hostname -s).master.0
-        rlRun "rhts-submit-log -l /var/log/repagreements.$(hostname -s).master.0"
-
         upgrade_master 
         ipa_upgrade_data_add $MYBEAKERMASTER $LATESTVER
-
-        ldapsearch -xLLL -h localhost \
-            -D "$ROOTDN" \
-            -w "$ROOTDNPWD" \
-            -b "cn=mapping tree,cn=config" \
-            objectClass=nsDS5ReplicationAgreement \
-            > /var/log/repagreements.$(hostname -s).master.1
-        rlRun "rhts-submit-log -l /var/log/repagreements.$(hostname -s).master.1"
-
-
-        log="/var/log/dirsrv/slapd-TESTRELM-COM/errors"
-        if [ $(echo "$MYROLE" |egrep "REPLICA|MASTER"|wc -l) -gt 0 ]; then
-            rlRun "ipa-replica-manage -p $ADMINPW list -v `hostname`"
-            rlRun "cp $log $log.ipa_upgrade_client_replica_master_all_3"
-            rlRun "rhts-submit-log -l $log.ipa_upgrade_client_replica_master_all_3"
-            #rlRun "ipa-replica-manage -p $ADMINPW re-initialize --from=$MASTER"
-        fi
         ipa_upgrade_data_check $MYBEAKERMASTER $LATESTVER new
         ipa_upgrade_data_check $MYBEAKERREPLICA1 $LATESTVER new
         ipa_upgrade_data_check $MYBEAKERCLIENT $LATESTVER new
