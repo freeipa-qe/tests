7ba12f162e841ab058f67c6a969fbe8ccf50ecf7
Yi Zhang <yzhang@REDHAT.COM>
2013-05-21 13:41:53 +0000

    change /sbin/ipa-client-install to ipa-client-install
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@5592 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/install-client-cli/t.ipa-client-install.extended.sssd.sh
+++ ipa-tests/beaker/ipa-server/acceptance/install-client-cli/t.ipa-client-install.extended.sssd.sh
@@ -11,7 +11,7 @@ ipaclientinstall_sssd_options_0001_test_single_option__permit(){
     rlPhaseStartTest "ipaclientinstall_sssd_options_0001_test_single_option__permit"
         rlLog " Single Option test: --permit"
         rlLog "Test Data: --server:$MASTER --password:$ADMINPW --unattended --realm:$RELM --domain:$DOMAIN --principal:$ADMINID --permit "
-        rlRun "/sbin/ipa-client-install --server=$MASTER --password=$ADMINPW --unattended --realm=$RELM --domain=$DOMAIN --principal=$ADMINID --permit"  
+        rlRun "ipa-client-install --server=$MASTER --password=$ADMINPW --unattended --realm=$RELM --domain=$DOMAIN --principal=$ADMINID --permit"  
         CheckConfig permit # Verify for: --permit
         rlRun "ipa-client-install --uninstall -U" 0 "uninstall ipa client"
     rlPhaseEnd
@@ -21,7 +21,7 @@ ipaclientinstall_sssd_options_0002_test_single_option__enable_dns_updates(){
     rlPhaseStartTest "ipaclientinstall_sssd_options_0002_test_single_option__enable_dns_updates"
         rlLog " Single Option test: --enable_dns_updates"
         rlLog "Test Data: --server:$MASTER --password:$ADMINPW --unattended --realm:$RELM --domain:$DOMAIN --principal:$ADMINID --enable_dns_updates "
-        rlRun "/sbin/ipa-client-install --server=$MASTER --password=$ADMINPW --unattended --realm=$RELM --domain=$DOMAIN --principal=$ADMINID --enable-dns-updates"  
+        rlRun "ipa-client-install --server=$MASTER --password=$ADMINPW --unattended --realm=$RELM --domain=$DOMAIN --principal=$ADMINID --enable-dns-updates"  
         CheckConfig enable_dns_updates  # Verify for: --enable-dns-updates
         rlRun "ipa-client-install --uninstall -U" 0 "uninstall ipa client"
     rlPhaseEnd
@@ -31,7 +31,7 @@ ipaclientinstall_sssd_options_0003_test_single_option__no_sssd(){
     rlPhaseStartTest "ipaclientinstall_sssd_options_0003_test_single_option__no_sssd"
         rlLog " Single Option test: --no-sssd"
         rlLog "Test Data: --server:$MASTER --password:$ADMINPW --unattended --realm:$RELM --domain:$DOMAIN --principal:$ADMINID --no-sssd:$HOSTNAME "
-        rlRun "/sbin/ipa-client-install --server=$MASTER --password=$ADMINPW --unattended --realm=$RELM --domain=$DOMAIN --principal=$ADMINID --no-sssd"  
+        rlRun "ipa-client-install --server=$MASTER --password=$ADMINPW --unattended --realm=$RELM --domain=$DOMAIN --principal=$ADMINID --no-sssd"  
         CheckConfig no_sssd  # Verify for: --no-sssd
         rlRun "ipa-client-install --uninstall -U" 0 "uninstall ipa client"
     rlPhaseEnd
@@ -41,7 +41,7 @@ ipaclientinstall_sssd_options_0004_test_single_option__no_krb5_offline_passwords
     rlPhaseStartTest "ipaclientinstall_sssd_options_0004_test_single_option__no_krb5_offline_passwords"
         rlLog " Single Option test: --no-krb5-offline-passwords"
         rlLog "Test Data: --server:$MASTER --password:$ADMINPW --unattended --realm:$RELM --domain:$DOMAIN --principal:$ADMINID --no-krb5-offline-passwords "
-        rlRun "/sbin/ipa-client-install --server=$MASTER --password=$ADMINPW --unattended --realm=$RELM --domain=$DOMAIN --principal=$ADMINID --no-krb5-offline-passwords"
+        rlRun "ipa-client-install --server=$MASTER --password=$ADMINPW --unattended --realm=$RELM --domain=$DOMAIN --principal=$ADMINID --no-krb5-offline-passwords"
         CheckConfig no_krb5_offline_passwords # Verify for: --no-krb5-offline-passwords
         rlRun "ipa-client-install --uninstall -U" 0 "uninstall ipa client"
     rlPhaseEnd
@@ -51,7 +51,7 @@ ipaclientinstall_sssd_options_0005_test_single_option__preserve_sssd(){
     rlPhaseStartTest "ipaclientinstall_sssd_options_0005_test_single_option__preserve_sssd"
         rlLog " Single Option test: --preserve-sssd"
         rlLog "Test Data: --server:$MASTER --password:$ADMINPW --unattended --realm:$RELM --domain:$DOMAIN --principal:$ADMINID --preserve-sssd "
-        rlRun "/sbin/ipa-client-install --server=$MASTER --password=$ADMINPW --unattended --realm=$RELM --domain=$DOMAIN --principal=$ADMINID --preserve-sssd"  
+        rlRun "ipa-client-install --server=$MASTER --password=$ADMINPW --unattended --realm=$RELM --domain=$DOMAIN --principal=$ADMINID --preserve-sssd"  
         CheckConfig preserv_sssd  # Verify for: --preserve-sssd
         rlRun "ipa-client-install --uninstall -U" 0 "uninstall ipa client"
     rlPhaseEnd
