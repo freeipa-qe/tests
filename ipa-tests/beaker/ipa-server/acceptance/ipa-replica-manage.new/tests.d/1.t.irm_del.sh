f258e4424e83d145f25bd841339d9e7fea600aa4
Scott Poore <spoore@REDHAT.COM>
2013-05-24 18:29:44 +0000

    ipa-replica-manage.new: - change bugchecks to remove them from running as separate phases.  They're already run inside of existing phases. - add del tests to 1.t.irm_del.sh
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@5660 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-replica-manage.new/tests.d/1.t.irm_del.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-replica-manage.new/tests.d/1.t.irm_del.sh
@@ -17,19 +17,49 @@ function irm_del_pos_0001()
     rlPhaseStartTest "irm_del_pos_0001: del, replica4"
     case "$MYROLE" in
     MASTER_*)
-        rlRun "rhts-sync-set -s '$TESTCOUNT.$FUNCNAME.0' -m $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BR3"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.1' $MY_BR4"
         ;;
     REPLICA1_*)
-        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BR3"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.1' $MY_BR4"
         ;;
     REPLICA2_*)
-        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BR3"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.1' $MY_BR4"
         ;;
     REPLICA3_*)
-        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        # Setup
+        testuser1="testuser$(date +%H%M%S)"
+        sleep 1
+        testuser2="testuser$(date +%H%M%S)"
+        irm_useradd $REPLICA3 $testuser1
+        irm_userchk $REPLICA4 $testuser1
+
+        # Test
+        rlRun "ipa-replica-manage $PWOPT del $REPLICA4 -f > $tmpout 2>&1"
+        rlRun "cat $tmpout"
+        rlAssertGrep "Deleted replication agreement from '$REPLICA3' to '$REPLICA4'" $tmpout
+        rlRun "ipa-replica-manage $PWOPT list $REPLICA3 > $tmpout 2>&1"
+        rlRun "cat $tmpout"
+        rlAssertNotGrep "$REPLICA4" $tmpout
+        irm_userdel $REPLICA3 $testuser1
+        irm_userchk $REPLICA3 $testuser1 2
+        irm_userchk $REPLICA4 $testuser1
+        irm_useradd $REPLICA3 $testuser2
+        irm_userchk $REPLICA4 $testuser2 2
+
+        rlRun "rhts-sync-set -s '$TESTCOUNT.$FUNCNAME.0' -m $MY_BR3"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.1' $MY_BR4"
         ;;
     REPLICA4_*)
-        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BR3"
+
+        # Cleanup
+        irm_uninstall
+        irm_install $REPLICA3
+
+        rlRun "rhts-sync-set -s '$TESTCOUNT.$FUNCNAME.1' -m $MY_BR4"
         ;;
     *)
     esac
@@ -50,19 +80,49 @@ function irm_del_pos_0002()
     rlPhaseStartTest "irm_del_pos_0002: del, replica4, remote"
     case "$MYROLE" in
     MASTER_*)
-        rlRun "rhts-sync-set -s '$TESTCOUNT.$FUNCNAME.0' -m $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BR2"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.1' $MY_BR1"
         ;;
     REPLICA1_*)
-        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BR2"
+        # Cleanup
+        irm_uninstall
+        irm_install $REPLICA1
+
+        rlRun "rhts-sync-set -s '$TESTCOUNT.$FUNCNAME.1' -m $MY_BR1"
         ;;
     REPLICA2_*)
-        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        # Setup
+        testuser1="testuser$(date +%H%M%S)"
+        sleep 1
+        testuser2="testuser$(date +%H%M%S)"
+        irm_useradd $MASTER $testuser1
+        irm_userchk $REPLICA1 $testuser1
+
+        # Test
+        rlRun "ipa-replica-manage $PWOPT -H $MASTER del $REPLICA1 -f > $tmpout 2>&1"
+        rlRun "cat $tmpout"
+        rlAssertGrep "Deleted replication agreement from '$MASTER' to '$REPLICA1'" $tmpout
+        rlRun "ipa-replica-manage $PWOPT list $MASTER > $tmpout 2>&1"
+        rlRun "cat $tmpout"
+        rlAssertNotGrep "$REPLICA1" $tmpout
+        irm_userdel $MASTER $testuser1
+        irm_userchk $MASTER $testuser1 2
+        irm_userchk $REPLICA1 $testuser1
+        irm_useradd $MASTER $testuser2
+        irm_userchk $REPLICA1 $testuser2 2
+        irm_userdel $MASTER $testuser2
+
+        rlRun "rhts-sync-set -s '$TESTCOUNT.$FUNCNAME.0' -m $MY_BR2"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.1'  $MY_BR1"
         ;;
     REPLICA3_*)
-        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BR2"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.1' $MY_BR1"
         ;;
     REPLICA4_*)
-        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BR2"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.1' $MY_BR1"
         ;;
     *)
     esac
@@ -70,31 +130,56 @@ function irm_del_pos_0002()
 }
 
 # irm_del_neg_0001 - del fail, with disconnected agreement
-#    ipa-replica-manage disconnect $REPLICA3 $REPLICA4
-#    ipa-replica-manage del $REPLICA3
+#    ipa-replica-manage disconnect $REPLICA2 $REPLICA2
+#    ipa-replica-manage del $REPLICA4
 #    irm_bugcheck_826677
-#    ipa-replica-manage list $REPLICA2|grep $REPLICA3
+#    ipa-replica-manage list $REPLICA2|grep $REPLICA3 #fail
 #    ipa-replica-manage connect $REPLICA3 $REPLICA4
 function irm_del_neg_0001()
 {
     tmpout=/tmp/test_${FUNCNAME}.out
     TESTCOUNT=$(( TESTCOUNT += 1 ))
-    rlPhaseStartTest "irm_del_neg_0001: del fail, with disconnected agreement"
+    rlPhaseStartTest "irm_del_neg_0001: del fail, with disconnected agreement [BZ826677]"
     case "$MYROLE" in
     MASTER_*)
-        rlRun "rhts-sync-set -s '$TESTCOUNT.$FUNCNAME.0' -m $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BR2"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.1' $MY_BR3"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.2' $MY_BR2"
         ;;
     REPLICA1_*)
-        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BR2"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.1' $MY_BR3"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.2' $MY_BR2"
         ;;
     REPLICA2_*)
-        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        # Setup
+        rlRun "ipa-replica-manage $PWOPT disconnect $REPLICA2 $REPLICA3"
+        
+        rlRun "rhts-sync-set -s '$TESTCOUNT.$FUNCNAME.0' -m $MY_BR2"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.1'  $MY_BR3"
+
+        # Cleanup
+        rlRun "ipa-replica-manage $PWOPT connect $REPLICA2 $REPLICA3"
+
+        rlRun "rhts-sync-set -s '$TESTCOUNT.$FUNCNAME.2' -m $MY_BR2"
         ;;
     REPLICA3_*)
-        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BR2"
+
+        # Test
+        rlRun "ipa-replica-manage $PWOPT del $REPLICA4 -f > $tmpout 2>&1" 1
+        rlRun "cat $tmpout"
+        irm_bugcheck_826677 $tmpout
+        rlRun "ipa-replica-manage $PWOPT list $REPLICA2 > $tmpout 2>&1"
+        rlAssertNotGrep "$REPLICA3" $tmpout
+
+        rlRun "rhts-sync-set -s '$TESTCOUNT.$FUNCNAME.1' -m $MY_BR3"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.2' $MY_BR2"
         ;;
     REPLICA4_*)
-        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BR2"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.1' $MY_BR3"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.2' $MY_BR2"
         ;;
     *)
     esac
@@ -114,19 +199,32 @@ function irm_del_neg_0002()
     rlPhaseStartTest "irm_del_neg_0002: del fail, with disconnected agreement, remote"
     case "$MYROLE" in
     MASTER_*)
-        rlRun "rhts-sync-set -s '$TESTCOUNT.$FUNCNAME.0' -m $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BR2"
         ;;
     REPLICA1_*)
-        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BR2"
         ;;
     REPLICA2_*)
-        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        # Setup
+        rlRun "ipa-replica-manage $PWOPT disconnect $REPLICA2 $REPLICA3"
+        
+        # Test
+        rlRun "ipa-replica-manage $PWOPT -H $REPLICA3 del $REPLICA4 -f > $tmpout 2>&1" 1
+        rlRun "cat $tmpout"
+        irm_bugcheck_826677 $tmpout
+        rlRun "ipa-replica-manage $PWOPT list $REPLICA2 > $tmpout 2>&1"
+        rlAssertNotGrep "$REPLICA3" $tmpout
+
+        # Cleanup
+        rlRun "ipa-replica-manage $PWOPT connect $REPLICA2 $REPLICA3"
+
+        rlRun "rhts-sync-set -s '$TESTCOUNT.$FUNCNAME.0' -m $MY_BR2"
         ;;
     REPLICA3_*)
-        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BR2"
         ;;
     REPLICA4_*)
-        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BR2"
         ;;
     *)
     esac
@@ -146,19 +244,33 @@ function irm_del_neg_0003()
     rlPhaseStartTest "irm_del_neg_0003: del fail, already deleted agreement [BZ754524]"
     case "$MYROLE" in
     MASTER_*)
+        rlRun "ipa-replica-manage $PWOPT del $REPLICA1 -f"
+        rlRun "ipa-replica-manage $PWOPT del $REPLICA1 -f > $tmpout 2>&1" 1
+        rlRun "cat $tmpout"
+        rlAssertGrep "'$MASTER' has no replication agreement for '$REPLICA1'" $tmpout
+        
         rlRun "rhts-sync-set -s '$TESTCOUNT.$FUNCNAME.0' -m $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.1' $MY_BR1"
         ;;
     REPLICA1_*)
-        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0'  $MY_BM"
+
+        irm_uninstall
+        irm_install $MASTER
+
+        rlRun "rhts-sync-set -s '$TESTCOUNT.$FUNCNAME.1' -m $MY_BR1"
         ;;
     REPLICA2_*)
         rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.1' $MY_BR1"
         ;;
     REPLICA3_*)
         rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.1' $MY_BR1"
         ;;
     REPLICA4_*)
         rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.1' $MY_BR1"
         ;;
     *)
     esac
@@ -178,19 +290,40 @@ function irm_del_neg_0004()
     rlPhaseStartTest "irm_del_neg_0004: del fail, already deleted agreement, remote [BZ754524]"
     case "$MYROLE" in
     MASTER_*)
+        rlRun "ipa-replica-manage $PWOPT del $REPLICA1 -f"
+
         rlRun "rhts-sync-set -s '$TESTCOUNT.$FUNCNAME.0' -m $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.1' $MY_BR2"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.2' $MY_BR1"
         ;;
     REPLICA1_*)
         rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.1' $MY_BR2"
+
+        irm_uninstall
+        irm_install $MASTER
+
+        rlRun "rhts-sync-set -s '$TESTCOUNT.$FUNCNAME.2' -m $MY_BR1"
         ;;
     REPLICA2_*)
         rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+
+        rlRun "ipa-replica-manage $PWOPT -H $MASTER del $REPLICA1 -f > $tmpout 2>&1" 1
+        rlRun "cat $tmpout"
+        rlAssertGrep "'$MASTER' has no replication agreement for '$REPLICA1'" $tmpout
+        
+        rlRun "rhts-sync-set -s '$TESTCOUNT.$FUNCNAME.1' -m $MY_BR2"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.2' $MY_BR1"
         ;;
     REPLICA3_*)
         rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.1' $MY_BR2"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.2' $MY_BR1"
         ;;
     REPLICA4_*)
         rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.0' $MY_BM"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.1' $MY_BR2"
+        rlRun "rhts-sync-block -s '$TESTCOUNT.$FUNCNAME.2' $MY_BR1"
         ;;
     *)
     esac
@@ -207,6 +340,10 @@ function irm_del_neg_0005()
     rlPhaseStartTest "irm_del_neg_0005: del fail, non-existent replica"
     case "$MYROLE" in
     MASTER_*)
+        rlRun "ipa-replica-manage $PWOPT del dne.$DOMAIN -f > $tmpout 2>&1" 1
+        rlRun "cat $tmpout"
+        rlAssertGrep "'$MASTER' has no replication agreement for 'dne.$DOMAIN'" $tmpout
+
         rlRun "rhts-sync-set -s '$TESTCOUNT.$FUNCNAME.0' -m $MY_BM"
         ;;
     REPLICA1_*)
@@ -236,6 +373,10 @@ function irm_del_neg_0006()
     rlPhaseStartTest "irm_del_neg_0006: del fail, non-existent replica, remote"
     case "$MYROLE" in
     MASTER_*)
+        rlRun "ipa-replica-manage $PWOPT -H $REPLICA4 del dne.$DOMAIN -f > $tmpout 2>&1" 1
+        rlRun "cat $tmpout"
+        rlAssertGrep "'$REPLICA4' has no replication agreement for 'dne.$DOMAIN'" $tmpout
+
         rlRun "rhts-sync-set -s '$TESTCOUNT.$FUNCNAME.0' -m $MY_BM"
         ;;
     REPLICA1_*)
--- ipa-tests/beaker/ipa-server/acceptance/ipa-replica-manage.new/tests.d/t.ipa-replica-manage.bug.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-replica-manage.new/tests.d/t.ipa-replica-manage.bug.sh
@@ -73,77 +73,67 @@ irm_bugcheck_831661()
 {
 	tmpout=$1
 	logserver=$2
-	#TESTORDER=$(( TESTORDER += 1 ))
-	rlPhaseStartTest "irm_bug_831661 - ipa-replica-manage re-initialize update failed due to named ldap timeout"
-		OUTPUTCHK=$(grep "reports: Update failed! Status.*System error" $tmpout|wc -l)
-		
-		sftp root@$logserver:/var/log/messages /opt/rhqa_ipa/messages.$logserver
-		SYSLOGCHK=$(grep "named.*LDAP query timed out. Try to adjust.*timeout" /opt/rhqa_ipa/messages.$logserver|wc -l)
-		if [ $OUTPUTCHK -gt 0 -a $SYSLOGCHK -gt 0 ]; then
-			rlFail "BZ 831661 found...ipa-replica-manage re-initialize update failed due to named ldap timeout"
-		else
-			rlPass "BZ 831661 not found."
-		fi
-	rlPhaseEnd
+	rlLog "irm_bug_831661 - ipa-replica-manage re-initialize update failed due to named ldap timeout"
+    OUTPUTCHK=$(grep "reports: Update failed! Status.*System error" $tmpout|wc -l)
+    
+    sftp root@$logserver:/var/log/messages /opt/rhqa_ipa/messages.$logserver
+    SYSLOGCHK=$(grep "named.*LDAP query timed out. Try to adjust.*timeout" /opt/rhqa_ipa/messages.$logserver|wc -l)
+    if [ $OUTPUTCHK -gt 0 -a $SYSLOGCHK -gt 0 ]; then
+        rlFail "BZ 831661 found...ipa-replica-manage re-initialize update failed due to named ldap timeout"
+    else
+        rlPass "BZ 831661 not found."
+    fi
 }	
 
 irm_bugcheck_839638()
 {
 	tmpout=$1
-	#TESTORDER=$(( TESTORDER += 1 ))
-	rlPhaseStartTest "irm_bugcheck_839638 - ipa-replica-manage allows disconnect of last connection for a single replica"
-		OUTPUTCHK1=$(grep "Deleted replication agreement" $tmpout|wc -l)
-		OUTPUTCHK2=$(grep "Cannot remove the last replication link" $tmpout | wc -l)
-		if [ $OUTPUTCHK1 -gt 0 -a $OUTPUTCHK2 -eq 0 ]; then
-			rlFail "BZ 839638 found...ipa-replica-manage allows disconnect of last connection for a single replica"
-		else
-			rlPass "BZ 839638 not found."
-		fi
-	rlPhaseEnd
+	rlLog "irm_bugcheck_839638 - ipa-replica-manage allows disconnect of last connection for a single replica"
+    OUTPUTCHK1=$(grep "Deleted replication agreement" $tmpout|wc -l)
+    OUTPUTCHK2=$(grep "Cannot remove the last replication link" $tmpout | wc -l)
+    if [ $OUTPUTCHK1 -gt 0 -a $OUTPUTCHK2 -eq 0 ]; then
+        rlFail "BZ 839638 found...ipa-replica-manage allows disconnect of last connection for a single replica"
+    else
+        rlPass "BZ 839638 not found."
+    fi
 }
 
 irm_bugcheck_826677()
 {
 	tmpout=$1
-	#TESTORDER=$(( TESTORDER += 1 ))
-	rlPhaseStartTest "irm_bugcheck_826677 - IPA cannot remove disconnected replica data to reconnect"
-		OUTPUTCHK1=$(grep "Deleting this server will orphan" $tmpout|wc -l)
-		OUTPUTCHK2=$(grep "You will need to reconfigure your replication topology to delete this server." $tmpout|wc -l)
-		if [ $OUTPUTCHK1 -gt 0 -a $OUTPUTCHK2 -gt 0 ]; then
-			rlPass "BZ 826677 not found"
-			rlPass "IPA now prevents orphaning replicas"
-		else
-			rlFail "BZ 826677 found...IPA cannot remove disconnected replica data to reconnect"	
-			rlFail "IPA should no longer allow orphaning replicas"
-		fi
-	rlPhaseEnd
+	rlLog "irm_bugcheck_826677 - IPA cannot remove disconnected replica data to reconnect"
+    OUTPUTCHK1=$(grep "Deleting this server will orphan" $tmpout|wc -l)
+    OUTPUTCHK2=$(grep "You will need to reconfigure your replication topology to delete this server." $tmpout|wc -l)
+    if [ $OUTPUTCHK1 -gt 0 -a $OUTPUTCHK2 -gt 0 ]; then
+        rlPass "BZ 826677 not found"
+        rlPass "IPA now prevents orphaning replicas"
+    else
+        rlFail "BZ 826677 found...IPA cannot remove disconnected replica data to reconnect"	
+        rlFail "IPA should no longer allow orphaning replicas"
+    fi
 }
 
 irm_bugcheck_754539()
 {
-	tmpout=$1
-	#TESTORDER=$(( TESTORDER += 1 ))
-	rlPhaseStartTest "irm_bugcheck_754539 - Connect after del using ipa-replica-manage fails"
-		OUTPUTCHK1=$(grep "You cannot connect to a previously deleted master" $tmpout|wc -l)
-		if [ $OUTPUTCHK1 -gt 0 ]; then
-			rlPass "BZ 754539 not found."
-		else
-			rlFail "BZ 754539 found...Connect after del using ipa-replica-manage fails"
-		fi
-	rlPhaseEnd
+    tmpout=$1
+    rlLog "irm_bugcheck_754539 - Connect after del using ipa-replica-manage fails"
+    OUTPUTCHK1=$(grep "You cannot connect to a previously deleted master" $tmpout|wc -l)
+    if [ $OUTPUTCHK1 -gt 0 ]; then
+        rlPass "BZ 754539 not found."
+    else
+        rlFail "BZ 754539 found...Connect after del using ipa-replica-manage fails"
+    fi
 }
 
 irm_bugcheck_823657()
 {
-	tmpout=$1
-	#TESTORDER=$(( TESTORDER += 1 ))
-	rlPhaseStartTest "irm_bugcheck_823657 - ipa-replica-manage connect fails with GSSAPI error after delete if using previous kerberos ticket"
-		OUTPUTCHK1=$(grep "SASL(-1): generic failure: GSSAPI Error: Unspecified GSS failure" $tmpout|wc -l)
-		OUTPUTCHK2=$(grep "You cannot connect to a previously deleted master" $tmpout|wc -l)
-		if [ $OUTPUTCHK1 -gt 0 -a $OUTPUTCHK2 -eq 0 ]; then
-			rlFail "BZ 823657 found...ipa-replica-manage connect fails with GSSAPI error after delete if using previous kerberos ticket"
-		else
-			rlPass "BZ 823657 not found."
-		fi
-	rlPhaseEnd
+    tmpout=$1
+    rlLog "irm_bugcheck_823657 - ipa-replica-manage connect fails with GSSAPI error after delete if using previous kerberos ticket"
+    OUTPUTCHK1=$(grep "SASL(-1): generic failure: GSSAPI Error: Unspecified GSS failure" $tmpout|wc -l)
+    OUTPUTCHK2=$(grep "You cannot connect to a previously deleted master" $tmpout|wc -l)
+    if [ $OUTPUTCHK1 -gt 0 -a $OUTPUTCHK2 -eq 0 ]; then
+        rlFail "BZ 823657 found...ipa-replica-manage connect fails with GSSAPI error after delete if using previous kerberos ticket"
+    else
+        rlPass "BZ 823657 not found."
+    fi
 }
