f3b4b6af0d09e8963efc17993dab8a02ffffc0e2
Kaleem Siddiqui <ksiddiqu@REDHAT.COM>
2013-05-30 06:36:29 +0000

    Removed debug string
    git-svn-id: https://svn.devel.redhat.com/repos/ipa-tests/trunk@5700 aeb79ed8-e03d-0410-be94-af332b95afa2

--- ipa-tests/beaker/ipa-server/acceptance/ipa-ca-install/t.ca-install.sh
+++ ipa-tests/beaker/ipa-server/acceptance/ipa-ca-install/t.ca-install.sh
@@ -235,7 +235,6 @@ echo 'expect eof ' >> $expfile
         rlRun "kinitAs $ADMINID $ADMINPW" 0 "Testing kinit as admin"
         rlRun "/bin/bash /opt/rhqa_ipa/replica-ca-install.bash" 0 "CA Replica installation with --no-host-dns"
 
-        rlRun "cat /var/log/ipareplica-ca-install.log" 0 "ipa replica CA debug log"
         rlRun "kinitAs $ADMINID $ADMINPW" 0 "Testing kinit as admin"
 
 	if [ -f /var/log/ipareplica-ca-install.log ]; then
