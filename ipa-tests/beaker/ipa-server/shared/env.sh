8b076d6c4e3c29c87720f02eba500e428e51bcaa
Namita Krishnan <nsoman@redhat.com>
2013-06-04 15:31:02 -0400

    adding files

---
+++ ipa-tests/beaker/ipa-server/shared/env.sh
@@ -0,0 +1,12 @@
+ADMINID=admin
+ADMINPW=<password>
+ROOTDN="cn=Directory Manager"
+ROOTDNPWD=<password>
+DNSFORWARD=<ip address>
+RELM=TESTRELM.COM
+DOMAIN=testrelm.com
+BASEDN="dc=testrelm,dc=com"
+NISDOMAIN=ipatest
+NTPSERVER=<ip address>
+
+export ADMINID ADMINPW ROOTDN ROOTDNPWD DNSFORWARD RELM DOMAIN NTPSERVER
